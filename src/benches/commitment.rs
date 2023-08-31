9ee889473d21becb1bef3b2e880f0f875e958aa6
Shahar Papini <spapini@starkware.co>
2023-08-31 03:11:49 +0300

    Full reduce

--- src/benches/commitment.rs
+++ src/benches/commitment.rs
@@ -2,9 +2,9 @@
 use rayon::prelude::{IntoParallelIterator, ParallelIterator};
 use std::{
     arch::x86_64::{
-        __m512i, _mm512_add_epi32, _mm512_add_epi64, _mm512_and_epi32, _mm512_load_epi32,
-        _mm512_mul_epi32, _mm512_or_epi32, _mm512_or_epi64, _mm512_set1_epi64, _mm512_slli_epi64,
-        _mm512_srli_epi64, _mm512_store_epi32, _mm512_sub_epi64,
+        __m512i, _mm512_add_epi32, _mm512_add_epi64, _mm512_and_epi32, _mm512_and_epi64,
+        _mm512_load_epi32, _mm512_mul_epi32, _mm512_or_epi32, _mm512_or_epi64, _mm512_set1_epi64,
+        _mm512_slli_epi64, _mm512_srli_epi64, _mm512_store_epi32, _mm512_sub_epi64,
     },
     cell::SyncUnsafeCell,
 };
@@ -19,10 +19,14 @@ const N_C_BITS: usize = N_TOTAL_BITS - N_THR_BITS - N_VEC_BITS - 3;
 // Index: |TTTTTTTTTT|CCCCCCCCCCCCCCCCCCCCC|VVVVVVVVV|
 //         N_THR_BITS         N_C_BITS      N_VEC_BITS
 #[inline(never)]
-unsafe fn fft_outer(values: *mut i32, twiddles: &[Vec<[Alignedi32s; 4]>], i_thr: usize) {
+unsafe fn fft_outer<const FULL_REDUCE: bool>(
+    values: *mut i32,
+    twiddles: &[Vec<[Alignedi32s; 4]>],
+    i_thr: usize,
+) {
     let thr_offset = i_thr << (N_C_BITS + N_VEC_BITS + 3);
     for i_h in 0..(1 << N_C_BITS) {
-        fft3::<0>(
+        fft3::<0, false>(
             values,
             &twiddles.get_unchecked(0)[..],
             thr_offset | (i_h << (N_VEC_BITS + 3)),
@@ -31,7 +35,7 @@ unsafe fn fft_outer(values: *mut i32, twiddles: &[Vec<[Alignedi32s; 4]>], i_thr:
             continue;
         }
         let i_h = i_h & (!((1 << 3) - 1));
-        fft3::<3>(
+        fft3::<3, false>(
             values,
             &twiddles.get_unchecked(1)[..],
             thr_offset | (i_h << (N_VEC_BITS + 3)),
@@ -40,7 +44,7 @@ unsafe fn fft_outer(values: *mut i32, twiddles: &[Vec<[Alignedi32s; 4]>], i_thr:
             continue;
         }
         let i_h = i_h & (!((1 << 6) - 1));
-        fft3::<6>(
+        fft3::<6, false>(
             values,
             &twiddles.get_unchecked(2)[..],
             thr_offset | (i_h << (N_VEC_BITS + 3)),
@@ -49,7 +53,7 @@ unsafe fn fft_outer(values: *mut i32, twiddles: &[Vec<[Alignedi32s; 4]>], i_thr:
             continue;
         }
         let i_h = i_h & (!((1 << 9) - 1));
-        fft3::<9>(
+        fft3::<9, FULL_REDUCE>(
             values,
             &twiddles.get_unchecked(3)[..],
             thr_offset | (i_h << (N_VEC_BITS + 3)),
@@ -61,7 +65,7 @@ unsafe fn fft_outer(values: *mut i32, twiddles: &[Vec<[Alignedi32s; 4]>], i_thr:
 
 // Index: |TTTTTTTTTT|HHHHHH|AAA|LLLLLLLLLL|VVVVVVVVV|
 //         N_THR_BITS          3   I_OFF    N_VEC_BITS
-unsafe fn fft3<const I_OFF: usize>(
+unsafe fn fft3<const I_OFF: usize, const FULL_REDUCE: bool>(
     values: *mut i32,
     twiddles: &[[Alignedi32s; 4]],
     c_offset: usize,
@@ -70,7 +74,7 @@ unsafe fn fft3<const I_OFF: usize>(
     let a_shift = N_VEC_BITS + I_OFF;
     let mask = _mm512_set1_epi64((1 << 31) - 1);
     let mask1 = _mm512_set1_epi64((1 << 32) - 1);
-    let p4 = _mm512_set1_epi64(((1 << 31) - 1) * 3);
+    let p3 = _mm512_set1_epi64(((1 << 31) - 1) * 3);
     let one = _mm512_set1_epi64(1);
     for i_l in 0..(1 << I_OFF) {
         let index = c_offset | (i_l << (N_VEC_BITS));
@@ -103,40 +107,41 @@ unsafe fn fft3<const I_OFF: usize>(
         let t4l = psi(one, t0l);
         let t5l = psi(one, t1l);
         let t6l = psi(one, t4l);
-        let (val0l, val4l) = butterfly(p4, mask, val0l, val4l, t6l);
-        let (val1l, val5l) = butterfly(p4, mask, val1l, val5l, t6l);
-        let (val2l, val6l) = butterfly(p4, mask, val2l, val6l, t6l);
-        let (val3l, val7l) = butterfly(p4, mask, val3l, val7l, t6l);
 
-        let (val0l, val2l) = butterfly(p4, mask, val0l, val2l, t4l);
-        let (val1l, val3l) = butterfly(p4, mask, val1l, val3l, t5l);
-        let (val4l, val6l) = butterfly(p4, mask, val4l, val6l, t4l);
-        let (val5l, val7l) = butterfly(p4, mask, val5l, val7l, t5l);
+        let (val0l, val4l) = butterfly::<false>(one, p3, mask, val0l, val4l, t6l);
+        let (val1l, val5l) = butterfly::<false>(one, p3, mask, val1l, val5l, t6l);
+        let (val2l, val6l) = butterfly::<false>(one, p3, mask, val2l, val6l, t6l);
+        let (val3l, val7l) = butterfly::<false>(one, p3, mask, val3l, val7l, t6l);
 
-        let (val0l, val1l) = butterfly(p4, mask, val0l, val1l, t0l);
-        let (val2l, val3l) = butterfly(p4, mask, val2l, val3l, t1l);
-        let (val4l, val5l) = butterfly(p4, mask, val4l, val5l, t2l);
-        let (val6l, val7l) = butterfly(p4, mask, val6l, val7l, t3l);
+        let (val0l, val2l) = butterfly::<false>(one, p3, mask, val0l, val2l, t4l);
+        let (val1l, val3l) = butterfly::<false>(one, p3, mask, val1l, val3l, t5l);
+        let (val4l, val6l) = butterfly::<false>(one, p3, mask, val4l, val6l, t4l);
+        let (val5l, val7l) = butterfly::<false>(one, p3, mask, val5l, val7l, t5l);
+
+        let (val0l, val1l) = butterfly::<FULL_REDUCE>(one, p3, mask, val0l, val1l, t0l);
+        let (val2l, val3l) = butterfly::<FULL_REDUCE>(one, p3, mask, val2l, val3l, t1l);
+        let (val4l, val5l) = butterfly::<FULL_REDUCE>(one, p3, mask, val4l, val5l, t2l);
+        let (val6l, val7l) = butterfly::<FULL_REDUCE>(one, p3, mask, val6l, val7l, t3l);
 
         // high
         let t4h = psi(one, t0h);
         let t5h = psi(one, t1h);
         let t6h = psi(one, t4h);
 
-        let (val0h, val4h) = butterfly(p4, mask, val0h, val4h, t6h);
-        let (val1h, val5h) = butterfly(p4, mask, val1h, val5h, t6h);
-        let (val2h, val6h) = butterfly(p4, mask, val2h, val6h, t6h);
-        let (val3h, val7h) = butterfly(p4, mask, val3h, val7h, t6h);
+        let (val0h, val4h) = butterfly::<false>(one, p3, mask, val0h, val4h, t6h);
+        let (val1h, val5h) = butterfly::<false>(one, p3, mask, val1h, val5h, t6h);
+        let (val2h, val6h) = butterfly::<false>(one, p3, mask, val2h, val6h, t6h);
+        let (val3h, val7h) = butterfly::<false>(one, p3, mask, val3h, val7h, t6h);
 
-        let (val0h, val2h) = butterfly(p4, mask, val0h, val2h, t4h);
-        let (val1h, val3h) = butterfly(p4, mask, val1h, val3h, t5h);
-        let (val4h, val6h) = butterfly(p4, mask, val4h, val6h, t4h);
-        let (val5h, val7h) = butterfly(p4, mask, val5h, val7h, t5h);
+        let (val0h, val2h) = butterfly::<false>(one, p3, mask, val0h, val2h, t4h);
+        let (val1h, val3h) = butterfly::<false>(one, p3, mask, val1h, val3h, t5h);
+        let (val4h, val6h) = butterfly::<false>(one, p3, mask, val4h, val6h, t4h);
+        let (val5h, val7h) = butterfly::<false>(one, p3, mask, val5h, val7h, t5h);
 
-        let (val0h, val1h) = butterfly(p4, mask, val0h, val1h, t0h);
-        let (val2h, val3h) = butterfly(p4, mask, val2h, val3h, t1h);
-        let (val4h, val5h) = butterfly(p4, mask, val4h, val5h, t2h);
-        let (val6h, val7h) = butterfly(p4, mask, val6h, val7h, t3h);
+        let (val0h, val1h) = butterfly::<FULL_REDUCE>(one, p3, mask, val0h, val1h, t0h);
+        let (val2h, val3h) = butterfly::<FULL_REDUCE>(one, p3, mask, val2h, val3h, t1h);
+        let (val4h, val5h) = butterfly::<FULL_REDUCE>(one, p3, mask, val4h, val5h, t2h);
+        let (val6h, val7h) = butterfly::<FULL_REDUCE>(one, p3, mask, val6h, val7h, t3h);
 
         // store
         _mm512_store_epi32(values.add(index | (0 << a_shift)), combine(val0l, val0h));
@@ -173,14 +178,21 @@ unsafe fn combine(l: __m512i, h: __m512i) -> __m512i {
 
 // 2 cc
 /// Computes (x>>31)|(x&MASK)
-unsafe fn reduce(mask: __m512i, x: __m512i) -> __m512i {
-    let x_h = _mm512_srli_epi64(x, 31);
-    let x_l = _mm512_and_epi32(x, mask);
-    _mm512_or_epi64(x_h, x_l)
+unsafe fn reduce<const FULL_REDUCE: bool>(one: __m512i, mask: __m512i, x: __m512i) -> __m512i {
+    if FULL_REDUCE {
+        let q = _mm512_add_epi64(_mm512_add_epi64(_mm512_srli_epi64(x, 31), x), one);
+        let q = _mm512_add_epi32(_mm512_srli_epi64(q, 31), x);
+        _mm512_and_epi64(q, mask)
+    } else {
+        let x_h = _mm512_srli_epi64(x, 31);
+        let x_l = _mm512_and_epi32(x, mask);
+        _mm512_or_epi64(x_h, x_l)
+    }
 }
 
 // 9 cc
-unsafe fn butterfly(
+unsafe fn butterfly<const FULL_REDUCE: bool>(
+    one: __m512i,
     p3: __m512i,
     mask: __m512i, // mask is 2^31-1.
     val0: __m512i,
@@ -194,14 +206,14 @@ unsafe fn butterfly(
     // m = val1*twiddle
     // twiddle in [0,2^31-1]. val in [0,2^32-1]
     // m in [0, 2^63-3*2^31+1].
-    let m = reduce(mask, _mm512_mul_epi32(val1, twiddle));
+    let m = reduce::<FULL_REDUCE>(one, mask, _mm512_mul_epi32(val1, twiddle));
     // m in [0, 3*2^31-4]
 
     // val0+m in [0, 4*2^31-5].
-    let r0 = reduce(mask, _mm512_add_epi64(val0, m));
+    let r0 = reduce::<FULL_REDUCE>(one, mask, _mm512_add_epi64(val0, m));
     // r0 in [0, 2^31+2].
     // val0+3p-m in [4,4*2^31+2].
-    let r1 = reduce(mask, _mm512_sub_epi64(_mm512_add_epi64(val0, p3), m));
+    let r1 = reduce::<FULL_REDUCE>(one, mask, _mm512_sub_epi64(_mm512_add_epi64(val0, p3), m));
     // r1 in [0, 2^31+3].
 
     (r0, r1)
@@ -271,7 +283,7 @@ pub fn fft_bench(
     (0..N_THR).into_par_iter().for_each(|i_thr| {
         // convert pointer back to a mutable slice.
         let values_slice = unsafe { unsafe_values.get().as_mut().unwrap() };
-        unsafe { fft_outer(values_slice[..].as_mut_ptr() as *mut i32, twiddles, i_thr) };
+        unsafe { fft_outer::<false>(values_slice[..].as_mut_ptr() as *mut i32, twiddles, i_thr) };
     });
     (0..N_THR).into_par_iter().for_each(|i_thr| {
         // convert pointer back to a mutable slice.
@@ -281,6 +293,6 @@ pub fn fft_bench(
     (0..N_THR).into_par_iter().for_each(|i_thr| {
         // convert pointer back to a mutable slice.
         let values_slice = unsafe { unsafe_values.get().as_mut().unwrap() };
-        unsafe { fft_outer(values_slice[..].as_mut_ptr() as *mut i32, twiddles, i_thr) };
+        unsafe { fft_outer::<true>(values_slice[..].as_mut_ptr() as *mut i32, twiddles, i_thr) };
     });
 }
