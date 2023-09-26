96673b77ac4ff445411db1c280ba88190c3d1938
Shahar Samocha <shahars@starkware.co>
2023-09-26 17:25:46 +0300

    Add tests to field operations

--- src/core/field/m31.rs
+++ src/core/field/m31.rs
@@ -131,3 +131,46 @@ impl DivAssign for M31 {
         *self = *self / rhs;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rand::Rng;
+
+    fn mul_p(a: u32, b: u32) -> u32 {
+        ((a as u64 * b as u64) % P as u64) as u32
+    }
+
+    fn add_p(a: u32, b: u32) -> u32 {
+        (a + b) % P
+    }
+
+    fn neg_p(a: u32) -> u32 {
+        if a == 0 {
+            0
+        } else {
+            P - a
+        }
+    }
+
+    #[test]
+    fn test_basic_operations() {
+        let mut rng = rand::thread_rng();
+        for _ in 0..10000 {
+            let x: u32 = rng.gen::<u32>() % P;
+            let y: u32 = rng.gen::<u32>() % P;
+            assert_eq!(
+                M31::from_u32_unchecked(add_p(x, y)),
+                M31::from_u32_unchecked(x) + M31::from_u32_unchecked(y)
+            );
+            assert_eq!(
+                M31::from_u32_unchecked(mul_p(x, y)),
+                M31::from_u32_unchecked(x) * M31::from_u32_unchecked(y)
+            );
+            assert_eq!(
+                M31::from_u32_unchecked(neg_p(x)),
+                -M31::from_u32_unchecked(x)
+            );
+        }
+    }
+}
