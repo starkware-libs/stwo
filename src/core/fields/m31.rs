7ffdcd6c2221fb2d3be43d146a3c371e73795975
Alon Haramati <alonh@starkware.co>
2023-09-27 15:32:25 +0300

    Implement complex mersenne 31 field.

--- src/core/fields/m31.rs
+++ src/core/fields/m31.rs
@@ -77,6 +77,7 @@ impl Add for M31 {
         Self::reduce((self.0 as u64) + (rhs.0 as u64))
     }
 }
+
 impl AddAssign for M31 {
     fn add_assign(&mut self, rhs: Self) {
         *self = *self + rhs;
@@ -126,6 +127,7 @@ impl Div for M31 {
         self * rhs.inverse()
     }
 }
+
 impl DivAssign for M31 {
     fn div_assign(&mut self, rhs: Self) {
         *self = *self / rhs;
