fa5d105134e221aa691891194cd0484ddab22274
Ohad Agadi <ohad.a@starkware.co>
2023-10-25 10:34:04 +0300

    wrote commit function

--- src/commitment_scheme/mdc_tree.rs
+++ src/commitment_scheme/mdc_tree.rs
@@ -1,5 +1,7 @@
+use crate::math;
+
 use super::{hasher::Hasher, NUM_BYTES_FELT};
-use std::collections::BTreeMap;
+use std::{collections::BTreeMap, iter::repeat, marker::PhantomData};
 
 type ColumnArray = Vec<Vec<u32>>;
 type ColumnLengthMap = BTreeMap<usize, Vec<Vec<u32>>>;
@@ -13,10 +15,128 @@ const MAX_SUBTREE_BOTTOM_LAYER_LENGTH: usize = 64;
 // TODO(Ohad): Remove #[allow(dead_code)].
 #[allow(dead_code)]
 pub struct MixedDegreeTree<T: Hasher> {
-    pub data: Vec<Vec<T::Hash>>,
+    pub data: Vec<Vec<Vec<u8>>>,
     pub height: usize,
     columns_size_map: ColumnLengthMap,
-    partial_trees: Vec<MixedDegreeTree<T>>,
+    sub_trees: Option<Vec<MixedDegreeTree<T>>>,
+    _marker: PhantomData<T>,
+}
+
+/// TODO(Ohad): Remove #[allow(dead_code)].
+#[allow(dead_code)]
+impl<T: Hasher> MixedDegreeTree<T> {
+    /// This operation runs on a single thread, and operates within L1 cache (ideally).
+    /// TODO(Ohad): Dynamically decide the amount of sub-trees and divide work accordingly.
+    pub fn commit(cols: Vec<Vec<u32>>) -> Self {
+        // Initiliaze map from column_length to corresponding column array.
+        let (columns_size_map, remainder_columns) =
+            self::sort_columns_and_extract_remainder(cols, MAX_SUBTREE_BOTTOM_LAYER_LENGTH);
+        let mut partial_trees: Option<Vec<MixedDegreeTree<T>>> = None;
+        let mut data: Vec<Vec<Vec<u8>>> = Vec::new();
+
+        // Deal with first layer first.
+        let first_layer_hash = if remainder_columns.is_empty() {
+            // We are at the bottom layer of the tree, no need to inject.
+            let bottom_layer_length = *columns_size_map.last_key_value().expect("Empty Map!").0;
+            let longest_columns = columns_size_map
+                .get(&bottom_layer_length)
+                .expect("Empty map!");
+            let bottom_layer_pre_hash = prepare_pre_hash_no_inject(longest_columns);
+            let first_layer_hash: Vec<T::Hash> = T::hash_many(&bottom_layer_pre_hash);
+            data.push(bottom_layer_pre_hash);
+            first_layer_hash
+        } else {
+            // There are sub-trees. collect them.
+            let partial_trees_vec = get_bottom_layer_from_subtree_roots::<T>(remainder_columns);
+            let roots = partial_trees_vec.iter().map(|tree| tree.root()).collect();
+            partial_trees = Some(partial_trees_vec);
+            roots
+        };
+        let height = math::log2_ceil(first_layer_hash.len());
+
+        // Collect data for the every layer then hash.
+        // TODO(Ohad): reduce copying, optimize further after hash interface is changed (direct hash-injection).
+        let mut child_values = first_layer_hash;
+        for _ in 0..height {
+            // Prepare values form previous layer. Inject if necessary.
+            let current_layer_length = child_values.len() / 2;
+            let columns_to_inject = columns_size_map.get(&current_layer_length);
+            child_values = match columns_to_inject {
+                Some(cols) => {
+                    let injected_pre_hash = inject::<T>(&child_values, cols);
+                    let injected_hash_results = T::hash_many(&injected_pre_hash);
+                    data.push(injected_pre_hash);
+                    injected_hash_results
+                }
+                None => {
+                    // Hash straight from pointed at address to avoid copying.
+                    //TODO(Ohad): working with slices is still more expensive than raw pointers, as building the slice pointer is redundent. optimize this.
+                    let child_values_byte_slice = unsafe {
+                        std::slice::from_raw_parts(
+                            child_values.as_ptr() as *const u8,
+                            current_layer_length * 2 * T::OUTPUT_SIZE_IN_BYTES,
+                        )
+                    };
+
+                    // Hash
+                    let hash_results = (0..current_layer_length)
+                        .map(|i| {
+                            T::hash(
+                                &child_values_byte_slice[i * 2 * T::OUTPUT_SIZE_IN_BYTES
+                                    ..i * 2 * T::OUTPUT_SIZE_IN_BYTES
+                                        + 2 * T::OUTPUT_SIZE_IN_BYTES],
+                            )
+                        })
+                        .collect();
+                    data.push(child_values.into_iter().map(|h| h.into()).collect());
+                    hash_results
+                }
+            };
+        }
+        assert_eq!(child_values.len(), 1);
+        data.push(vec![child_values[0].into()]);
+
+        Self {
+            data,
+            height,
+            columns_size_map,
+            sub_trees: partial_trees,
+            _marker: PhantomData::<T>,
+        }
+    }
+
+    pub fn root(&self) -> T::Hash {
+        let hash_bytes = self
+            .data
+            .last()
+            .expect("Root extraction error!")
+            .last()
+            .expect("Root extraction error!");
+        T::Hash::from(hash_bytes.clone())
+    }
+}
+
+// Recursive call to commit. Slices column array and commits on every slice.
+// TODO(Ohad): Consider unsafe ptr conversions instead of iter slicing.
+pub fn get_bottom_layer_from_subtree_roots<T: Hasher>(
+    cols: Vec<Vec<u32>>,
+) -> Vec<MixedDegreeTree<T>> {
+    assert!(cols.last().unwrap().len() >= MAX_SUBTREE_BOTTOM_LAYER_LENGTH);
+
+    // Slice columns.
+    let mut sliced_cols = Vec::<Vec<Vec<u32>>>::with_capacity(MAX_SUBTREE_BOTTOM_LAYER_LENGTH);
+    sliced_cols.extend(repeat(Vec::<Vec<u32>>::new()).take(MAX_SUBTREE_BOTTOM_LAYER_LENGTH));
+    cols.into_iter().for_each(|c| {
+        c.chunks(c.len() / MAX_SUBTREE_BOTTOM_LAYER_LENGTH)
+            .enumerate()
+            .for_each(|(i, c)| sliced_cols[i].push(c.to_vec()))
+    });
+
+    // Recursive commit on smalled trees
+    sliced_cols
+        .into_iter()
+        .map(|cols| MixedDegreeTree::<T>::commit(cols))
+        .collect()
 }
 
 /// Takes columns that should be commited on, sorts and maps by length.
@@ -110,7 +230,10 @@ mod tests {
     use crate::commitment_scheme::{
         blake3_hash::Blake3Hasher,
         hasher::Hasher,
-        mdc_tree::{inject, prepare_pre_hash_no_inject},
+        mdc_tree::{
+            get_bottom_layer_from_subtree_roots, inject, prepare_pre_hash_no_inject,
+            MixedDegreeTree,
+        },
     };
 
     fn init_test_trace() -> Vec<Vec<u32>> {
@@ -125,20 +248,41 @@ mod tests {
         cols
     }
 
+    fn init_test_trace2() -> Vec<Vec<u32>> {
+        let col1 = vec![1, 2, 3, 4];
+        let col2 = vec![5, 6];
+        let col3 = vec![7, 8];
+        let col4 = vec![9];
+        let cols: ColumnArray = vec![col1, col2, col3, col4];
+        cols
+    }
+
+    fn init_short_single_column() -> Vec<Vec<u32>> {
+        let col: Vec<u32> = std::iter::repeat(0).take(256).collect();
+        let cols = vec![col];
+        cols
+    }
+
+    fn init_long_single_column() -> Vec<Vec<u32>> {
+        let col: Vec<u32> = std::iter::repeat(0).take(16777216).collect();
+        let cols = vec![col];
+        cols
+    }
+
     #[test]
     fn sort_columns_and_extract_remainder_test() {
         let cols = init_test_trace();
         let (mut col_length_map, remainder_columns) =
             sort_columns_and_extract_remainder(cols.clone(), MAX_SUBTREE_BOTTOM_LAYER_LENGTH);
 
-        // Test map
+        // Test map.
         assert_eq!(col_length_map.get(&4).expect("no such key: 4").len(), 1);
         assert_eq!(col_length_map.get(&2).expect("no such key: 2").len(), 2);
         assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 1);
         assert_eq!(col_length_map.pop_last().expect("Empty map").1[1][0], 7);
         assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 9);
 
-        //Test remainder
+        // Test remainder.
         assert_eq!(remainder_columns, vec![cols[0].clone()]);
     }
 
@@ -167,5 +311,37 @@ mod tests {
             "17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f0100000003000000");
         assert_eq!(hex::encode(&injected_hash[1]),
             "17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f0200000004000000");
+    }
+
+    #[test]
+    fn partial_tree_commit_test() {
+        let cols = init_test_trace2();
+        let partial_tree: MixedDegreeTree<Blake3Hasher> =
+            MixedDegreeTree::<Blake3Hasher>::commit(cols);
+        assert_eq!(
+            partial_tree.root().to_string(),
+            "748cc35381cd5a7af1d95c563beeff0585454891e602d83845ea7569009acd76"
+        );
+    }
+
+    #[test]
+    #[ignore]
+    fn large_partial_tree_commit_test() {
+        let cols = init_long_single_column();
+        let partial_tree = MixedDegreeTree::<Blake3Hasher>::commit(cols);
+        assert_eq!(
+            partial_tree.root().to_string(),
+            "e2bc3329cfb7d030e598fc9c1320997762b2f8eb2328599b061d3898ffccb60b"
+        );
+    }
+
+    #[test]
+    fn get_bottom_layer_test() {
+        let cols = init_short_single_column();
+        let bottom_layer = get_bottom_layer_from_subtree_roots::<Blake3Hasher>(cols);
+        assert_eq!(
+            bottom_layer[0].root().to_string(),
+            "1da27fbdfb31a12605cb450b13aa42bb3b83b6ce9c6567934c948b5c29948cb4"
+        )
     }
 }
