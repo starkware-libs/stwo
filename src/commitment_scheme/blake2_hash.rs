4a26cbf651b2926cfa7a96a26beffffc4ce40b01
Ohad Agadi <ohad.a@starkware.co>
2023-10-26 13:50:43 +0300

    added hashing to a specific destination

--- src/commitment_scheme/blake2_hash.rs
+++ src/commitment_scheme/blake2_hash.rs
@@ -1,6 +1,7 @@
 use std::fmt;
 
-use blake2::{Blake2s256, Digest};
+use blake2::digest::{Update, VariableOutput};
+use blake2::{Blake2s256, Blake2sVar, Digest};
 
 // Wrapper for the blake2s hash type.
 #[derive(Clone, Copy, PartialEq)]
@@ -43,14 +44,14 @@ impl super::hasher::Hasher for Blake2sHasher {
 
     fn hash(val: &[u8]) -> Self::Hash {
         let mut hasher = Blake2s256::new();
-        hasher.update(val);
+        blake2::Digest::update(&mut hasher, val);
         Blake2sHash(hasher.finalize().into())
     }
 
     fn concat_and_hash(v1: &Self::Hash, v2: &Self::Hash) -> Blake2sHash {
         let mut hasher = Blake2s256::new();
-        hasher.update(v1.0);
-        hasher.update(v2.0);
+        blake2::Digest::update(&mut hasher, v1.0);
+        blake2::Digest::update(&mut hasher, v2.0);
 
         Blake2sHash(hasher.finalize().into())
     }
@@ -58,11 +59,33 @@ impl super::hasher::Hasher for Blake2sHasher {
     fn hash_many(data: &[Vec<u8>]) -> Vec<Self::Hash> {
         data.iter().map(|x| Self::hash(x)).collect()
     }
+
+    fn hash_one_in_place(data: &[u8], dst: &mut [u8]) {
+        let mut hasher = Blake2sVar::new(Self::OUTPUT_SIZE_IN_BYTES).unwrap();
+        hasher.update(data);
+        hasher.finalize_variable(dst).unwrap();
+    }
+
+    unsafe fn hash_many_in_place(
+        data: &[*const u8],
+        single_input_length_bytes: usize,
+        dst: &[*mut u8],
+    ) {
+        data.iter()
+            .map(|p| std::slice::from_raw_parts(*p, single_input_length_bytes))
+            .zip(
+                dst.iter()
+                    .map(|p| std::slice::from_raw_parts_mut(*p, Self::OUTPUT_SIZE_IN_BYTES)),
+            )
+            .for_each(|(input, out)| Self::hash_one_in_place(input, out))
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use crate::commitment_scheme::{blake2_hash, hasher::Hasher};
+
+    use super::Blake2sHasher;
 
     #[test]
     fn single_hash_test() {
@@ -84,5 +107,30 @@ mod tests {
                 "4a0d129873403037c2cd9b9048203687f6233fb6738956e0349bd4320fec3e90"
             );
         }
+    }
+
+    #[test]
+    fn hash_xof_test() {
+        let input = b"a";
+        let mut out = [0_u8; 32];
+
+        Blake2sHasher::hash_one_in_place(input, &mut out[..]);
+        assert_eq!(
+            "4a0d129873403037c2cd9b9048203687f6233fb6738956e0349bd4320fec3e90",
+            hex::encode(out)
+        )
+    }
+
+    #[test]
+    fn hash_many_xof_test() {
+        let input1 = "a";
+        let input2 = "b";
+        let input_arr = [input1.as_ptr(), input2.as_ptr()];
+
+        let mut out = [0_u8; 96];
+        let out_ptrs = [out.as_mut_ptr(), unsafe { out.as_mut_ptr().add(42) }];
+        unsafe { Blake2sHasher::hash_many_in_place(&input_arr, 1, &out_ptrs) };
+
+        assert_eq!("4a0d129873403037c2cd9b9048203687f6233fb6738956e0349bd4320fec3e900000000000000000000004449e92c9a7657ef2d677b8ef9da46c088f13575ea887e4818fc455a2bca50000000000000000000000000000000000000000000000", hex::encode(out));
     }
 }
--- src/commitment_scheme/blake3_hash.rs
+++ src/commitment_scheme/blake3_hash.rs
@@ -1,6 +1,5 @@
-use std::fmt;
-
 use super::hasher::Name;
+use std::fmt;
 
 // Wrapper for the blake3 hash type.
 #[derive(Clone, Copy, PartialEq, Debug)]
@@ -52,14 +51,44 @@ impl super::hasher::Hasher for Blake3Hasher {
         Blake3Hash(*hasher.finalize().as_bytes())
     }
 
+    fn hash_one_in_place(data: &[u8], dst: &mut [u8]) {
+        assert_eq!(
+            dst.len(),
+            Self::OUTPUT_SIZE_IN_BYTES,
+            "Attempt to Generate blake3 hash of size different than 32 bytes!"
+        );
+        let mut hasher = blake3::Hasher::new();
+        hasher.update(data);
+        let mut output_reader = hasher.finalize_xof();
+        output_reader.fill(dst)
+    }
+
     fn hash_many(data: &[Vec<u8>]) -> Vec<Self::Hash> {
         data.iter().map(|x| Self::hash(x)).collect()
+    }
+
+    //TODO(Ohad): Implement better blake3 module (SIMD & Memory optimizations)
+    unsafe fn hash_many_in_place(
+        data: &[*const u8],
+        single_input_length_bytes: usize,
+        dst: &[*mut u8],
+    ) {
+        data.iter()
+            .map(|p| std::slice::from_raw_parts(*p, single_input_length_bytes))
+            .zip(
+                dst.iter()
+                    .map(|p| std::slice::from_raw_parts_mut(*p, Self::OUTPUT_SIZE_IN_BYTES)),
+            )
+            .for_each(|(input, out)| Self::hash_one_in_place(input, out))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::commitment_scheme::{blake3_hash, hasher::Hasher};
+    use crate::commitment_scheme::{
+        blake3_hash::{self, Blake3Hasher},
+        hasher::Hasher,
+    };
 
     #[test]
     fn single_hash_test() {
@@ -81,5 +110,30 @@ mod tests {
                 "17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f"
             );
         }
+    }
+
+    #[test]
+    fn hash_xof_test() {
+        let input = b"a";
+        let mut out = [0_u8; 32];
+
+        Blake3Hasher::hash_one_in_place(input, &mut out[..]);
+        assert_eq!(
+            "17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f",
+            hex::encode(out)
+        )
+    }
+
+    #[test]
+    fn hash_many_xof_test() {
+        let input1 = "a";
+        let input2 = "b";
+        let input_arr = [input1.as_ptr(), input2.as_ptr()];
+
+        let mut out = [0_u8; 96];
+        let out_ptrs = [out.as_mut_ptr(), unsafe { out.as_mut_ptr().add(42) }];
+        unsafe { Blake3Hasher::hash_many_in_place(&input_arr, 1, &out_ptrs) };
+
+        assert_eq!("17762fddd969a453925d65717ac3eea21320b66b54342fde15128d6caf21215f0000000000000000000010e5cf3d3c8a4f9f3468c8cc58eea84892a22fdadbc1acb22410190044c1d55300000000000000000000000000000000000000000000", hex::encode(out));
     }
 }
