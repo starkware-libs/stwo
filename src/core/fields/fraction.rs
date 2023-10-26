042abbb86c6bf7d4267b669939144e1df83a4f5b
Shahar Samocha <shahars@starkware.co>
2023-10-26 16:56:10 +0300

    Fraction implementation of a field

---
+++ src/core/fields/fraction.rs
@@ -0,0 +1,65 @@
+use num_traits::{Num, NumAssign};
+use std::ops::{Add, AddAssign};
+
+#[derive(Copy, Clone, Debug)]
+pub struct Frac<T> {
+    pub numer: T,
+    pub denom: T,
+}
+
+impl<T> Frac<T>
+where
+    T: Copy,
+{
+    pub fn new(numer: T, denom: T) -> Self {
+        Self { numer, denom }
+    }
+}
+
+impl<T: Num + Copy> Add for Frac<T> {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Self::new(
+            self.numer * rhs.denom + self.denom * rhs.numer,
+            self.denom * rhs.denom,
+        )
+    }
+}
+
+impl<T: NumAssign + Copy> AddAssign<Frac<T>> for Frac<T> {
+    fn add_assign(&mut self, rhs: Frac<T>) {
+        *self = *self + rhs;
+    }
+}
+
+impl<T: Num + Copy> Add<T> for Frac<T> {
+    type Output = Self;
+
+    fn add(self, rhs: T) -> Self::Output {
+        Self::new(self.numer * rhs + self.denom, self.denom * rhs)
+    }
+}
+
+impl<T: NumAssign + Copy> AddAssign<T> for Frac<T> {
+    fn add_assign(&mut self, rhs: T) {
+        *self = *self + rhs;
+    }
+}
+
+#[test]
+fn test_frac() {
+    use super::m31::{M31, P};
+    use num_traits::One;
+
+    let a = Frac::new(M31::one(), M31::from_u32_unchecked(P - 1));
+    let b = Frac::new(M31::one(), M31::from_u32_unchecked(2));
+    let mut c = a + b;
+    assert_eq!(c.numer * M31::from_u32_unchecked(P - 2), c.denom);
+
+    c += c;
+    assert_eq!(
+        c.numer * M31::from_u32_unchecked(P - 2),
+        c.denom * M31::from_u32_unchecked(2)
+    );
+}
--- src/core/fields/mod.rs
+++ src/core/fields/mod.rs
@@ -1,3 +1,4 @@
 pub mod avx512_m31;
 pub mod cm31;
+pub mod fraction;
 pub mod m31;
