8cf45f0453bd9630ea92a0ce97d3b9946776acab
Shahar Samocha <shahars@starkware.co>
2023-11-02 11:45:58 +0200

    Add boundary constraint evaluation for fibonacci claim

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -11,9 +11,10 @@ pub struct Fibonacci {
     pub eval_domain: CircleDomain,
     pub constraint_coset: Coset,
     pub constraint_eval_domain: CircleDomain,
+    pub claim: Field,
 }
 impl Fibonacci {
-    pub fn new(n_bits: usize) -> Self {
+    pub fn new(n_bits: usize, claim: Field) -> Self {
         let trace_coset = CanonicCoset::new(n_bits);
         let eval_domain = trace_coset.eval_domain(n_bits + 1);
         let constraint_coset = Coset::subgroup(n_bits);
@@ -23,6 +24,7 @@ impl Fibonacci {
             eval_domain,
             constraint_coset,
             constraint_eval_domain,
+            claim,
         }
     }
     pub fn get_trace(&self) -> CircleEvaluation {
@@ -42,19 +44,24 @@ impl Fibonacci {
         // Returns as a CircleEvaluation.
         CircleEvaluation::new_canonical_ordered(self.trace_coset, trace)
     }
-    pub fn eval_constraint(&self, trace: impl PolyOracle) -> Field {
+    pub fn eval_step_constraint(&self, trace: impl PolyOracle) -> Field {
         trace.get_at(self.trace_coset.index_at(0)).square()
             + trace.get_at(self.trace_coset.index_at(1)).square()
             - trace.get_at(self.trace_coset.index_at(2))
     }
-    pub fn eval_quotient(&self, trace: impl PolyOracle) -> Field {
+
+    pub fn eval_step_quotient(&self, trace: impl PolyOracle) -> Field {
         let excluded0 = self.constraint_coset.at(self.constraint_coset.len() - 2);
         let excluded1 = self.constraint_coset.at(self.constraint_coset.len() - 1);
-        let num = self.eval_constraint(trace)
+        let num = self.eval_step_constraint(trace)
             * point_excluder(excluded0, trace.point())
             * point_excluder(excluded1, trace.point());
         let denom = coset_vanishing(self.constraint_coset, trace.point());
         num / denom
+    }
+
+    pub fn eval_boundary_constraint(&self, trace: impl PolyOracle, value: Field) -> Field {
+        trace.get_at(self.trace_coset.index_at(0)) - value
     }
 }
 
@@ -63,20 +70,47 @@ fn test_constraint_on_trace() {
     use crate::core::constraints::EvalByEvaluation;
     use num_traits::Zero;
 
-    let fib = Fibonacci::new(3);
+    let fib = Fibonacci::new(3, Field::from_u32_unchecked(1056169651));
     let trace = fib.get_trace();
 
+    // Assert that the step constraint is satisfied on the trace.
     for p_ind in fib
         .constraint_coset
         .iter_indices()
         .take(fib.constraint_coset.len() - 2)
     {
-        let res = fib.eval_constraint(EvalByEvaluation {
+        let res = fib.eval_step_constraint(EvalByEvaluation {
             offset: p_ind,
             eval: &trace,
         });
         assert_eq!(res, Field::zero());
     }
+
+    // Assert that the first trace value is 1.
+    assert_eq!(
+        fib.eval_boundary_constraint(
+            EvalByEvaluation {
+                offset: fib.constraint_coset.index_at(0),
+                eval: &trace,
+            },
+            Field::one()
+        ),
+        Field::zero()
+    );
+
+    // Assert that the last trace value is the claim.
+    assert_eq!(
+        fib.eval_boundary_constraint(
+            EvalByEvaluation {
+                offset: fib
+                    .constraint_coset
+                    .index_at(fib.constraint_coset.len() - 1),
+                eval: &trace,
+            },
+            fib.claim
+        ),
+        Field::zero()
+    );
 }
 
 #[test]
@@ -85,8 +119,7 @@ fn test_quotient_is_low_degree() {
     use crate::core::constraints::EvalByEvaluation;
     use crate::core::constraints::EvalByPoly;
 
-    let fib = Fibonacci::new(5);
-
+    let fib = Fibonacci::new(5, Field::from_u32_unchecked(443693538));
     let trace = fib.get_trace();
     let trace_poly = trace.interpolate();
 
@@ -95,7 +128,7 @@ fn test_quotient_is_low_degree() {
     // Compute quotient on the evaluation domain.
     let mut quotient_values = Vec::with_capacity(fib.constraint_eval_domain.len());
     for p_ind in fib.constraint_eval_domain.iter_indices() {
-        quotient_values.push(fib.eval_quotient(EvalByEvaluation {
+        quotient_values.push(fib.eval_step_quotient(EvalByEvaluation {
             offset: p_ind,
             eval: &extended_evaluation,
         }));
@@ -111,7 +144,7 @@ fn test_quotient_is_low_degree() {
     let point = point_index.to_point();
     assert_eq!(
         quotient_poly.eval_at_point(point),
-        fib.eval_quotient(EvalByPoly {
+        fib.eval_step_quotient(EvalByPoly {
             point,
             poly: &trace_poly
         })
