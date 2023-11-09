4d83a78d30a75931cd26d0840cf97c391fe4b871
Ohad Agadi <ohad.a@starkware.co>
2023-11-09 13:45:07 +0200

    changes to inject & map columns

--- src/commitment_scheme/utils.rs
+++ src/commitment_scheme/utils.rs
@@ -35,7 +35,7 @@ pub fn allocate_balanced_tree(
 
 /// Performes a 2-to-1 hash on a layer of a merkle tree.
 pub fn hash_layer<T: Hasher>(layer: &[u8], node_size: usize, dst: &mut [u8]) {
-    let n_nodes_in_layer = crate::math::usize_div_ceil(layer.len(), node_size);
+    let n_nodes_in_layer = crate::math::usize_safe_div(layer.len(), node_size);
     assert!(n_nodes_in_layer.is_power_of_two());
     assert!(n_nodes_in_layer / 2 <= dst.len() / T::OUTPUT_SIZE_IN_BYTES);
 
@@ -53,22 +53,13 @@ pub fn hash_layer<T: Hasher>(layer: &[u8], node_size: usize, dst: &mut [u8]) {
 }
 
 /// Takes columns that should be commited on, sorts and maps by length.
-/// Extracts columns that are too long for handling by subtrees.
-pub fn map_columns_sorted<T: Sized>(
-    cols: ColumnArray<T>,
-    max_layer_length_to_keep: usize,
-) -> (ColumnLengthMap<T>, ColumnArray<T>) {
+pub fn map_columns_sorted<T: Sized>(cols: ColumnArray<T>) -> ColumnLengthMap<T> {
     let mut columns_length_map: ColumnLengthMap<T> = BTreeMap::new();
-    let mut remainder_columns: ColumnArray<T> = Vec::new();
     for c in cols {
-        if c.len() <= (max_layer_length_to_keep) {
-            let length_index_entry = columns_length_map.entry(c.len()).or_default();
-            length_index_entry.push(c);
-        } else {
-            remainder_columns.push(c);
-        }
+        let length_index_entry = columns_length_map.entry(c.len()).or_default();
+        length_index_entry.push(c);
     }
-    (columns_length_map, remainder_columns)
+    columns_length_map
 }
 
 /// Given columns of the same length, transforms to bytes and concatenates corresponding column elements.
@@ -78,7 +69,6 @@ pub fn map_columns_sorted<T: Sized>(
 // TODO(Ohad): Think about endianess.
 pub unsafe fn transpose_to_bytes<T: Sized>(column_array: &ColumnArray<T>, dst: &[*mut u8]) {
     let column_length = column_array[0].len();
-    assert_eq!(column_length, dst.len());
 
     for (i, ptr) in dst.iter().enumerate().take(column_length) {
         unsafe {
@@ -97,14 +87,16 @@ pub unsafe fn transpose_to_bytes<T: Sized>(column_array: &ColumnArray<T>, dst: &
 
 /// Inject columns to pre-allocated arrays.
 /// # Safety
-/// Pointers in 'dst' should point to pre-allocated memory with enough space to store column_array.len() amount of u32 elements + 2*OUTPUT_SIZE of bytes.
-pub unsafe fn inject<const OUTPUT_SIZE_BYTES: usize, T: Sized>(
+/// dst should point to pre-allocated memory with enough space to store the entire column array + offset*(n_rows/n_rows_in_node) amount of T elements.
+pub unsafe fn inject<T: Sized>(
     column_array: &ColumnArray<T>,
     dst: &mut [u8],
+    n_rows_in_node: usize,
+    offset: usize,
 ) {
-    let offset = column_array.len() * std::mem::size_of::<T>() + 2 * OUTPUT_SIZE_BYTES;
-    let offseted_pointers: Vec<*mut u8> = (2 * OUTPUT_SIZE_BYTES..dst.len())
-        .step_by(offset)
+    let ptr_offset = column_array.len() * n_rows_in_node * std::mem::size_of::<T>() + offset;
+    let offseted_pointers: Vec<*mut u8> = (offset..dst.len())
+        .step_by(ptr_offset)
         .map(|i| unsafe { dst.as_mut_ptr().add(i) })
         .collect();
     transpose_to_bytes::<T>(column_array, &offseted_pointers);
@@ -122,12 +114,8 @@ mod tests {
         math,
     };
 
-    const MAX_SUBTREE_BOTTOM_LAYER_LENGTH: usize = 64;
-
     fn init_test_trace() -> ColumnArray<u32> {
-        let col0 = std::iter::repeat(0)
-            .take(2 * MAX_SUBTREE_BOTTOM_LAYER_LENGTH)
-            .collect();
+        let col0 = std::iter::repeat(0).take(8).collect();
         let col1 = vec![1, 2, 3, 4];
         let col2 = vec![5, 6];
         let col3 = vec![7, 8];
@@ -148,24 +136,20 @@ mod tests {
     #[test]
     fn sort_columns_and_extract_remainder_test() {
         let cols = init_test_trace();
-        let (mut col_length_map, remainder_columns) =
-            map_columns_sorted(cols.clone(), MAX_SUBTREE_BOTTOM_LAYER_LENGTH);
+        let mut col_length_map = map_columns_sorted(cols.clone());
 
         // Test map
         assert_eq!(col_length_map.get(&4).expect("no such key: 4").len(), 1);
         assert_eq!(col_length_map.get(&2).expect("no such key: 2").len(), 2);
+        assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 0);
         assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 1);
-        assert_eq!(col_length_map.pop_last().expect("Empty map").1[1][0], 7);
-        assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 9);
-
-        //Test remainder
-        assert_eq!(remainder_columns, vec![cols[0].clone()]);
+        assert_eq!(col_length_map.pop_last().expect("Empty map").1[0][0], 5);
     }
 
     #[test]
     fn transpose_test() {
         let cols = init_transpose_test_trace();
-        let mut map = map_columns_sorted(cols, MAX_SUBTREE_BOTTOM_LAYER_LENGTH).0;
+        let mut map = map_columns_sorted(cols);
         let columns_to_transpose = map.pop_last().expect("msg").1;
 
         let mut out1 = [0_u8; 8];
@@ -194,16 +178,22 @@ mod tests {
     #[test]
     fn inject_test() {
         let cols = init_transpose_test_trace();
-        let mut map = map_columns_sorted(cols, MAX_SUBTREE_BOTTOM_LAYER_LENGTH).0;
+        let mut map = map_columns_sorted(cols);
         let columns_to_transpose = map.pop_last().expect("msg").1;
-        let mut out = [0_u8; 288];
+        let offset: usize = 1;
+        let mut out = [0_u8; 36];
         unsafe {
-            inject::<{ Blake3Hasher::OUTPUT_SIZE_IN_BYTES }, u32>(
-                &columns_to_transpose,
-                &mut out[..],
-            );
+            inject::<u32>(&columns_to_transpose, &mut out[..], 1, offset);
         }
-        assert_eq!(hex::encode(out), "000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000100000005000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000200000006000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000300000007000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000400000008000000");
+        // offset[1,5]offset[2,6]offset...
+        assert_eq!(
+            hex::encode(&out[..14]),
+            hex::encode(0u8.to_le_bytes())
+                + &hex::encode(1u32.to_le_bytes())
+                + &hex::encode(5u32.to_le_bytes())
+                + &hex::encode(0u8.to_le_bytes())
+                + &hex::encode(2u32.to_le_bytes())
+        );
     }
 
     #[test]
