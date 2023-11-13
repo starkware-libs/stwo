983b1dbf07c06b0bff09fbb16645d2108ce69b99
Alon Haramati <alonh@starkware.co>
2023-11-13 13:15:05 +0200

    Implement Sub and Div between QM31 and M31.

--- src/core/fields/qm31.rs
+++ src/core/fields/qm31.rs
@@ -58,7 +58,7 @@ mod tests {
         let x = QM31::from_u32_unchecked(1, 2, 3, 4);
         let y = QM31::from_u32_unchecked(4, 5, 6, 7);
         let m = M31::from_u32_unchecked(8);
-        let q = QM31::from_u32_unchecked(8, 0, 0, 0);
+        let q = QM31::from(m);
         assert_eq!(x + y, QM31::from_u32_unchecked(5, 7, 9, 11));
         assert_eq!(y + m, y + q);
     }
@@ -68,7 +68,7 @@ mod tests {
         let x = QM31::from_u32_unchecked(1, 2, 3, 4);
         let y = QM31::from_u32_unchecked(4, 5, 6, 7);
         let m = M31::from_u32_unchecked(8);
-        let q = QM31::from_u32_unchecked(8, 0, 0, 0);
+        let q = QM31::from(m);
         assert_eq!(x * y, QM31::from_u32_unchecked(P - 106, 38, P - 16, 50));
         assert_eq!(y * m, y * q);
     }
@@ -83,13 +83,19 @@ mod tests {
     fn test_subtraction() {
         let x = QM31::from_u32_unchecked(1, 2, 3, 4);
         let y = QM31::from_u32_unchecked(4, 5, 6, 7);
+        let m = M31::from_u32_unchecked(8);
+        let q = QM31::from(m);
         assert_eq!(x - y, QM31::from_u32_unchecked(P - 3, P - 3, P - 3, P - 3));
+        assert_eq!(y - m, y - q);
     }
 
     #[test]
     fn test_division() {
         let x = QM31::from_u32_unchecked(P - 106, 38, P - 16, 50);
         let y = QM31::from_u32_unchecked(4, 5, 6, 7);
+        let m = M31::from_u32_unchecked(8);
+        let q = QM31::from(m);
         assert_eq!(x / y, QM31::from_u32_unchecked(1, 2, 3, 4));
+        assert_eq!(y / m, y / q);
     }
 }
