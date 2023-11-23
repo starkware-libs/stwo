d93a14f2c44ff8afb4b7e346df465220bd66e942
Shahar Samocha <shahars@starkware.co>
2023-11-23 14:09:04 +0200

    Benchmark M31 circle point addition

---
+++ benches/circle.rs
@@ -0,0 +1,57 @@
+use criterion::black_box;
+use prover_research::core::circle::{CirclePointIndex, M31_CIRCLE_ORDER_BITS};
+use rand::Rng;
+
+pub const N_ELEMENTS: usize = 1 << 10;
+pub const N_STATE_ELEMENTS: usize = 8;
+
+pub fn circle_point_bench(c: &mut criterion::Criterion) {
+    let mut rng = rand::thread_rng();
+    let mut circle_indices: Vec<CirclePointIndex> = Vec::new();
+
+    let mut index_state: [CirclePointIndex; N_STATE_ELEMENTS] = [(); N_STATE_ELEMENTS]
+        .map(|_| CirclePointIndex(rng.gen::<usize>() % (1 << M31_CIRCLE_ORDER_BITS)));
+
+    let mut point_state = index_state
+        .iter()
+        .map(|index| index.to_point())
+        .collect::<Vec<_>>();
+
+    for _ in 0..(N_ELEMENTS) {
+        let point_index = CirclePointIndex(rng.gen::<usize>() % (1 << M31_CIRCLE_ORDER_BITS));
+        circle_indices.push(point_index);
+    }
+
+    c.bench_function("CirclePoint addition", |b| {
+        b.iter(|| {
+            for index in &circle_indices {
+                for _ in 0..128 {
+                    for point in &mut point_state {
+                        *point = *point + index.to_point();
+                    }
+                }
+            }
+        })
+    });
+
+    c.bench_function("CirclePointIndex addition", |b| {
+        b.iter(|| {
+            for index in &circle_indices {
+                for _ in 0..128 {
+                    for index2 in &mut index_state {
+                        let _p = black_box((*index2 + *index).to_point());
+                        *index2 = *index2 + *index;
+                    }
+                }
+            }
+        })
+    });
+    for (index, point) in index_state.iter().zip(point_state.iter()) {
+        assert_eq!(index.to_point(), *point);
+    }
+}
+
+#[cfg(target_arch = "x86_64")]
+criterion::criterion_group!(circle_benches, circle_point_bench,);
+
+criterion::criterion_main!(circle_benches);
