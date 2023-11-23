f0a3c930293a11f73dc9ab8fc7aa77942d3a9b63
Ohad Agadi <ohad.a@starkware.co>
2023-11-23 14:55:50 +0200

    update toolchain version in clippy script

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-cargo +nightly-2023-07-05 clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
+cargo +nightly-2023-11-27 clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
     -D nonstandard-style -D rust-2018-idioms -D unused
