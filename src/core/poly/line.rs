e76bf171cf2deacc3d29073596171dfd8cc17862
Andrew Milson <andrew.j.milson@gmail.com>
2023-11-28 11:06:17 +0000

    Move line domain to line.rs

--- src/core/poly/line.rs
+++ src/core/poly/line.rs
@@ -1,38 +1,70 @@
 use crate::core::circle::Coset;
+use crate::core::fields::m31::BaseField;
 
-/// Domain defined as the x-coordinates of all points in a [CircleDomain]
+/// Domain comprising of the x-coordinates of points in a [Coset]
 #[derive(Copy, Clone, Debug)]
 pub struct LineDomain {
     coset: Coset,
 }
 
 impl LineDomain {
-    pub fn new(coset: Coset) {
-        assert!(n.is_power_of_two());
-        assert!(n as u32 <= CircleGroup::ORDER / 4);
-        let g = CircleGroup::point_with_order(4 * n as u32);
-        let coset = Coset::new(g.double().double(), g);
-        Self::from_coset(coset)
+    /// Returns a domain comprising of the x-coordinates of points in a coset
+    ///
+    /// # Panics
+    ///
+    /// * if the coset items don't all have unique x-coordinates
+    /// * if the coset lies entirely on the x-axis.
+    pub fn new(coset: Coset) -> Self {
+        assert!(coset.initial().order_bits() >= coset.step.order_bits() + 2);
+        Self { coset }
     }
 
     /// Returns the `i`th domain element
-    // TODO: could use Index trait
+    ///
+    /// # Panics
+    ///
+    /// Panics if the index exceeds the size of the domain
     pub fn at(&self, i: usize) -> BaseField {
-        let n = self.len().get();
-        assert!(i < n, "the len is {n} but index is {i}");
+        let n = self.len();
+        assert!(i < n, "the size of the domain is {n} but index is {i}");
         self.coset.at(i).x
     }
 
     /// Returns the number of elements in the domain
-    // TODO: size might be a better name
+    // TODO: size might be a more appropriate name
+    // a line domain can never be empty
+    #[allow(clippy::len_without_is_empty)]
     pub fn len(&self) -> usize {
-        // half the size because a [CircleDomain] is symmetric across the x-axis
-        // there's always at least one item in the domain
-        NonZeroUsize::new(self.0.len() / 2).unwrap()
+        self.coset.len()
     }
 
     /// Returns an iterator over elements in the domain
     pub fn iter(&self) -> impl Iterator<Item = BaseField> {
-        self.0.iter().take(self.len().get()).map(|p| p.x)
+        self.coset.iter().map(|p| p.x)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::LineDomain;
+    use crate::core::circle::Coset;
+
+    #[test]
+    #[should_panic]
+    fn bad_line_domain() {
+        // the coset points don't have unique x-coordinates
+        let coset = Coset::odds(8);
+
+        LineDomain::new(coset);
+    }
+
+    #[test]
+    fn line_domain_has_correct_size() {
+        const N: usize = 8;
+        let coset = Coset::half_odds(N);
+
+        let line_domain = LineDomain::new(coset);
+
+        assert_eq!(line_domain.len(), N);
     }
 }
