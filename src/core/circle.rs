2242c183fa98d8b51f9be9b8c5d42cf43b8e3593
Andrew Milson <andrew.j.milson@gmail.com>
2023-11-28 14:25:21 +0000

    Update grammar in comment

--- src/core/circle.rs
+++ src/core/circle.rs
@@ -26,12 +26,14 @@ impl<F: Field> CirclePoint<F> {
         *self + *self
     }
 
-    /// Applies the circle's x-coordinate doubling map i.e. `psi_x(x)`
+    /// Applies the circle's x-coordinate doubling map.
+    ///
+    /// i.e. `psi_x(x) = 2x^2 - 1`
     pub fn double_x(x: F) -> F {
         x.square().double() - F::one()
     }
 
-    /// Returns the order of a point
+    /// Returns the order of a point.
     ///
     /// # Examples
     ///
