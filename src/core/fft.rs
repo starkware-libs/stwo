7305b50730e515925e3c1dde2ec4505f1b5cc34a
Alon Haramati <91828241+alonh5@users.noreply.github.com>
2023-12-03 11:11:01 +0200

    Implement ExtensionOf trait. (#112)

--- src/core/fft.rs
+++ src/core/fft.rs
@@ -1,13 +1,13 @@
 use super::fields::m31::BaseField;
-use super::fields::Field;
+use super::fields::ExtensionOf;
 
-pub fn butterfly<F: Field>(v0: &mut F, v1: &mut F, twid: BaseField) {
+pub fn butterfly<F: ExtensionOf<BaseField>>(v0: &mut F, v1: &mut F, twid: BaseField) {
     let tmp = *v1 * twid;
     *v1 = *v0 - tmp;
     *v0 += tmp;
 }
 
-pub fn ibutterfly<F: Field>(v0: &mut F, v1: &mut F, itwid: BaseField) {
+pub fn ibutterfly<F: ExtensionOf<BaseField>>(v0: &mut F, v1: &mut F, itwid: BaseField) {
     let tmp = *v0;
     *v0 = tmp + *v1;
     *v1 = (tmp - *v1) * itwid;
