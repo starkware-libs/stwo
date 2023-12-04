c22c42cbeea53ccd1eaac5aca3a43571d621edf6
Shahar Papini <spapini@starkware.co>
2023-12-04 17:18:07 +0200

    Add rust codegen

---
+++ src/examples/mod.rs
@@ -0,0 +1,16 @@
+#[allow(clippy::modulo_one)]
+#[allow(clippy::identity_op)]
+#[allow(clippy::uninit_vec)]
+#[allow(dead_code)]
+mod fibonacci_code;
+mod ops;
+
+#[test]
+fn test_fibonacci_generation() {
+    use self::fibonacci_code::compute;
+    use crate::core::fields::m31::M31;
+    let out = compute(&fibonacci_code::Input {
+        secret: vec![M31::from_u32_unchecked(1)],
+    });
+    println!("{:?}", out.f);
+}
--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,7 @@
 #![feature(new_uninit)]
 pub mod commitment_scheme;
 pub mod core;
+pub mod examples;
 pub mod fibonacci;
 pub mod math;
 pub mod platform;
