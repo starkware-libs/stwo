318066f0244a6aa9899501b311cfbf0ca4e3e88b
Andrew Milson <andrew.j.milson@gmail.com>
2023-12-04 11:30:25 +0000

    Remove array_chunks feature

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 #![feature(stdsimd, new_uninit)]
-=======
-#![feature(stdsimd, new_uninit, array_chunks)]
->>>>>>> b08ee47 (Add 2-local transformation)
 pub mod commitment_scheme;
 pub mod core;
 pub mod fibonacci;
