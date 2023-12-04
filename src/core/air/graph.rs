0039e4540527834258af47d29ae2ad4caf9c45cf
Shahar Papini <spapini@starkware.co>
2023-12-04 17:18:07 +0200

    Add rust codegen

--- src/core/air/graph.rs
+++ src/core/air/graph.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
@@ -12,9 +14,60 @@ pub struct GraphNode {
 }
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
+pub enum Op {
+    Input {
+        name: String,
+        ty: String,
+    },
+    Pad {
+        input: String,
+        prepend: Vec<String>,
+        append: Vec<String>,
+    },
+    Slice {
+        input: String,
+        start: i64,
+        end: i64,
+        step: i64,
+    },
+    Interleave {
+        inputs: Vec<String>,
+    },
+    Pointwise {
+        op: PointwiseOp,
+        inputs: Vec<String>,
+    },
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PointwiseOp {
+    pub name: String,
+    pub params: Vec<OpParam>,
+}
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
 pub enum OpParam {
     Int(i64),
     String(String),
     Bool(bool),
     List(Vec<OpParam>),
+}
+impl Display for OpParam {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            OpParam::Int(i) => write!(f, "{}", i),
+            OpParam::String(s) => write!(f, "{}", s),
+            OpParam::Bool(b) => write!(f, "{}", b),
+            OpParam::List(l) => {
+                write!(f, "[")?;
+                for (i, item) in l.iter().enumerate() {
+                    if i > 0 {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}", item)?;
+                }
+                write!(f, "]")
+            }
+        }
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 #![feature(stdsimd, new_uninit)]
 pub mod commitment_scheme;
 pub mod core;
+pub mod examples;
 pub mod fibonacci;
 pub mod math;
 pub mod platform;
