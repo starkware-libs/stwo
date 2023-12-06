0feb6513b15be96fd4a048c845302d74fec6b21e
Alon Haramati <91828241+alonh5@users.noreply.github.com>
2023-12-06 16:12:23 +0200

    Add Debug + Display bounds to Field trait. (#123)

--- src/core/fields/mod.rs
+++ src/core/fields/mod.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Debug, Display};
 use std::ops::Neg;
 
 use num_traits::{NumAssign, NumAssignOps, NumOps};
@@ -8,7 +9,7 @@ pub mod cm31;
 pub mod m31;
 pub mod qm31;
 
-pub trait Field: NumAssign + Neg<Output = Self> + Copy {
+pub trait Field: NumAssign + Neg<Output = Self> + Copy + Debug + Display {
     fn square(&self) -> Self {
         (*self) * (*self)
     }
