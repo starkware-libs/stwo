4ad4004fa6cf44a97fd07f12e866b7259f9a7096
Ohad Agadi <ohad.a@starkware.co>
2023-12-13 10:49:29 +0200

    implemented into_slice for extension fields

--- src/core/fields/mod.rs
+++ src/core/fields/mod.rs
@@ -39,9 +39,20 @@ pub trait Field: NumAssign + Neg<Output = Self> + Copy + Debug + Display {
 ///
 /// Do not use unless you are aware of the endianess in the platform you are compiling for, and the
 /// Field element's representation in memory.
+// TODO(Ohad): Do not compile on non-le targets.
 pub unsafe trait IntoSlice<T: Sized>: Sized {
-    fn into_slice(sl: &[Self]) -> &[T];
+    fn into_slice(sl: &[Self]) -> &[T] {
+        unsafe {
+            std::slice::from_raw_parts(
+                sl.as_ptr() as *const T,
+                std::mem::size_of_val(sl) / std::mem::size_of::<T>(),
+            )
+        }
+    }
 }
+
+unsafe impl<F: Field> IntoSlice<u8> for F {}
+
 pub trait ExtensionOf<F: Field>: Field + From<F> + NumOps<F> + NumAssignOps<F> {}
 
 impl<F: Field> ExtensionOf<F> for F {}
