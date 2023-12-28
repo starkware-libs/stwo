c5f8049658028540389571e51a69b01b814d554a
Ohad Agadi <ohad.a@starkware.co>
2023-12-28 13:11:50 +0200

    added display functions for mixed_degree_decommit

--- src/commitment_scheme/mixed_degree_decommitment.rs
+++ src/commitment_scheme/mixed_degree_decommitment.rs
@@ -1,6 +1,12 @@
+use std::fmt;
+
 use super::hasher::Hasher;
 use crate::core::fields::Field;
 
+/// A Merkle proof of queried indices.
+/// Used for storing a all the paths from the query leaves to the root.
+/// A correctly generated decommitment should hold all the information needed to generate the root
+/// of the tree, proving the queried values.
 pub struct MixedDecommitment<F: Field, H: Hasher> {
     pub decommitment_layers: Vec<Vec<DecommitmentNode<F, H>>>,
 }
@@ -13,8 +19,99 @@ impl<F: Field, H: Hasher> MixedDecommitment<F, H> {
     }
 }
 
+/// A node in a Merkle tree's decommitment path.
+/// Holds the input to the hash function, excluding information that can be calculated by a verifier
+/// given the entire decommitment.
+/// Hashes - one or less of the children hashes(zero happens at the leaves or at the joining point
+/// of 2 coinciding paths). injected elements - if exists.
+///
+/// # Attributes
+///
+/// * `hash` - Optional value, hash of one of the node's children.
+/// * `injected_elements` - Elements injected to the node.
+/// * `position_in_layer` - The position of the provided hash in the layer - for debugging purposes,
+///   can be deducted by a verifier.
 pub struct DecommitmentNode<F: Field, H: Hasher> {
     pub hash: Option<H::Hash>,
     pub injected_elements: Vec<F>,
     pub position_in_layer: usize,
+}
+
+impl<F: Field, H: Hasher> fmt::Display for MixedDecommitment<F, H> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match self.decommitment_layers.last() {
+            Some(_) => {
+                for (i, layer) in self.decommitment_layers.iter().enumerate() {
+                    f.write_str(&std::format!("\nLayer #[{}]:", i))?;
+                    for (j, node) in layer.iter().enumerate() {
+                        f.write_str(&std::format!("\n\tNode #[{}]: {}", j, node))?;
+                    }
+                }
+            }
+            None => f.write_str("Empty Path!")?,
+        }
+        Ok(())
+    }
+}
+
+impl<F: Field, H: Hasher> fmt::Display for DecommitmentNode<F, H> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.write_str(&std::format!(
+            "Position in Layer: {}, ",
+            self.position_in_layer
+        ))?;
+        if let Some(hash) = self.hash {
+            f.write_str(&std::format!("Hash: {}, ", hash))?;
+        }
+        f.write_str(&std::format!(
+            " Injected Elements: {:?}",
+            self.injected_elements
+        ))?;
+        f.write_str("\n")?;
+        Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::commitment_scheme::blake3_hash::Blake3Hasher;
+    use crate::commitment_scheme::hasher::Hasher;
+    use crate::core::fields::m31::M31;
+
+    #[test]
+    fn display_test() {
+        let path = super::MixedDecommitment::<M31, Blake3Hasher>::new(vec![
+            vec![super::DecommitmentNode::<M31, Blake3Hasher> {
+                hash: Some(Blake3Hasher::hash(b"a")),
+                injected_elements: (0..3).map(M31::from_u32_unchecked).collect(),
+                position_in_layer: 0,
+            }],
+            vec![
+                super::DecommitmentNode::<M31, Blake3Hasher> {
+                    hash: Some(Blake3Hasher::hash(b"b")),
+                    injected_elements: (3..6).map(M31::from_u32_unchecked).collect(),
+                    position_in_layer: 1,
+                },
+                super::DecommitmentNode::<M31, Blake3Hasher> {
+                    hash: Some(Blake3Hasher::hash(b"c")),
+                    injected_elements: (6..9).map(M31::from_u32_unchecked).collect(),
+                    position_in_layer: 0,
+                },
+            ],
+            vec![
+                super::DecommitmentNode::<M31, Blake3Hasher> {
+                    hash: Some(Blake3Hasher::hash(b"d")),
+                    injected_elements: Vec::new(),
+                    position_in_layer: 2,
+                },
+                super::DecommitmentNode::<M31, Blake3Hasher> {
+                    hash: Some(Blake3Hasher::hash(b"e")),
+                    injected_elements: Vec::new(),
+                    position_in_layer: 1,
+                },
+            ],
+        ]);
+
+        println!("{}", path)
+    }
 }
