0103a40281894e7813515d62ba0b26c022b326a8
Ohad Agadi <ohad.a@starkware.co>
2024-01-03 11:46:25 +0200

    added mixed degree merkle tree

---
+++ src/commitment_scheme/mixed_degree_merkle_tree.rs
@@ -0,0 +1,42 @@
+use std::collections::BTreeSet;
+
+use super::hasher::Hasher;
+use super::merkle_input::MerkleTreeInput;
+use super::merkle_multilayer::MerkleMultiLayer;
+use super::mixed_degree_decommitment::MixedDecommitment;
+use crate::core::fields::{Field, IntoSlice};
+
+pub struct MixedDegreeMerkleTree<'a, F: Field + Sync, H: Hasher> {
+    pub input: MerkleTreeInput<'a, F>,
+    pub lower_layers: MerkleMultiLayer<H>,
+    pub top_layers: Option<MerkleMultiLayer<H>>,
+}
+
+impl<'a, F: Field + Sync, H: Hasher> MixedDegreeMerkleTree<'a, F, H>
+where
+    F: IntoSlice<H::NativeType>,
+{
+    pub fn commit(_input: MerkleTreeInput<'a, F>) -> Self {
+        todo!()
+    }
+
+    pub fn generate_decommitment(&self, _queries: BTreeSet<usize>) -> MixedDecommitment<F, H> {
+        todo!()
+    }
+
+    pub fn root(&self) -> H::Hash {
+        match &self.top_layers {
+            Some(top_layers) => top_layers
+                .get_roots()
+                .next()
+                .expect("Empty top layer")
+                .to_owned(),
+            None => self
+                .lower_layers
+                .get_roots()
+                .next()
+                .expect("Empty Tree")
+                .to_owned(),
+        }
+    }
+}
