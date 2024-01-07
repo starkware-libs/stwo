24018e1f657aad3f7842fe83ee547b8995996239
Andrew Milson <andrew.j.milson@gmail.com>
2024-01-07 18:26:48 -0500

    Add misc FRI changes

--- benches/fri.rs
+++ benches/fri.rs
@@ -1,18 +1,18 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use prover_research::core::fields::m31::BaseField;
-use prover_research::core::fri::apply_drp;
+use prover_research::core::fri::fold_line;
 use prover_research::core::poly::line::LineEvaluation;
 
-fn fri_drp(c: &mut Criterion) {
+fn folding_benchmark(c: &mut Criterion) {
     const N: usize = 1 << 12;
     let evals = LineEvaluation::new(vec![BaseField::from_u32_unchecked(712837213); N]);
     let alpha = BaseField::from_u32_unchecked(12389);
-    c.bench_function("drp", |b| {
+    c.bench_function("fold_line", |b| {
         b.iter(|| {
-            black_box(apply_drp(black_box(&evals), black_box(alpha)));
+            black_box(fold_line(black_box(&evals), black_box(alpha)));
         })
     });
 }
 
-criterion_group!(benches, fri_drp);
+criterion_group!(benches, folding_benchmark);
 criterion_main!(benches);
