8ef4378e48e31114bc77a848eba3cd605dd8eece
Alon Haramati <alonh@starkware.co>
2024-01-08 17:14:38 +0200

    Commit on evaluation in bit reverse order.

--- src/core/poly/circle.rs
+++ src/core/poly/circle.rs
@@ -379,6 +379,7 @@ mod tests {
     use crate::core::constraints::{EvalByEvaluation, PolyOracle};
     use crate::core::fields::m31::{BaseField, M31};
     use crate::core::fields::Field;
+    use crate::core::utils::bit_reverse_index;
     use crate::m31;
 
     #[test]
@@ -493,5 +494,21 @@ mod tests {
         let not_canonic_domain = CircleDomain::new(half_coset);
 
         assert!(!not_canonic_domain.is_canonic());
+    }
+
+    #[test]
+    pub fn test_bit_reverse_indices() {
+        let log_domain_size = 7;
+        let log_small_domain_size = 5;
+        let domain = CanonicCoset::new(log_domain_size);
+        let small_domain = CanonicCoset::new(log_small_domain_size);
+        for i in 0..2u32.pow(log_domain_size) {
+            let point = domain.at(bit_reverse_index(i, log_domain_size) as usize);
+            let n_folds = log_domain_size - log_small_domain_size;
+            let small_point = small_domain
+                .at(bit_reverse_index(i / 2_u32.pow(n_folds), log_small_domain_size) as usize);
+            let expected_small_point = point.repeated_double(n_folds);
+            assert_eq!(expected_small_point, small_point);
+        }
     }
 }
--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,3 +1,13 @@
 pub fn bit_reverse_index(x: u32, log_domain_size: u32) -> u32 {
     x.reverse_bits() >> (32 - log_domain_size)
 }
+
+// TODO(AlonH): Consider benchmarking this function.
+pub fn bit_reverse_vec<T: Copy>(vec: &Vec<T>, log_domain_size: u32) -> Vec<T> {
+    assert!(vec.len() == (1 << log_domain_size));
+    let mut result = Vec::with_capacity(vec.len());
+    for i in 0..vec.len() {
+        result.push(vec[bit_reverse_index(i as u32, log_domain_size) as usize]);
+    }
+    result
+}
