9a8d7317fd91858c85a81d1f4fde34fbeedf5903
Ohad Agadi <ohad.a@starkware.co>
2024-01-08 10:46:31 +0200

    new() function for mixed degree tree

--- src/commitment_scheme/mixed_degree_merkle_tree.rs
+++ src/commitment_scheme/mixed_degree_merkle_tree.rs
@@ -4,6 +4,7 @@ use super::hasher::Hasher;
 use super::merkle_input::MerkleTreeInput;
 use super::merkle_multilayer::MerkleMultiLayer;
 use super::mixed_degree_decommitment::MixedDecommitment;
+use crate::commitment_scheme::merkle_multilayer::MerkleMultiLayerConfig;
 use crate::core::fields::{Field, IntoSlice};
 
 pub struct MixedDegreeMerkleTree<'a, F: Field, H: Hasher> {
@@ -11,6 +12,7 @@ pub struct MixedDegreeMerkleTree<'a, F: Field, H: Hasher> {
     pub layers: Vec<MerkleMultiLayer<H>>,
 }
 
+/// Sets the heights of the multi layers in the tree in ascending order.
 pub struct MixedDegreeMerkleTreeConfig {
     pub multi_layer_sizes: Vec<usize>,
 }
@@ -19,10 +21,27 @@ impl<'a, F: Field, H: Hasher> MixedDegreeMerkleTree<'a, F, H>
 where
     F: IntoSlice<H::NativeType>,
 {
-    pub fn new(_input: MerkleTreeInput<'a, F>, _config: MixedDegreeMerkleTreeConfig) -> Self {
-        todo!()
+    pub fn new(input: MerkleTreeInput<'a, F>, config: MixedDegreeMerkleTreeConfig) -> Self {
+        let tree_height = input.max_injected_depth();
+        let config_tree_height = config.multi_layer_sizes.iter().sum::<usize>();
+        assert_eq!(
+            config_tree_height, tree_height,
+            "Sum of the layer heights is not equal to the tree height deferred by the input"
+        );
+
+        let mut layers = Vec::<MerkleMultiLayer<H>>::new();
+        let mut current_depth = tree_height;
+        for layer_height in config.multi_layer_sizes.into_iter() {
+            let layer_config =
+                MerkleMultiLayerConfig::new(layer_height, 1 << (current_depth - layer_height));
+            layers.push(MerkleMultiLayer::<H>::new(layer_config));
+            current_depth -= layer_height;
+        }
+
+        MixedDegreeMerkleTree { input, layers }
     }
-    pub fn commit() -> H::Hash {
+
+    pub fn commit(&self) -> H::Hash {
         todo!()
     }
 
@@ -39,5 +58,57 @@ where
             }
             None => panic!("Empty tree!"),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{MixedDegreeMerkleTree, MixedDegreeMerkleTreeConfig};
+    use crate::commitment_scheme::blake2_hash::Blake2sHasher;
+    use crate::core::fields::m31::M31;
+
+    #[test]
+    fn new_mixed_degree_merkle_tree_test() {
+        let mut input = super::MerkleTreeInput::<M31>::new();
+        let column = vec![M31::from_u32_unchecked(0); 1 << 12];
+        input.insert_column(12, &column);
+
+        let multi_layer_sizes = [5, 4, 3].to_vec();
+        let tree = MixedDegreeMerkleTree::<M31, Blake2sHasher>::new(
+            input,
+            MixedDegreeMerkleTreeConfig {
+                multi_layer_sizes: multi_layer_sizes.clone(),
+            },
+        );
+
+        let mut remaining_height = multi_layer_sizes.iter().sum::<usize>();
+        multi_layer_sizes
+            .iter()
+            .enumerate()
+            .for_each(|(i, layer_height)| {
+                assert_eq!(tree.layers[i].config.sub_tree_height, *layer_height);
+                assert_eq!(
+                    tree.layers[i].config.n_sub_trees,
+                    1 << (remaining_height - layer_height)
+                );
+                remaining_height -= layer_height;
+            });
+    }
+
+    #[test]
+    #[should_panic]
+    fn new_mixed_degree_merkle_tree_bad_config_test() {
+        let mut input = super::MerkleTreeInput::<M31>::new();
+        let column = vec![M31::from_u32_unchecked(0); 4096];
+        input.insert_column(12, &column);
+
+        // This should panic because the sum of the layer heights is not equal to the tree height
+        // deferred by the input.
+        MixedDegreeMerkleTree::<M31, Blake2sHasher>::new(
+            input,
+            MixedDegreeMerkleTreeConfig {
+                multi_layer_sizes: [5, 4, 2].to_vec(),
+            },
+        );
     }
 }
