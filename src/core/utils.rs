620d1283385f5c54c4e42f21b944303194970c75
Alon Haramati <alonh@starkware.co>
2024-01-08 16:36:30 +0200

    Implement bit reverse for indices.

---
+++ src/core/utils.rs
@@ -0,0 +1,28 @@
+pub fn bit_reverse_index(x: u32, log_domain_size: u32) -> u32 {
+    x.reverse_bits() >> (32 - log_domain_size)
+}
+
+#[cfg(test)]
+mod tests {
+    use num_traits::Pow;
+
+    use crate::core::utils::bit_reverse_index;
+
+    #[test]
+    fn test_bit_reverse_index() {
+        fn verify_bit_reversed(x: u32, y: u32, log_domain_size: u32) {
+            for i in 0..log_domain_size {
+                let x_bit = (x >> i) & 1;
+                let y_bit = (y >> (log_domain_size - i - 1)) & 1;
+                assert!(x_bit == y_bit);
+            }
+        }
+
+        let log_domain_size = 7;
+        for i in 0..2.pow(log_domain_size) {
+            let x = i as u32;
+            let y = bit_reverse_index(x, log_domain_size);
+            verify_bit_reversed(x, y, log_domain_size);
+        }
+    }
+}
