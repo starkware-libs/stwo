39c28361e116d40727062c2b9110392ec5959eb8
Alon Haramati <alonh@starkware.co>
2024-01-08 17:14:38 +0200

    Commit on evaluation in bit reverse order.

--- src/core/utils.rs
+++ src/core/utils.rs
@@ -1,3 +1,18 @@
 pub fn bit_reverse_index(x: u32, log_domain_size: u32) -> u32 {
     x.reverse_bits() >> (32 - log_domain_size)
 }
+
+// TODO(AlonH): Consider benchmarking this function.
+/// A naive (not cache friendly) implementation of bit-reversal permutation.
+pub fn bit_reverse_in_place<T: Copy>(array: &mut [T], log_domain_size: u32) {
+    assert!(array.len() == (1 << log_domain_size));
+    for i in 0..array.len() {
+        array.swap(i, bit_reverse_index(i as u32, log_domain_size) as usize);
+    }
+}
+
+pub fn bit_reverse_vec<T: Copy>(vec: &Vec<T>, log_domain_size: u32) -> Vec<T> {
+    let mut result = vec.to_owned();
+    bit_reverse_in_place(&mut result, log_domain_size);
+    result
+}
