71c1781f4dbfce8af7c09f92b11bea0f8f807b9e
Alon Haramati <alonh@starkware.co>
2024-01-14 12:44:47 +0200

    Create Queries struct.

--- src/core/queries.rs
+++ src/core/queries.rs
@@ -1,43 +1,52 @@
 use std::collections::BTreeSet;
+use std::ops::Deref;
 
 use super::channel::{Blake2sChannel, Channel};
 
 pub const UPPER_BOUND_QUERY_BYTES: usize = 4;
 
-/// Randomizes a set of query indices uniformly over the range [0, 2^`log_query_size`).
-pub fn generate_queries(
-    channel: &mut Blake2sChannel,
-    log_query_size: u32,
-    n_queries: usize,
-) -> BTreeSet<usize> {
-    let mut queries = BTreeSet::new();
-    let mut query_cnt = 0;
-    let max_query = (1 << log_query_size) - 1;
-    loop {
-        let random_bytes = channel.draw_random_bytes();
-        for chunk in random_bytes.chunks_exact(UPPER_BOUND_QUERY_BYTES) {
-            let query_bits = u32::from_le_bytes(chunk.try_into().unwrap());
-            let quotient_query = query_bits & max_query;
-            queries.insert(quotient_query as usize);
-            query_cnt += 1;
-            if query_cnt == n_queries {
-                return queries;
+pub struct Queries(pub BTreeSet<usize>);
+
+impl Queries {
+    /// Randomizes a set of query indices uniformly over the range [0, 2^`log_query_size`).
+    pub fn generate(channel: &mut Blake2sChannel, log_query_size: u32, n_queries: usize) -> Self {
+        let mut queries = BTreeSet::new();
+        let mut query_cnt = 0;
+        let max_query = (1 << log_query_size) - 1;
+        loop {
+            let random_bytes = channel.draw_random_bytes();
+            for chunk in random_bytes.chunks_exact(UPPER_BOUND_QUERY_BYTES) {
+                let query_bits = u32::from_le_bytes(chunk.try_into().unwrap());
+                let quotient_query = query_bits & max_query;
+                queries.insert(quotient_query as usize);
+                query_cnt += 1;
+                if query_cnt == n_queries {
+                    return Self(queries);
+                }
             }
         }
     }
+
+    /// Calculates the matching query indices in a folded domain (i.e each domain point is doubled)
+    /// given `self` (the queries of the original domain) and the number of folds between domains.
+    pub fn fold(&self, n_folds: u32) -> Self {
+        Self(self.iter().map(|q| q >> n_folds).collect())
+    }
 }
 
-/// Calculates the matching query indices in a folded domain (i.e each domain point is doubled)
-/// given the queries of the original domain and the number of folds between domains.
-pub fn get_folded_queries(queries: &BTreeSet<usize>, n_folds: u32) -> BTreeSet<usize> {
-    queries.iter().map(|q| q >> n_folds).collect()
+impl Deref for Queries {
+    type Target = BTreeSet<usize>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::generate_queries;
     use crate::commitment_scheme::blake2_hash::Blake2sHash;
     use crate::core::channel::{Blake2sChannel, Channel};
+    use crate::core::queries::Queries;
 
     #[test]
     fn test_generate_queries() {
@@ -45,11 +54,11 @@ mod tests {
         let log_query_size = 31;
         let n_queries = 100;
 
-        let queries = generate_queries(channel, log_query_size, n_queries);
+        let queries = Queries::generate(channel, log_query_size, n_queries);
 
         assert!(queries.len() == n_queries);
-        for query in queries {
-            assert!(query < 1 << log_query_size);
+        for query in queries.iter() {
+            assert!(*query < 1 << log_query_size);
         }
     }
 }
