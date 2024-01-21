e895b8ee6c5b395a2b8e9d896a2c23dc4c8baba1
Shahar Papini <spapini@starkware.co>
2024-01-21 11:43:25 +0200

    Component interface

--- src/core/air/mod.rs
+++ src/core/air/mod.rs
@@ -1,12 +1,64 @@
 use std::collections::BTreeMap;
 use std::ops::Deref;
 
+use self::evaluation::{DomainEvaluationAccumulator, PointEvaluationAccumulator};
 use super::circle::{CirclePoint, CirclePointIndex};
 use super::fields::m31::BaseField;
+use super::fields::qm31::QM31;
 use super::fields::ExtensionOf;
-use super::poly::circle::PointMapping;
+use super::poly::circle::{CirclePoly, PointMapping};
 use crate::core::constraints::PolyOracle;
 use crate::core::poly::circle::CanonicCoset;
+
+pub mod evaluation;
+
+/// Arithmetic Intermediate Representation (AIR).
+/// An Air instance is assumed to already contain all the information needed to
+/// evaluate the constraints.
+/// For instance, all interaction elements are assumed to be present in it.
+/// Therefore, an AIR is generated only after the initial trace commitment phase.
+// TODO(spapini): consider renaming this struct.
+pub trait Air {
+    fn visit_components<V: ComponentVisitor>(&self, v: &mut V);
+}
+pub trait ComponentVisitor {
+    fn visit<C: Component>(&mut self, component: &C);
+}
+
+/// A component is a set of trace columns of various sizes along with a set of
+/// constraints on them.
+pub trait Component {
+    fn max_constraint_log_degree_bound(&self) -> u32;
+
+    /// Evaluates the constraint quotients of the component on constraint evaluation domains.
+    /// See [`super::poly::circle::CircleDomain::constraint_evaluation_domain`].
+    /// Accumulates quotients in `evaluation_accumulator`.
+    // Note: This will be computed using a MaterializedGraph.
+    fn evaluate_constraint_quotients_on_domain(
+        &self,
+        trace: &ComponentTrace,
+        evaluation_accumulator: &mut DomainEvaluationAccumulator,
+    );
+
+    /// Evaluates the mask values for the constraints at a point.
+    fn mask_values_at_point(
+        &self,
+        point: CirclePoint<QM31>,
+        component_trace: &ComponentTrace,
+    ) -> Vec<QM31>;
+
+    /// Evaluates the constraint quotients combination of the component, given the mask values.
+    fn evaluate_quotients_by_mask(
+        &self,
+        point: CirclePoint<QM31>,
+        mask: &[QM31],
+        evaluation_accumulator: &mut PointEvaluationAccumulator,
+    );
+
+    // TODO(spapini): Extra functions for FRI and decommitment.
+}
+
+pub struct ComponentTrace(pub Vec<CirclePoly<BaseField>>);
 
 pub struct MaskItem {
     pub column_index: usize,
