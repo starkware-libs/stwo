cdd5ac01f8d5b828c15541f90f53929f8a56a1e1
Ohad Agadi <ohad.a@starkware.co>
2024-01-24 17:01:05 +0200

    added decommit_dingle_layer private function

--- src/commitment_scheme/mixed_degree_merkle_tree.rs
+++ src/commitment_scheme/mixed_degree_merkle_tree.rs
@@ -1,3 +1,5 @@
+use std::iter::Peekable;
+
 use super::hasher::Hasher;
 use super::merkle_input::MerkleTreeInput;
 use super::merkle_multilayer::MerkleMultiLayer;
@@ -110,7 +112,85 @@ where
         panic!()
     }
 
+    // Takes iterators to the ancestor indices of the previous layers and the current layer's
+    // queried node indices. Advances on both simultaneously to produce the current layer's
+    // proof in ascending order (left -> right).
     // TODO(Ohad): remove '_'.
+    fn _decommit_layer(
+        &self,
+        layer_depth: usize,
+        mut ancestors_of_previous_layers: Peekable<impl Iterator<Item = usize>>,
+        mut current_layer_node_queries: Peekable<impl Iterator<Item = usize>>,
+    ) -> Vec<DecommitmentNode<F, H>> {
+        let mut proof_layer = Vec::<DecommitmentNode<F, H>>::new();
+
+        while let Some(query) = ancestors_of_previous_layers.next() {
+            // Handle node queries that precede the next ancestor-query.
+            self._append_preceding_queried_nodes(
+                &mut current_layer_node_queries,
+                query / 2,
+                layer_depth,
+                &mut proof_layer,
+            );
+
+            // Handle ancestor query.
+            if let Some(node) =
+                self._get_ancestor_query_node(query, &mut ancestors_of_previous_layers, layer_depth)
+            {
+                proof_layer.push(node);
+            }
+        }
+
+        // Consumes remaining node queries.
+        self._append_preceding_queried_nodes(
+            &mut current_layer_node_queries,
+            usize::MAX,
+            layer_depth,
+            &mut proof_layer,
+        );
+        proof_layer
+    }
+
+    // Consumes node queries until the next ancestor query is reached.
+    // TODO(Ohad): remove '_'.
+    // TODO(Ohad): remove clippy allow, it doesn't like a vector passed as ref, but it should be
+    // here.
+    #[allow(clippy::ptr_arg)]
+    fn _append_preceding_queried_nodes(
+        &self,
+        _node_query_iterator: &mut Peekable<impl Iterator<Item = usize>>,
+        _node_index_upper_bound: usize,
+        _layer_depth: usize,
+        _proof_layer: &mut Vec<DecommitmentNode<F, H>>,
+    ) {
+        todo!()
+    }
+
+    // Returns the a node that includes an ancestor of a previous layer query if needed, otherwise
+    // returns None.
+    fn _get_ancestor_query_node(
+        &self,
+        query: usize,
+        ancestors_iterator: &mut Peekable<impl Iterator<Item = usize>>,
+        layer_depth: usize,
+    ) -> Option<DecommitmentNode<F, H>> {
+        match ancestors_iterator.peek() {
+            // If both children are in the layer, only injected elements are needed
+            // to calculate the parent.
+            Some(next_q) if *next_q == query ^ 1 => {
+                ancestors_iterator.next();
+                None
+            }
+            _ => {
+                if query % 2 == 0 {
+                    self._get_node(layer_depth, query / 2, false, true)
+                } else {
+                    self._get_node(layer_depth, query / 2, true, false)
+                }
+            }
+        }
+    }
+
     fn _get_node(
         &self,
         layer_depth: usize,
@@ -133,7 +213,6 @@ where
         } else {
             None
         };
-
         Some(DecommitmentNode {
             right_hash,
             left_hash,
