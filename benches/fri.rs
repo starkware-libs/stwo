388de6e62b0d198ff8811688d81141ff3ff4d58f
Andrew Milson <andrew.j.milson@gmail.com>
2024-01-28 23:06:04 -0500

    Add extract_evaluation to FriLayerProof

--- benches/fri.rs
+++ benches/fri.rs
@@ -1,11 +1,16 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use prover_research::core::fields::m31::BaseField;
 use prover_research::core::fri::fold_line;
-use prover_research::core::poly::line::LineEvaluation;
+use prover_research::core::poly::circle::CanonicCoset;
+use prover_research::core::poly::line::{LineDomain, LineEvaluation};
 
 fn folding_benchmark(c: &mut Criterion) {
-    const N: usize = 1 << 12;
-    let evals = LineEvaluation::new(vec![BaseField::from_u32_unchecked(712837213); N]);
+    const LOG_SIZE: u32 = 12;
+    let domain = LineDomain::new(CanonicCoset::new(LOG_SIZE).coset());
+    let evals = LineEvaluation::new(
+        domain,
+        vec![BaseField::from_u32_unchecked(712837213); 1 << LOG_SIZE],
+    );
     let alpha = BaseField::from_u32_unchecked(12389);
     c.bench_function("fold_line", |b| {
         b.iter(|| {
