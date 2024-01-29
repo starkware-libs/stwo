e515b00a0d40b93afa16e298337ca7ad19ffcc53
Shahar Samocha <shahars@starkware.co>
2024-01-29 11:08:16 +0200

    Added proof_of_work config, proof structs

--- src/core/proof_of_work.rs
+++ src/core/proof_of_work.rs
@@ -1,31 +1,41 @@
 use crate::commitment_scheme::hasher::Hasher;
 
+#[derive(Clone, Debug)]
+pub struct ProofOfWorkConfig {
+    pub work_bits: u32,
+}
+
+#[derive(Clone, Debug)]
+pub struct ProofOfWorkProof {
+    pub salt: u64,
+}
+
 pub struct ProofOfWork<H: Hasher<NativeType = u8>> {
+    config: ProofOfWorkConfig,
     seed: H::Hash,
-    work_bits: u32,
 }
 
 // TODO(ShaharS): Consider to split to prover and verifier and create traits for them.
 impl<H: Hasher<NativeType = u8>> ProofOfWork<H> {
-    pub fn new(seed: H::Hash, work_bits: u32) -> Self {
-        Self { seed, work_bits }
+    pub fn new(seed: H::Hash, config: ProofOfWorkConfig) -> Self {
+        Self { config, seed }
     }
 
-    pub fn prove(&self) -> u64 {
+    pub fn prove(&self) -> ProofOfWorkProof {
         let mut nonce = 0u64;
         // TODO(ShaharS): naive implementation, should be replaced with a parallel one.
         loop {
             let hash = self.hash_with_nonce(nonce);
-            if check_leading_zeros(hash.as_ref(), self.work_bits) {
-                return nonce;
+            if check_leading_zeros(hash.as_ref(), self.config.work_bits) {
+                return ProofOfWorkProof { salt: nonce };
             }
             nonce += 1;
         }
     }
 
-    pub fn verify(&self, nonce: u64) -> bool {
-        let hash = self.hash_with_nonce(nonce);
-        check_leading_zeros(hash.as_ref(), self.work_bits)
+    pub fn verify(&self, proof: &ProofOfWorkProof) -> bool {
+        let hash = self.hash_with_nonce(proof.salt);
+        check_leading_zeros(hash.as_ref(), self.config.work_bits)
     }
 
     fn hash_with_nonce(&self, nonce: u64) -> H::Hash {
@@ -59,38 +69,39 @@ fn check_leading_zeros(bytes: &[u8], bound_bits: u32) -> bool {
 mod tests {
     use super::ProofOfWork;
     use crate::commitment_scheme::blake2_hash::{Blake2sHash, Blake2sHasher};
+    use crate::core::proof_of_work::{ProofOfWorkConfig, ProofOfWorkProof};
 
     #[test]
     fn test_verify_proof_of_work_success() {
         let proof_of_work_prover = ProofOfWork::<Blake2sHasher> {
             seed: Blake2sHash::from(vec![0; 32]),
-            work_bits: 10,
+            config: ProofOfWorkConfig { work_bits: 11 },
         };
-        let valid_salt = 133;
+        let proof = ProofOfWorkProof { salt: 133 };
 
-        assert!(proof_of_work_prover.verify(valid_salt));
+        assert!(proof_of_work_prover.verify(&proof));
     }
 
     #[test]
     fn test_verify_proof_of_work_fail() {
         let proof_of_work_prover = ProofOfWork::<Blake2sHasher> {
             seed: Blake2sHash::from(vec![0; 32]),
-            work_bits: 1,
+            config: ProofOfWorkConfig { work_bits: 1 },
         };
-        let invalid_salt = 0;
+        let invalid_proof = ProofOfWorkProof { salt: 0 };
 
-        assert!(!proof_of_work_prover.verify(invalid_salt));
+        assert!(!proof_of_work_prover.verify(&invalid_proof));
     }
 
     #[test]
     fn test_proof_of_work() {
         let proof_of_work_prover = ProofOfWork::<Blake2sHasher> {
             seed: Blake2sHash::from(vec![0; 32]),
-            work_bits: 12,
+            config: ProofOfWorkConfig { work_bits: 12 },
         };
 
-        let salt = proof_of_work_prover.prove();
+        let proof = proof_of_work_prover.prove();
 
-        assert!(proof_of_work_prover.verify(salt));
+        assert!(proof_of_work_prover.verify(&proof));
     }
 }
