bcdda70c601348ee4544bc8d2598e034548f4bd8
Ohad Agadi <ohad.a@starkware.co>
2024-02-08 13:52:24 +0200

    removed debug info

--- src/commitment_scheme/mixed_degree_decommitment.rs
+++ src/commitment_scheme/mixed_degree_decommitment.rs
@@ -30,52 +30,14 @@ impl<F: Field, H: Hasher> MixedDecommitment<F, H> {
 ///
 /// * `left_hash`, 'right_hash' - Optional values, hash of one of the node's children.
 /// * `witness_elements` - Elements injected to the node that are part of the witness.
-/// * 'DebugInfo' - Debug information, only available in debug builds.
-///     * `d.queried_values` - Elements injected to the node that are not part of the witness.
-///     * `d.position_in_layer` - The position of the provided hash in the layer - for debugging
-///       purposes,
 pub struct DecommitmentNode<F: Field, H: Hasher> {
     pub left_hash: Option<H::Hash>,
     pub right_hash: Option<H::Hash>,
     pub witness_elements: Vec<F>,
-    pub d: DebugInfo<F>,
-}
 
-#[cfg(debug_assertions)]
-pub struct DebugInfo<F: Field> {
+    // TODO(Ohad): remove these.
     pub queried_values: Vec<F>,
     pub position_in_layer: usize,
-}
-
-#[cfg(debug_assertions)]
-impl<F: Field> DebugInfo<F> {
-    pub fn new(queried_values: Vec<F>, position_in_layer: usize) -> Self {
-        Self {
-            queried_values,
-            position_in_layer,
-        }
-    }
-
-    pub fn queried_values(&self) -> &Vec<F> {
-        &self.queried_values
-    }
-
-    pub fn position_in_layer(&self) -> usize {
-        self.position_in_layer
-    }
-
-    pub fn set_queried_values(&mut self, queried_values: Vec<F>) {
-        self.queried_values = queried_values;
-    }
-
-    pub fn set_position_in_layer(&mut self, position_in_layer: usize) {
-        self.position_in_layer = position_in_layer;
-    }
-}
-
-#[cfg(not(debug_assertions))]
-pub struct DebugInfo<F> {
-    pub _phantom: std::marker::PhantomData<F>,
 }
 
 impl<F: Field, H: Hasher> fmt::Display for MixedDecommitment<F, H> {
@@ -107,14 +69,6 @@ impl<F: Field, H: Hasher> fmt::Display for DecommitmentNode<F, H> {
             " Witness Elements: {:?}",
             self.witness_elements
         ))?;
-        #[cfg(debug_assertions)]
-        {
-            f.write_str(&std::format!(
-                " Queried Values: {:?}, Position in Layer: {}",
-                self.d.queried_values(),
-                self.d.position_in_layer()
-            ))?;
-        }
         f.write_str("\n")?;
         Ok(())
     }
@@ -125,7 +79,6 @@ impl<F: Field, H: Hasher> fmt::Display for DecommitmentNode<F, H> {
 mod tests {
     use crate::commitment_scheme::blake3_hash::Blake3Hasher;
     use crate::commitment_scheme::hasher::Hasher;
-    use crate::commitment_scheme::mixed_degree_decommitment::DebugInfo;
     use crate::core::fields::m31::M31;
 
     #[test]
@@ -135,20 +88,23 @@ mod tests {
                 left_hash: Some(Blake3Hasher::hash(b"a")),
                 right_hash: None,
                 witness_elements: (0..6).step_by(2).map(M31::from_u32_unchecked).collect(),
-                d: DebugInfo::new((1..7).step_by(2).map(M31::from_u32_unchecked).collect(), 0),
+                queried_values: (1..7).step_by(2).map(M31::from_u32_unchecked).collect(),
+                position_in_layer: 0,
             }],
             vec![
                 super::DecommitmentNode::<M31, Blake3Hasher> {
                     right_hash: Some(Blake3Hasher::hash(b"b")),
                     left_hash: None,
                     witness_elements: (3..6).step_by(2).map(M31::from_u32_unchecked).collect(),
-                    d: DebugInfo::new((4..7).step_by(2).map(M31::from_u32_unchecked).collect(), 0),
+                    queried_values: (4..7).step_by(2).map(M31::from_u32_unchecked).collect(),
+                    position_in_layer: 0,
                 },
                 super::DecommitmentNode::<M31, Blake3Hasher> {
                     left_hash: Some(Blake3Hasher::hash(b"c")),
                     right_hash: None,
                     witness_elements: (6..9).step_by(2).map(M31::from_u32_unchecked).collect(),
-                    d: DebugInfo::new((7..10).step_by(2).map(M31::from_u32_unchecked).collect(), 0),
+                    queried_values: (7..10).step_by(2).map(M31::from_u32_unchecked).collect(),
+                    position_in_layer: 0,
                 },
             ],
         ]);
