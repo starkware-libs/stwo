28acd2113236bae75f7d4cc713cc13a985ce33f1
Alon Haramati <alonh@starkware.co>
2024-02-14 13:10:02 +0200

    Decouple commitment from decommitment.

--- src/fibonacci.rs
+++ src/fibonacci.rs
@@ -53,17 +53,12 @@ pub struct AdditionalProofData {
     pub oods_quotients: Vec<CircleEvaluation<SecureField, BitReversedOrder>>,
 }
 
-// TODO(AlonH): Removed this struct and separate the decommitment from the the commitment in the
-// stark proof.
-pub struct CommitmentProof<F: ExtensionOf<BaseField>, H: Hasher> {
-    pub decommitment: MerkleDecommitment<F, H>,
-    pub commitment: H::Hash,
-}
-
 pub struct FibonacciProof {
     pub public_input: BaseField,
-    pub trace_commitment: CommitmentProof<BaseField, MerkleHasher>,
-    pub composition_polynomial_commitment: CommitmentProof<SecureField, MerkleHasher>,
+    pub trace_commitments: Vec<<MerkleHasher as Hasher>::Hash>,
+    pub trace_decommitments: Vec<MerkleDecommitment<BaseField, MerkleHasher>>,
+    pub composition_polynomial_commitment: <MerkleHasher as Hasher>::Hash,
+    pub composition_polynomial_decommitment: MerkleDecommitment<SecureField, MerkleHasher>,
     pub trace_oods_values: Vec<SecureField>,
     pub composition_polynomial_opened_values: Vec<SecureField>,
     pub trace_opened_values: Vec<BaseField>,
@@ -291,14 +286,10 @@ impl Fibonacci {
 
         FibonacciProof {
             public_input: self.claim,
-            trace_commitment: CommitmentProof {
-                decommitment: trace_decommitment,
-                commitment: trace_commitment_scheme.root(),
-            },
-            composition_polynomial_commitment: CommitmentProof {
-                decommitment: composition_polynomial_decommitment,
-                commitment: composition_polynomial_commitment_scheme.root(),
-            },
+            trace_commitments: vec![trace_commitment_scheme.root()],
+            trace_decommitments: vec![trace_decommitment],
+            composition_polynomial_commitment: composition_polynomial_commitment_scheme.root(),
+            composition_polynomial_decommitment,
             trace_oods_values: trace_oods_evaluation.values,
             composition_polynomial_opened_values,
             trace_opened_values,
@@ -319,9 +310,9 @@ pub fn verify_proof<const N_BITS: u32>(proof: FibonacciProof) -> bool {
     let channel = &mut Channel::new(Blake2sHasher::hash(BaseField::into_slice(&[
         proof.public_input
     ])));
-    channel.mix_digest(proof.trace_commitment.commitment);
+    channel.mix_digest(proof.trace_commitments[0]);
     let random_coeff = channel.draw_random_secure_felts()[0];
-    channel.mix_digest(proof.composition_polynomial_commitment.commitment);
+    channel.mix_digest(proof.composition_polynomial_commitment);
     let oods_point = CirclePoint::<SecureField>::get_random_point(channel);
     let mask = fib.get_mask();
     let trace_oods_points = get_oods_points(&mask, oods_point, &[fib.trace_domain]);
@@ -368,12 +359,12 @@ pub fn verify_proof<const N_BITS: u32>(proof: FibonacciProof) -> bool {
         composition_polynomial_opening_positions.len(),
         proof.composition_polynomial_opened_values.len() >> FRI_STEP_SIZE
     );
-    assert!(proof.trace_commitment.decommitment.verify(
-        proof.trace_commitment.commitment,
+    assert!(proof.trace_decommitments[0].verify(
+        proof.trace_commitments[0],
         &trace_opening_positions.flatten()
     ));
-    assert!(proof.composition_polynomial_commitment.decommitment.verify(
-        proof.composition_polynomial_commitment.commitment,
+    assert!(proof.composition_polynomial_decommitment.verify(
+        proof.composition_polynomial_commitment,
         &composition_polynomial_opening_positions.flatten()
     ));
 
