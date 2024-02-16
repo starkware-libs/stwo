c681c241a65a1c91760eddd857d456fb6f628f5a
Shahar Papini <spapini@starkware.co>
2024-02-16 21:11:21 +0200

    AVX backend

--- src/core/fields/m31.rs
+++ src/core/fields/m31.rs
@@ -3,6 +3,8 @@ use std::ops::{
     Add, AddAssign, Div, DivAssign, Mul, MulAssign, Neg, Rem, RemAssign, Sub, SubAssign,
 };
 
+use bytemuck::{Pod, Zeroable};
+
 use super::ComplexConjugate;
 use crate::impl_field;
 
@@ -10,7 +12,8 @@ pub const MODULUS_BITS: u32 = 31;
 pub const N_BYTES_FELT: usize = 4;
 pub const P: u32 = 2147483647; // 2 ** 31 - 1
 
-#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash)]
+#[repr(transparent)]
+#[derive(Copy, Clone, Debug, Default, PartialEq, Eq, PartialOrd, Ord, Hash, Pod, Zeroable)]
 pub struct M31(u32);
 pub type BaseField = M31;
 
