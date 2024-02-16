9404723c69ba639502f103ba057773fcc6f68491
Shahar Papini <spapini@starkware.co>
2024-02-16 21:11:21 +0200

    AVX backend

--- src/core/fields/mod.rs
+++ src/core/fields/mod.rs
@@ -11,7 +11,7 @@ pub mod qm31;
 
 pub trait FieldOps<F: Field> {
     type Column: Column<F>;
-    fn bit_reverse_column(column: Self::Column) -> Self::Column;
+    fn bit_reverse_column(column: &mut Self::Column);
 }
 
 pub type Col<B, F> = <B as FieldOps<F>>::Column;
