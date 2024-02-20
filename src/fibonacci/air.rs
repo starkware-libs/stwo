18c186744ed5f2f51a9cc3481bd65c9b17adbe4d
Alon Haramati <alonh@starkware.co>
2024-02-20 15:46:40 +0200

    max_constraint_log_degree_bound in Air trait.

--- src/fibonacci/air.rs
+++ src/fibonacci/air.rs
@@ -1,5 +1,5 @@
 use super::component::FibonacciComponent;
-use crate::core::air::{Air, ComponentVisitor};
+use crate::core::air::{Air, Component, ComponentVisitor};
 
 pub struct FibonacciAir {
     pub component: FibonacciComponent,
@@ -14,5 +14,9 @@ impl FibonacciAir {
 impl Air for FibonacciAir {
     fn visit_components<V: ComponentVisitor>(&self, v: &mut V) {
         v.visit(&self.component);
+    }
+
+    fn max_constraint_log_degree_bound(&self) -> u32 {
+        self.component.max_constraint_log_degree_bound()
     }
 }
