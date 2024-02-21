aec508173203f0b16f59cb8b4252fdf3f19defe9
Shahar Papini <spapini@starkware.co>
2024-02-21 15:15:13 +0200

    ifft_full

--- src/core/backend/avx512/mod.rs
+++ src/core/backend/avx512/mod.rs
@@ -15,6 +15,8 @@ use crate::core::fields::{Column, FieldOps};
 use crate::core::utils;
 
 const VECS_LOG_SIZE: usize = 4;
+pub const CACHED_FFT_LOG_SIZE: usize = 16;
+pub const MIN_FFT_LOG_SIZE: usize = 5;
 
 #[derive(Copy, Clone, Debug)]
 pub struct AVX512Backend;
