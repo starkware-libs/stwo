4eedc9bd98f6dfadbbab067cf6c97e0bdb9300b4
Alon Haramati <alonh@starkware.co>
2024-02-21 13:18:54 +0200

    Move max_constraint_log_degree_bound to AirExt trait.

--- src/fibonacci/air.rs
+++ src/fibonacci/air.rs
@@ -1,5 +1,5 @@
 use super::component::FibonacciComponent;
-use crate::core::air::{Air, Component, ComponentVisitor};
+use crate::core::air::{Air, ComponentVisitor};
 use crate::core::backend::CPUBackend;
 
 pub struct FibonacciAir {
@@ -15,9 +15,5 @@ impl FibonacciAir {
 impl Air<CPUBackend> for FibonacciAir {
     fn visit_components<V: ComponentVisitor<CPUBackend>>(&self, v: &mut V) {
         v.visit(&self.component);
-    }
-
-    fn max_constraint_log_degree_bound(&self) -> u32 {
-        self.component.max_constraint_log_degree_bound()
     }
 }
