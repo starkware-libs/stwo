eb29ab4b99136f6fea30050645a41c17a03c2535
Ohad Agadi <ohad.a@starkware.co>
2024-02-22 14:22:47 +0200

    unoptimised batch-inverse

--- src/core/backend/cpu/mod.rs
+++ src/core/backend/cpu/mod.rs
@@ -14,6 +14,30 @@ impl Backend for CPUBackend {}
 
 impl<F: Field> FieldOps<F> for CPUBackend {
     type Column = Vec<F>;
+
+    // TODO(Ohad): Optimize.
+    fn batch_inverse(column: &Self::Column) -> Self::Column {
+        let n = column.len();
+        let mut r_i = Vec::with_capacity(n);
+
+        // First pass.
+        r_i.push(column[0]);
+        for i in 1..n {
+            r_i.push(r_i[i - 1] * column[i]);
+        }
+
+        // Inverse cumulative mul.
+        let mut inverse = r_i[n - 1].inverse();
+
+        // Second pass.
+        // Using the r_i array to store the result.
+        for i in (1..n).rev() {
+            r_i[i] = r_i[i - 1] * inverse;
+            inverse *= column[i];
+        }
+        r_i[0] = inverse;
+        r_i
+    }
 }
 
 impl<F: Clone + Debug> Column<F> for Vec<F> {
@@ -25,3 +49,34 @@ impl<F: Clone + Debug> Column<F> for Vec<F> {
 pub type CPUCirclePoly<F> = CirclePoly<CPUBackend, F>;
 pub type CPUCircleEvaluation<F, EvalOrder = NaturalOrder> =
     CircleEvaluation<CPUBackend, F, EvalOrder>;
+
+#[cfg(test)]
+mod tests {
+    use itertools::Itertools;
+    use rand::prelude::*;
+    use rand::rngs::SmallRng;
+
+    use crate::core::backend::{CPUBackend, FieldOps};
+    use crate::core::fields::qm31::QM31;
+    use crate::core::fields::Field;
+
+    #[test]
+    fn test_batch_inverse() {
+        let mut rng = SmallRng::seed_from_u64(0);
+        let elements: Vec<QM31> = (0..10)
+            .map(|_| {
+                QM31::from_u32_unchecked(
+                    rng.gen::<u32>(),
+                    rng.gen::<u32>(),
+                    rng.gen::<u32>(),
+                    rng.gen::<u32>(),
+                )
+            })
+            .collect();
+        let expected = elements.iter().map(|e| e.inverse()).collect_vec();
+
+        let inversed = CPUBackend::batch_inverse(&elements);
+
+        assert_eq!(expected, inversed);
+    }
+}
