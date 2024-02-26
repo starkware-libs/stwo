87a530eac1177300f98603dda1cf7fd55d0f15b7
Shahar Papini <spapini@starkware.co>
2024-02-26 11:40:48 +0200

    benchmark script

---
+++ scripts/bench.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Can be used as a drop in replacement for `cargo bench`.
+# For example, `./scripts/bench.sh` will run all benchmarks.
+# or `./scripts/bench.sh M31` will run only the M31 benchmarks.
+RUSTFLAGS="-Awarnings -C target-cpu=native -C target-feature=+avx512f -C opt-level=2" cargo bench "$@"
