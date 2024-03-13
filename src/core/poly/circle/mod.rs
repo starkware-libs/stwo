3f034edf7bc47c06606e279d43d847fc7ff164ac
Shahar Papini <spapini@starkware.co>
2024-03-13 09:34:16 +0200

    Use only canonic domains

--- src/core/poly/circle/mod.rs
+++ src/core/poly/circle/mod.rs
@@ -14,60 +14,20 @@ pub use secure_poly::{combine_secure_value, SecureCirclePoly};
 
 #[cfg(test)]
 mod tests {
-    use super::{CanonicCoset, CircleDomain};
+    use super::CanonicCoset;
     use crate::core::backend::cpu::CPUCircleEvaluation;
     use crate::core::fields::m31::BaseField;
-    use crate::core::fields::FieldExpOps;
-    use crate::core::poly::NaturalOrder;
     use crate::core::utils::bit_reverse_index;
 
     #[test]
     fn test_interpolate_and_eval() {
-        let domain = CircleDomain::constraint_evaluation_domain(3);
+        let domain = CanonicCoset::new(3).circle_domain();
         assert_eq!(domain.log_size(), 3);
         let evaluation =
             CPUCircleEvaluation::new(domain, (0..8).map(BaseField::from_u32_unchecked).collect());
         let poly = evaluation.clone().interpolate();
         let evaluation2 = poly.evaluate(domain);
         assert_eq!(evaluation.values, evaluation2.values);
-    }
-
-    #[test]
-    fn test_mixed_degree_example() {
-        let log_size = 4;
-
-        // Compute domains.
-        let domain0 = CanonicCoset::new(log_size);
-        let eval_domain0 = domain0.evaluation_domain(log_size + 4);
-        let domain1 = CanonicCoset::new(log_size + 2);
-        let eval_domain1 = domain1.evaluation_domain(log_size + 3);
-        let constraint_domain = CircleDomain::constraint_evaluation_domain(log_size + 1);
-
-        // Compute values.
-        let values1: Vec<_> = (0..(domain1.size() as u32))
-            .map(BaseField::from_u32_unchecked)
-            .collect();
-        let values0: Vec<_> = values1[1..].iter().step_by(4).map(|x| *x * *x).collect();
-
-        // Extend.
-        let trace_eval0 = CPUCircleEvaluation::new_canonical_ordered(domain0, values0);
-        let eval0 = trace_eval0.interpolate().evaluate(eval_domain0);
-        let trace_eval1 = CPUCircleEvaluation::new_canonical_ordered(domain1, values1);
-        let eval1 = trace_eval1.interpolate().evaluate(eval_domain1);
-
-        // Compute constraint.
-        let constraint_eval = CPUCircleEvaluation::<BaseField, NaturalOrder>::new(
-            constraint_domain,
-            constraint_domain
-                .iter_indices()
-                .map(|ind| {
-                    // The constraint is poly0(x+off0)^2 = poly1(x+off1).
-                    eval0.get_at(ind).square() - eval1.get_at(domain1.index_at(1) + ind).square()
-                })
-                .collect(),
-        );
-        // TODO(spapini): Check low degree.
-        println!("{:?}", constraint_eval);
     }
 
     #[test]
