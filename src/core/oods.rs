2818f4f195b6320a20d79b272ae70d9e7a805df2
Ohad Agadi <ohad.a@starkware.co>
2024-03-13 15:53:52 +0200

    integerated batch inverse to oods

--- src/core/oods.rs
+++ src/core/oods.rs
@@ -1,11 +1,12 @@
 use itertools::enumerate;
+use num_traits::Zero;
 
 use super::backend::cpu::CPUCircleEvaluation;
 use super::circle::CirclePoint;
 use super::constraints::{complex_conjugate_line, pair_vanishing, point_vanishing};
 use super::fields::m31::BaseField;
 use super::fields::qm31::SecureField;
-use super::fields::ComplexConjugate;
+use super::fields::{ComplexConjugate, FieldExpOps};
 use super::poly::circle::CircleEvaluation;
 use super::poly::{BitReversedOrder, NaturalOrder};
 use super::utils::bit_reverse_index;
@@ -55,6 +56,8 @@ pub fn get_oods_quotient(
     CircleEvaluation::new(eval.domain, values)
 }
 
+const CHUNK_LOG_SIZE: usize = 7;
+const CHUNK_SIZE: usize = 1 << CHUNK_LOG_SIZE;
 /// Returns the pair OODS quotient (i.e quotienting out both the oods point and its complex
 /// conjugate) polynomial evaluation over the whole domain. Used in case we don't want the highest
 /// monomial of the resulting quotient polynomial to increase which might take it out of the fft
@@ -65,14 +68,44 @@ pub fn get_pair_oods_quotient(
     eval: &CPUCircleEvaluation<BaseField, BitReversedOrder>,
 ) -> CPUCircleEvaluation<SecureField, NaturalOrder> {
     let mut values = Vec::with_capacity(eval.domain.size());
-    for (i, point) in enumerate(eval.domain.iter()) {
-        let index = bit_reverse_index(i, eval.domain.log_size());
-        values.push(eval_pair_oods_quotient_at_point(
-            point,
-            eval.values[index],
-            oods_point,
-            oods_value,
-        ));
+
+    // Fallback to the non-chunked version if the domain is not big enough.
+    if eval.domain.log_size() < CHUNK_LOG_SIZE as u32 {
+        for (i, point) in enumerate(eval.domain.iter()) {
+            let index = bit_reverse_index(i, eval.domain.log_size());
+            values.push(eval_pair_oods_quotient_at_point(
+                point,
+                eval.values[index],
+                oods_point,
+                oods_value,
+            ));
+        }
+    } else {
+        let denom = oods_point.complex_conjugate().y - oods_point.y;
+        let denom_inv = denom.inverse();
+        for chunk in eval.domain.iter().enumerate().array_chunks::<CHUNK_SIZE>() {
+            // Cached values for complex conjugate lines calculation.
+            let mul = (oods_value.complex_conjugate() - oods_value) * denom_inv;
+            let add = oods_value + mul * (-oods_point.y);
+
+            let numerators: [SecureField; CHUNK_SIZE] = std::array::from_fn(|i| {
+                let point = chunk[i].1;
+                let idx = bit_reverse_index(chunk[i].0, eval.domain.log_size());
+                let value = eval.values[idx];
+                let complex_conjugate_line = add + (mul * (point.y));
+                value - complex_conjugate_line
+            });
+            let denominators: [SecureField; CHUNK_SIZE] = std::array::from_fn(|i| {
+                let point = chunk[i].1;
+                pair_vanishing(oods_point, oods_point.complex_conjugate(), point.into_ef())
+            });
+            let mut inversed_denominators = [SecureField::zero(); CHUNK_SIZE];
+            SecureField::batch_inverse(&denominators, &mut inversed_denominators);
+
+            for i in 0..CHUNK_SIZE {
+                values.push(numerators[i] * inversed_denominators[i]);
+            }
+        }
     }
     CircleEvaluation::new(eval.domain, values)
 }
