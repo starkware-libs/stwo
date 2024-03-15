74e5964eb292be775c9269c876df9df9fe595160
Shahar Papini <spapini@starkware.co>
2024-03-15 18:16:49 +0200

    Air with dyn component

--- src/core/prover/utils.rs
+++ src/core/prover/utils.rs
@@ -1,4 +1,4 @@
-use crate::core::air::{Air, Component, ComponentVisitor};
+use crate::core::air::Air;
 use crate::core::backend::CPUBackend;
 use crate::core::commitment_scheme::TreeColumns;
 use crate::core::ComponentVec;
@@ -14,23 +14,12 @@ pub fn tree_wise_to_component_wise<T>(
     air: &impl Air<CPUBackend>,
     mut values: TreeColumns<T>,
 ) -> ComponentVec<T> {
-    // Recombine the trace values by component, and not by tree.
-    struct Visitor<'a, T> {
-        by_tree: &'a mut std::vec::IntoIter<T>,
-        by_component: ComponentVec<T>,
+    let values = &mut values.0.remove(0).into_iter();
+    let mut by_component = ComponentVec::default();
+    for component in air.components() {
+        by_component
+            .0
+            .push(values.take(component.mask().len()).collect());
     }
-    impl<T> ComponentVisitor<CPUBackend> for Visitor<'_, T> {
-        fn visit<C: Component<CPUBackend>>(&mut self, component: &C) {
-            self.by_component
-                .0
-                .push(self.by_tree.take(component.mask().len()).collect());
-        }
-    }
-    let values = values.0.remove(0);
-    let mut recombiner = Visitor {
-        by_tree: &mut values.into_iter(),
-        by_component: ComponentVec::default(),
-    };
-    air.visit_components(&mut recombiner);
-    recombiner.by_component
+    by_component
 }
