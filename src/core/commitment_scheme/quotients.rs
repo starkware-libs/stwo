4758cf88f443db7646ea40cd6a1c9e0b25c39e51
Shahar Papini <spapini@starkware.co>
2024-03-15 17:49:16 +0200

    Clean up fri quotients

--- src/core/commitment_scheme/quotients.rs
+++ src/core/commitment_scheme/quotients.rs
@@ -3,18 +3,15 @@ use std::collections::BTreeMap;
 
 use itertools::{izip, multiunzip, Itertools};
 
-use crate::core::backend::cpu::quotients::accumulate_row_quotients;
-use crate::core::backend::Backend;
+use crate::core::backend::{Backend, CPUBackend};
 use crate::core::circle::CirclePoint;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
-use crate::core::fields::secure_column::SecureColumn;
 use crate::core::fri::SparseCircleEvaluation;
 use crate::core::poly::circle::{CanonicCoset, CircleDomain, CircleEvaluation, SecureEvaluation};
 use crate::core::poly::BitReversedOrder;
 use crate::core::prover::VerificationError;
 use crate::core::queries::SparseSubCircleDomain;
-use crate::core::utils::bit_reverse_index;
 
 pub trait QuotientOps: Backend {
     /// Accumulates the quotients of the columns at the given domain.
@@ -28,7 +25,7 @@ pub trait QuotientOps: Backend {
         columns: &[&CircleEvaluation<Self, BaseField, BitReversedOrder>],
         random_coeff: SecureField,
         samples: &[ColumnSampleBatch],
-    ) -> SecureColumn<Self>;
+    ) -> SecureEvaluation<Self>;
 }
 
 /// A batch of column samplings at a point.
@@ -41,19 +38,19 @@ pub struct ColumnSampleBatch {
 impl ColumnSampleBatch {
     /// Groups column opening by opening point.
     /// # Arguments
-    /// opening: For each column, a vector of openings.
-    pub fn new(openings: &[&Vec<PointSample>]) -> Vec<Self> {
-        openings
+    /// opening: For each column, a vector of samples.
+    pub fn new(samples: &[&Vec<PointSample>]) -> Vec<Self> {
+        samples
             .iter()
             .enumerate()
-            .flat_map(|(column_index, openings)| {
-                openings.iter().map(move |opening| (column_index, opening))
+            .flat_map(|(column_index, samples)| {
+                samples.iter().map(move |opening| (column_index, opening))
             })
             .group_by(|(_, opening)| opening.point)
             .into_iter()
-            .map(|(point, column_openings)| Self {
+            .map(|(point, column_samples)| Self {
                 point,
-                column_indices_and_values: column_openings
+                column_indices_and_values: column_samples
                     .map(|(column_index, opening)| (column_index, opening.value))
                     .collect(),
             })
@@ -76,33 +73,32 @@ pub fn compute_fri_quotients<B: QuotientOps>(
         .into_iter()
         .sorted_by_key(|(log_size, _)| Reverse(*log_size))
         .map(|(log_size, tuples)| {
-            let (columns, openings): (Vec<_>, Vec<_>) = multiunzip(tuples);
+            let (columns, samples): (Vec<_>, Vec<_>) = multiunzip(tuples);
             let domain = CanonicCoset::new(log_size).circle_domain();
             // TODO: slice.
-            let batched_openings = ColumnSampleBatch::new(&openings);
-            let values = B::accumulate_quotients(domain, &columns, random_coeff, &batched_openings);
-            SecureEvaluation { domain, values }
+            let batched_samples = ColumnSampleBatch::new(&samples);
+            B::accumulate_quotients(domain, &columns, random_coeff, &batched_samples)
         })
         .collect()
 }
 
 pub fn fri_answers(
     column_log_sizes: Vec<u32>,
-    openings: &[Vec<PointSample>],
+    samples: &[Vec<PointSample>],
     random_coeff: SecureField,
     query_domain_per_log_size: BTreeMap<u32, SparseSubCircleDomain>,
     queried_values_per_column: &[Vec<BaseField>],
 ) -> Result<Vec<SparseCircleEvaluation<SecureField>>, VerificationError> {
-    izip!(column_log_sizes, openings, queried_values_per_column)
+    izip!(column_log_sizes, samples, queried_values_per_column)
         .group_by(|(c, ..)| *c)
         .into_iter()
         .sorted_by_key(|(log_size, _)| Reverse(*log_size))
         .map(|(log_size, tuples)| {
-            let (_, openings, queried_valued_per_column): (Vec<_>, Vec<_>, Vec<_>) =
+            let (_, samples, queried_valued_per_column): (Vec<_>, Vec<_>, Vec<_>) =
                 multiunzip(tuples);
             fri_answers_for_log_size(
                 log_size,
-                &openings,
+                &samples,
                 random_coeff,
                 &query_domain_per_log_size[&log_size],
                 &queried_valued_per_column,
@@ -113,13 +109,13 @@ pub fn fri_answers(
 
 pub fn fri_answers_for_log_size(
     log_size: u32,
-    openings: &[&Vec<PointSample>],
+    samples: &[&Vec<PointSample>],
     random_coeff: SecureField,
     query_domain: &SparseSubCircleDomain,
     queried_values_per_column: &[&Vec<BaseField>],
 ) -> Result<SparseCircleEvaluation<SecureField>, VerificationError> {
     let commitment_domain = CanonicCoset::new(log_size).circle_domain();
-    let batched_openings = ColumnSampleBatch::new(openings);
+    let batched_samples = ColumnSampleBatch::new(samples);
     for x in queried_values_per_column {
         if x.len() != query_domain.flatten().len() {
             return Err(VerificationError::InvalidStructure);
@@ -141,20 +137,13 @@ pub fn fri_answers_for_log_size(
                         CircleEvaluation::new(domain, q.take(domain.size()).copied().collect_vec())
                     })
                     .collect_vec();
-                // TODO(spapini): bit reverse iterator.
-                let values = (0..domain.size())
-                    .map(|row| {
-                        let domain_point = domain.at(bit_reverse_index(row, log_size));
-                        accumulate_row_quotients(
-                            &batched_openings,
-                            &column_evals.iter().collect_vec(),
-                            row,
-                            random_coeff,
-                            domain_point,
-                        )
-                    })
-                    .collect();
-                CircleEvaluation::new(domain, values)
+                CPUBackend::accumulate_quotients(
+                    domain,
+                    &column_evals.iter().collect_vec(),
+                    random_coeff,
+                    &batched_samples,
+                )
+                .to_cpu()
             })
             .collect(),
     );
