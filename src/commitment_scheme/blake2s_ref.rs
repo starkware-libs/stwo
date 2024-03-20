1cdc3c22c44ee72032334e223efcd8d8f91a9af0
Shahar Papini <spapini@starkware.co>
2024-03-20 16:55:30 +0200

    avx blake

---
+++ src/commitment_scheme/blake2s_ref.rs
@@ -0,0 +1,217 @@
+//! An AVX512 implementation of the BLAKE2s compression function.
+//! Based on https://github.com/oconnor663/blake2_simd/blob/master/blake2s/src/avx2.rs .
+
+pub const IV: [u32; 8] = [
+    0x6A09E667, 0xBB67AE85, 0x3C6EF372, 0xA54FF53A, 0x510E527F, 0x9B05688C, 0x1F83D9AB, 0x5BE0CD19,
+];
+
+pub const SIGMA: [[u8; 16]; 10] = [
+    [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+    [14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3],
+    [11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4],
+    [7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8],
+    [9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13],
+    [2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9],
+    [12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11],
+    [13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10],
+    [6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5],
+    [10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0],
+];
+
+#[inline(always)]
+fn add(a: u32, b: u32) -> u32 {
+    a.wrapping_add(b)
+}
+
+#[inline(always)]
+fn xor(a: u32, b: u32) -> u32 {
+    a ^ b
+}
+
+#[inline(always)]
+fn rot16(x: u32) -> u32 {
+    (x >> 16) | (x << (32 - 16))
+}
+
+#[inline(always)]
+fn rot12(x: u32) -> u32 {
+    (x >> 12) | (x << (32 - 12))
+}
+
+#[inline(always)]
+fn rot8(x: u32) -> u32 {
+    (x >> 8) | (x << (32 - 8))
+}
+
+#[inline(always)]
+fn rot7(x: u32) -> u32 {
+    (x >> 7) | (x << (32 - 7))
+}
+
+#[inline(always)]
+fn round(v: &mut [u32; 16], m: [u32; 16], r: usize) {
+    v[0] = add(v[0], m[SIGMA[r][0] as usize]);
+    v[1] = add(v[1], m[SIGMA[r][2] as usize]);
+    v[2] = add(v[2], m[SIGMA[r][4] as usize]);
+    v[3] = add(v[3], m[SIGMA[r][6] as usize]);
+    v[0] = add(v[0], v[4]);
+    v[1] = add(v[1], v[5]);
+    v[2] = add(v[2], v[6]);
+    v[3] = add(v[3], v[7]);
+    v[12] = xor(v[12], v[0]);
+    v[13] = xor(v[13], v[1]);
+    v[14] = xor(v[14], v[2]);
+    v[15] = xor(v[15], v[3]);
+    v[12] = rot16(v[12]);
+    v[13] = rot16(v[13]);
+    v[14] = rot16(v[14]);
+    v[15] = rot16(v[15]);
+    v[8] = add(v[8], v[12]);
+    v[9] = add(v[9], v[13]);
+    v[10] = add(v[10], v[14]);
+    v[11] = add(v[11], v[15]);
+    v[4] = xor(v[4], v[8]);
+    v[5] = xor(v[5], v[9]);
+    v[6] = xor(v[6], v[10]);
+    v[7] = xor(v[7], v[11]);
+    v[4] = rot12(v[4]);
+    v[5] = rot12(v[5]);
+    v[6] = rot12(v[6]);
+    v[7] = rot12(v[7]);
+    v[0] = add(v[0], m[SIGMA[r][1] as usize]);
+    v[1] = add(v[1], m[SIGMA[r][3] as usize]);
+    v[2] = add(v[2], m[SIGMA[r][5] as usize]);
+    v[3] = add(v[3], m[SIGMA[r][7] as usize]);
+    v[0] = add(v[0], v[4]);
+    v[1] = add(v[1], v[5]);
+    v[2] = add(v[2], v[6]);
+    v[3] = add(v[3], v[7]);
+    v[12] = xor(v[12], v[0]);
+    v[13] = xor(v[13], v[1]);
+    v[14] = xor(v[14], v[2]);
+    v[15] = xor(v[15], v[3]);
+    v[12] = rot8(v[12]);
+    v[13] = rot8(v[13]);
+    v[14] = rot8(v[14]);
+    v[15] = rot8(v[15]);
+    v[8] = add(v[8], v[12]);
+    v[9] = add(v[9], v[13]);
+    v[10] = add(v[10], v[14]);
+    v[11] = add(v[11], v[15]);
+    v[4] = xor(v[4], v[8]);
+    v[5] = xor(v[5], v[9]);
+    v[6] = xor(v[6], v[10]);
+    v[7] = xor(v[7], v[11]);
+    v[4] = rot7(v[4]);
+    v[5] = rot7(v[5]);
+    v[6] = rot7(v[6]);
+    v[7] = rot7(v[7]);
+
+    v[0] = add(v[0], m[SIGMA[r][8] as usize]);
+    v[1] = add(v[1], m[SIGMA[r][10] as usize]);
+    v[2] = add(v[2], m[SIGMA[r][12] as usize]);
+    v[3] = add(v[3], m[SIGMA[r][14] as usize]);
+    v[0] = add(v[0], v[5]);
+    v[1] = add(v[1], v[6]);
+    v[2] = add(v[2], v[7]);
+    v[3] = add(v[3], v[4]);
+    v[15] = xor(v[15], v[0]);
+    v[12] = xor(v[12], v[1]);
+    v[13] = xor(v[13], v[2]);
+    v[14] = xor(v[14], v[3]);
+    v[15] = rot16(v[15]);
+    v[12] = rot16(v[12]);
+    v[13] = rot16(v[13]);
+    v[14] = rot16(v[14]);
+    v[10] = add(v[10], v[15]);
+    v[11] = add(v[11], v[12]);
+    v[8] = add(v[8], v[13]);
+    v[9] = add(v[9], v[14]);
+    v[5] = xor(v[5], v[10]);
+    v[6] = xor(v[6], v[11]);
+    v[7] = xor(v[7], v[8]);
+    v[4] = xor(v[4], v[9]);
+    v[5] = rot12(v[5]);
+    v[6] = rot12(v[6]);
+    v[7] = rot12(v[7]);
+    v[4] = rot12(v[4]);
+    v[0] = add(v[0], m[SIGMA[r][9] as usize]);
+    v[1] = add(v[1], m[SIGMA[r][11] as usize]);
+    v[2] = add(v[2], m[SIGMA[r][13] as usize]);
+    v[3] = add(v[3], m[SIGMA[r][15] as usize]);
+    v[0] = add(v[0], v[5]);
+    v[1] = add(v[1], v[6]);
+    v[2] = add(v[2], v[7]);
+    v[3] = add(v[3], v[4]);
+    v[15] = xor(v[15], v[0]);
+    v[12] = xor(v[12], v[1]);
+    v[13] = xor(v[13], v[2]);
+    v[14] = xor(v[14], v[3]);
+    v[15] = rot8(v[15]);
+    v[12] = rot8(v[12]);
+    v[13] = rot8(v[13]);
+    v[14] = rot8(v[14]);
+    v[10] = add(v[10], v[15]);
+    v[11] = add(v[11], v[12]);
+    v[8] = add(v[8], v[13]);
+    v[9] = add(v[9], v[14]);
+    v[5] = xor(v[5], v[10]);
+    v[6] = xor(v[6], v[11]);
+    v[7] = xor(v[7], v[8]);
+    v[4] = xor(v[4], v[9]);
+    v[5] = rot7(v[5]);
+    v[6] = rot7(v[6]);
+    v[7] = rot7(v[7]);
+    v[4] = rot7(v[4]);
+}
+
+/// Performs a Blake2s compression.
+pub fn compress(
+    h_vecs: [u32; 8],
+    msg_vecs: [u32; 16],
+    count_low: u32,
+    count_high: u32,
+    lastblock: u32,
+    lastnode: u32,
+) -> [u32; 8] {
+    let mut v = [
+        h_vecs[0],
+        h_vecs[1],
+        h_vecs[2],
+        h_vecs[3],
+        h_vecs[4],
+        h_vecs[5],
+        h_vecs[6],
+        h_vecs[7],
+        IV[0],
+        IV[1],
+        IV[2],
+        IV[3],
+        xor(IV[4], count_low),
+        xor(IV[5], count_high),
+        xor(IV[6], lastblock),
+        xor(IV[7], lastnode),
+    ];
+
+    round(&mut v, msg_vecs, 0);
+    round(&mut v, msg_vecs, 1);
+    round(&mut v, msg_vecs, 2);
+    round(&mut v, msg_vecs, 3);
+    round(&mut v, msg_vecs, 4);
+    round(&mut v, msg_vecs, 5);
+    round(&mut v, msg_vecs, 6);
+    round(&mut v, msg_vecs, 7);
+    round(&mut v, msg_vecs, 8);
+    round(&mut v, msg_vecs, 9);
+
+    [
+        xor(xor(h_vecs[0], v[0]), v[8]),
+        xor(xor(h_vecs[1], v[1]), v[9]),
+        xor(xor(h_vecs[2], v[2]), v[10]),
+        xor(xor(h_vecs[3], v[3]), v[11]),
+        xor(xor(h_vecs[4], v[4]), v[12]),
+        xor(xor(h_vecs[5], v[5]), v[13]),
+        xor(xor(h_vecs[6], v[6]), v[14]),
+        xor(xor(h_vecs[7], v[7]), v[15]),
+    ]
+}
--- src/commitment_scheme/mod.rs
+++ src/commitment_scheme/mod.rs
@@ -1,4 +1,7 @@
 pub mod blake2_hash;
+#[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
+pub mod blake2s_avx;
+pub mod blake2s_ref;
 pub mod blake3_hash;
 pub mod hasher;
 pub mod merkle_decommitment;
