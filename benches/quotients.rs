d4ebbc6b40ff3f39dd09f28a9e894618984278b0
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2024-03-28 16:12:16 +0200

    New AVX quotients (#549)

---
+++ benches/quotients.rs
@@ -0,0 +1,90 @@
+#![feature(iter_array_chunks)]
+
+use criterion::{black_box, Criterion};
+use itertools::Itertools;
+use stwo::core::backend::CPUBackend;
+use stwo::core::circle::SECURE_FIELD_CIRCLE_GEN;
+use stwo::core::commitment_scheme::quotients::{ColumnSampleBatch, QuotientOps};
+use stwo::core::fields::m31::BaseField;
+use stwo::core::fields::qm31::SecureField;
+use stwo::core::poly::circle::{CanonicCoset, CircleEvaluation};
+use stwo::core::poly::BitReversedOrder;
+
+pub fn cpu_quotients(c: &mut criterion::Criterion) {
+    const LOG_SIZE: u32 = 16;
+    const SIZE: usize = 1 << LOG_SIZE;
+    const N_COLS: usize = 1 << 8;
+    let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
+    let cols = (0..N_COLS)
+        .map(|_| {
+            let values = (0..SIZE).map(BaseField::from).collect();
+            CircleEvaluation::<CPUBackend, _, BitReversedOrder>::new(domain, values)
+        })
+        .collect_vec();
+    let random_coeff = SecureField::from_u32_unchecked(0, 1, 2, 3);
+    let a = SecureField::from_u32_unchecked(5, 6, 7, 8);
+    let samples = vec![ColumnSampleBatch {
+        point: SECURE_FIELD_CIRCLE_GEN,
+        columns_and_values: (0..N_COLS).map(|i| (i, a)).collect(),
+    }];
+
+    let col_refs = &cols.iter().collect_vec();
+    c.bench_function("cpu quotients 2^8 x 2^16", |b| {
+        b.iter(|| {
+            black_box(CPUBackend::accumulate_quotients(
+                black_box(domain),
+                black_box(col_refs),
+                black_box(random_coeff),
+                black_box(&samples),
+            ))
+        })
+    });
+}
+
+#[cfg(target_arch = "x86_64")]
+pub fn avx512_quotients(c: &mut criterion::Criterion) {
+    use stwo::core::backend::avx512::AVX512Backend;
+
+    const LOG_SIZE: u32 = 20;
+    const SIZE: usize = 1 << LOG_SIZE;
+    const N_COLS: usize = 1 << 8;
+    let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
+    let cols = (0..N_COLS)
+        .map(|_| {
+            let values = (0..SIZE as u32)
+                .map(BaseField::from_u32_unchecked)
+                .collect();
+            CircleEvaluation::<AVX512Backend, _, BitReversedOrder>::new(domain, values)
+        })
+        .collect_vec();
+    let random_coeff = SecureField::from_m31_array(std::array::from_fn(BaseField::from));
+    let a = SecureField::from_m31_array(std::array::from_fn(|i| BaseField::from(3 * i)));
+    let samples = vec![ColumnSampleBatch {
+        point: SECURE_FIELD_CIRCLE_GEN,
+        columns_and_values: (0..N_COLS).map(|i| (i, a)).collect(),
+    }];
+
+    let col_refs = &cols.iter().collect_vec();
+    c.bench_function("avx quotients 2^8 x 2^20", |b| {
+        b.iter(|| {
+            black_box(AVX512Backend::accumulate_quotients(
+                black_box(domain),
+                black_box(col_refs),
+                black_box(random_coeff),
+                black_box(&samples),
+            ))
+        })
+    });
+}
+
+#[cfg(target_arch = "x86_64")]
+criterion::criterion_group!(
+    name=quotients;
+    config = Criterion::default().sample_size(10);
+    targets=avx512_quotients, cpu_quotients);
+#[cfg(not(target_arch = "x86_64"))]
+criterion::criterion_group!(
+    name=quotients;
+    config = Criterion::default().sample_size(10);
+    targets=cpu_quotients);
+criterion::criterion_main!(quotients);
--- src/core/backend/avx512/m31.rs
+++ src/core/backend/avx512/m31.rs
@@ -2,11 +2,14 @@ use core::arch::x86_64::{
     __m512i, _mm512_add_epi32, _mm512_min_epu32, _mm512_mul_epu32, _mm512_srli_epi64,
     _mm512_sub_epi32,
 };
-use std::arch::x86_64::{_mm512_load_epi32, _mm512_permutex2var_epi32, _mm512_store_epi32};
+use std::arch::x86_64::{
+    _mm512_load_epi32, _mm512_permutex2var_epi32, _mm512_set1_epi32, _mm512_setzero_si512,
+    _mm512_store_epi32,
+};
 use std::fmt::Display;
 use std::ops::{Add, AddAssign, Mul, MulAssign, Neg, Sub, SubAssign};
 
-use num_traits::One;
+use num_traits::{One, Zero};
 
 use super::tranpose_utils::{
     EVENS_CONCAT_EVENS, HHALF_INTERLEAVE_HHALF, LHALF_INTERLEAVE_LHALF, ODDS_CONCAT_ODDS,
@@ -24,6 +27,10 @@ pub const M512P: __m512i = unsafe { core::mem::transmute([P; K_BLOCK_SIZE]) };
 pub struct PackedBaseField(pub __m512i);
 
 impl PackedBaseField {
+    pub fn broadcast(value: M31) -> Self {
+        Self(unsafe { _mm512_set1_epi32(value.0 as i32) })
+    }
+
     pub fn from_array(v: [M31; K_BLOCK_SIZE]) -> PackedBaseField {
         unsafe { Self(std::mem::transmute(v)) }
     }
@@ -80,10 +87,6 @@ impl PackedBaseField {
     /// Sums all the elements in the packed M31 element.
     pub fn pointwise_sum(self) -> M31 {
         self.to_array().into_iter().sum()
-    }
-
-    pub fn broadcast(x: M31) -> Self {
-        Self(unsafe { std::arch::x86_64::_mm512_set1_epi32(x.0 as i32) })
     }
 }
 
@@ -234,6 +237,15 @@ impl SubAssign for PackedBaseField {
     }
 }
 
+impl Zero for PackedBaseField {
+    fn zero() -> Self {
+        Self(unsafe { _mm512_setzero_si512() })
+    }
+    fn is_zero(&self) -> bool {
+        self.to_array().iter().all(|x| x.is_zero())
+    }
+}
+
 impl One for PackedBaseField {
     fn one() -> Self {
         Self(unsafe { core::mem::transmute([M31::one(); K_BLOCK_SIZE]) })
@@ -242,6 +254,7 @@ impl One for PackedBaseField {
 
 impl FieldExpOps for PackedBaseField {
     fn inverse(&self) -> Self {
+        assert!(!self.is_zero(), "0 has no inverse");
         self.pow((P - 2) as u128)
     }
 }
