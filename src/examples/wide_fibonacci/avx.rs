d0f297b1bf5d2339b023827c664c9923946b1393
Alon Haramati <alonh@starkware.co>
2024-04-02 13:10:13 +0300

    Configure wide fib.

--- src/examples/wide_fibonacci/avx.rs
+++ src/examples/wide_fibonacci/avx.rs
@@ -15,8 +15,7 @@ use crate::core::fields::{FieldExpOps, FieldOps};
 use crate::core::poly::circle::{CanonicCoset, CircleEvaluation};
 use crate::core::poly::BitReversedOrder;
 use crate::core::ColumnVec;
-
-const N_COLS: usize = 1 << 8;
+use crate::examples::wide_fibonacci::structs::N_COLUMNS;
 
 pub struct WideFibAir {
     component: WideFibComponent,
@@ -36,7 +35,7 @@ pub fn gen_trace(
     log_size: usize,
 ) -> ColumnVec<CircleEvaluation<AVX512Backend, BaseField, BitReversedOrder>> {
     assert!(log_size >= VECS_LOG_SIZE);
-    let mut trace = (0..N_COLS)
+    let mut trace = (0..N_COLUMNS)
         .map(|_| Col::<AVX512Backend, BaseField>::zeros(1 << log_size))
         .collect_vec();
     for vec_index in 0..(1 << (log_size - VECS_LOG_SIZE)) {
@@ -60,15 +59,15 @@ pub fn gen_trace(
 
 impl Component<AVX512Backend> for WideFibComponent {
     fn n_constraints(&self) -> usize {
-        N_COLS - 1
+        N_COLUMNS - 1
     }
 
     fn max_constraint_log_degree_bound(&self) -> u32 {
-        self.log_size + 1
+        self.log_column_size() + 1
     }
 
     fn trace_log_degree_bounds(&self) -> Vec<u32> {
-        vec![self.log_size; N_COLS]
+        vec![self.log_column_size(); N_COLUMNS]
     }
 
     fn evaluate_constraint_quotients_on_domain(
@@ -76,9 +75,9 @@ impl Component<AVX512Backend> for WideFibComponent {
         trace: &ComponentTrace<'_, AVX512Backend>,
         evaluation_accumulator: &mut DomainEvaluationAccumulator<AVX512Backend>,
     ) {
-        assert_eq!(trace.columns.len(), N_COLS);
+        assert_eq!(trace.columns.len(), N_COLUMNS);
         // TODO(spapini): Steal evaluation from commitment.
-        let eval_domain = CanonicCoset::new(self.log_size + 1).circle_domain();
+        let eval_domain = CanonicCoset::new(self.log_column_size() + 1).circle_domain();
         let trace_eval = trace
             .columns
             .iter()
@@ -87,15 +86,16 @@ impl Component<AVX512Backend> for WideFibComponent {
 
         // Denoms.
         // TODO(spapini): Make this prettier.
-        let zero_domain = CanonicCoset::new(self.log_size).coset;
+        let zero_domain = CanonicCoset::new(self.log_column_size()).coset;
         let mut denoms =
             BaseFieldVec::from_iter(eval_domain.iter().map(|p| coset_vanishing(zero_domain, p)));
         <AVX512Backend as ColumnOps<BaseField>>::bit_reverse_column(&mut denoms);
         let mut denom_inverses = BaseFieldVec::zeros(denoms.len());
         <AVX512Backend as FieldOps<BaseField>>::batch_inverse(&denoms, &mut denom_inverses);
 
-        let constraint_log_degree_bound = self.log_size + 1;
-        let [accum] = evaluation_accumulator.columns([(constraint_log_degree_bound, N_COLS - 1)]);
+        let constraint_log_degree_bound = self.log_column_size() + 1;
+        let [accum] =
+            evaluation_accumulator.columns([(constraint_log_degree_bound, N_COLUMNS - 1)]);
 
         for vec_row in 0..(1 << (eval_domain.log_size() - VECS_LOG_SIZE as u32)) {
             // Numerator.
@@ -106,16 +106,16 @@ impl Component<AVX512Backend> for WideFibComponent {
                     PackedBaseField::zero(),
                     PackedBaseField::zero(),
                     PackedBaseField::zero(),
-                ]) * PackedSecureField::broadcast(accum.random_coeff_powers[N_COLS - 2]);
+                ]) * PackedSecureField::broadcast(accum.random_coeff_powers[N_COLUMNS - 2]);
 
             let mut a_sq = a.square();
             let mut b_sq = trace_eval[1].data[vec_row].square();
             #[allow(clippy::needless_range_loop)]
-            for i in 0..(N_COLS - 2) {
+            for i in 0..(N_COLUMNS - 2) {
                 unsafe {
                     let c = *trace_eval.get_unchecked(i + 2).data.get_unchecked(vec_row);
                     row_res +=
-                        PackedSecureField::broadcast(accum.random_coeff_powers[N_COLS - 3 - i])
+                        PackedSecureField::broadcast(accum.random_coeff_powers[N_COLUMNS - 3 - i])
                             * (a_sq + b_sq - c);
                     (a_sq, b_sq) = (b_sq, c.square());
                 }
@@ -132,7 +132,7 @@ impl Component<AVX512Backend> for WideFibComponent {
         &self,
         point: CirclePoint<SecureField>,
     ) -> ColumnVec<Vec<CirclePoint<SecureField>>> {
-        let mask = Mask(vec![vec![0_usize]; 256]);
+        let mask = Mask(vec![vec![0_usize]; N_COLUMNS]);
         mask.iter()
             .map(|col| col.iter().map(|_| point).collect())
             .collect()
@@ -144,10 +144,10 @@ impl Component<AVX512Backend> for WideFibComponent {
         mask: &ColumnVec<Vec<SecureField>>,
         evaluation_accumulator: &mut PointEvaluationAccumulator,
     ) {
-        let zero_domain = CanonicCoset::new(self.log_size).coset;
+        let zero_domain = CanonicCoset::new(self.log_column_size()).coset;
         let denominator = coset_vanishing(zero_domain, point);
         evaluation_accumulator.accumulate((mask[0][0] - SecureField::one()) / denominator);
-        for i in 0..(N_COLS - 2) {
+        for i in 0..(N_COLUMNS - 2) {
             let numerator = mask[i][0].square() + mask[i + 1][0].square() - mask[i + 2][0];
             evaluation_accumulator.accumulate(numerator / denominator);
         }
@@ -164,15 +164,18 @@ mod tests {
     use crate::core::fields::IntoSlice;
     use crate::core::prover::{prove, verify};
     use crate::examples::wide_fibonacci::avx::{gen_trace, WideFibAir};
-    use crate::examples::wide_fibonacci::structs::WideFibComponent;
+    use crate::examples::wide_fibonacci::structs::{WideFibComponent, LOG_N_COLUMNS};
 
     #[test]
     fn test_avx_wide_fib_prove() {
         // Note: For benchmarks, increase to 17, to get 128MB of trace.
-        const LOG_SIZE: u32 = 12;
-        let component = WideFibComponent { log_size: LOG_SIZE };
+        const LOG_N_ROWS: u32 = 12;
+        let component = WideFibComponent {
+            log_fibonacci_size: LOG_N_COLUMNS as u32,
+            log_n_instances: LOG_N_ROWS,
+        };
         let air = WideFibAir { component };
-        let trace = gen_trace(LOG_SIZE as usize);
+        let trace = gen_trace(LOG_N_ROWS as usize);
         let channel = &mut Blake2sChannel::new(Blake2sHasher::hash(BaseField::into_slice(&[])));
         let proof = prove(&air, channel, trace).unwrap();
 
--- src/examples/wide_fibonacci/mod.rs
+++ src/examples/wide_fibonacci/mod.rs
@@ -8,11 +8,10 @@ pub mod trace_gen;
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
-    use num_traits::{One, Zero};
+    use num_traits::Zero;
 
-    use super::structs::{Input, WideFibComponent};
+    use super::structs::{Input, WideFibComponent, LOG_N_COLUMNS};
     use super::trace_asserts::assert_constraints_on_row;
-    use super::trace_gen::write_trace_row;
     use crate::core::air::accumulation::DomainEvaluationAccumulator;
     use crate::core::air::{Component, ComponentTrace};
     use crate::core::backend::cpu::CPUCircleEvaluation;
@@ -20,46 +19,48 @@ mod tests {
     use crate::core::fields::m31::BaseField;
     use crate::core::fields::qm31::QM31;
     use crate::core::poly::circle::CanonicCoset;
-
-    fn fill_trace(private_input: &[Input]) -> Vec<Vec<BaseField>> {
-        let zero_vec = vec![BaseField::zero(); private_input.len()];
-        let mut dst = vec![zero_vec; 64];
-        for (offset, input) in private_input.iter().enumerate() {
-            write_trace_row(&mut dst, input, offset);
-        }
-        dst
-    }
+    use crate::m31;
 
     #[test]
     fn test_wide_fib_trace() {
+        let wide_fib = WideFibComponent {
+            log_fibonacci_size: LOG_N_COLUMNS as u32,
+            log_n_instances: 1,
+        };
         let input = Input {
-            a: BaseField::from_u32_unchecked(0x76),
-            b: BaseField::from_u32_unchecked(0x483),
+            a: m31!(0x76),
+            b: m31!(0x483),
         };
 
-        let trace = fill_trace(&[input]);
-        let flat_trace = trace.into_iter().flatten().collect_vec();
-        assert_constraints_on_row(&flat_trace);
+        let trace = wide_fib.fill_initial_trace(vec![input, input]);
+        let trace0 = trace.iter().map(|col| col[0]).collect_vec();
+        let trace1 = trace.iter().map(|col| col[1]).collect_vec();
+
+        assert_constraints_on_row(&trace0);
+        assert_constraints_on_row(&trace1);
     }
 
     #[test]
     fn test_wide_fib_constraints() {
-        let wide_fib = WideFibComponent { log_size: 7 };
+        let wide_fib = WideFibComponent {
+            log_fibonacci_size: LOG_N_COLUMNS as u32,
+            log_n_instances: 7,
+        };
         let mut acc = DomainEvaluationAccumulator::new(
             QM31::from_u32_unchecked(1, 2, 3, 4),
-            wide_fib.log_size + 1,
+            Component::<CPUBackend>::max_constraint_log_degree_bound(&wide_fib),
             Component::<CPUBackend>::n_constraints(&wide_fib),
         );
-        let inputs = (0..1 << wide_fib.log_size)
+        let inputs = (0..1 << wide_fib.log_n_instances)
             .map(|i| Input {
-                a: BaseField::one(),
-                b: BaseField::from_u32_unchecked(i as u32),
+                a: m31!(1),
+                b: m31!(i as u32),
             })
             .collect_vec();
 
-        let trace = fill_trace(&inputs);
+        let trace = wide_fib.fill_initial_trace(inputs);
 
-        let trace_domain = CanonicCoset::new(wide_fib.log_size);
+        let trace_domain = CanonicCoset::new(wide_fib.log_column_size());
         let trace = trace
             .into_iter()
             .map(|col| CPUCircleEvaluation::new_canonical_ordered(trace_domain, col))
