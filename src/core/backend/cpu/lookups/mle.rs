1da916e54f4202338cda2ef1a0b18746b7c58892
Andrew Milson <andrew.j.milson@gmail.com>
2024-04-04 05:25:31 +0000

    Add multilinear extension

---
+++ src/core/backend/cpu/lookups/mle.rs
@@ -0,0 +1,38 @@
+use std::iter::zip;
+
+use crate::core::backend::CPUBackend;
+use crate::core::fields::m31::BaseField;
+use crate::core::fields::qm31::SecureField;
+use crate::core::lookups::mle::{Mle, MleOps};
+
+impl MleOps<BaseField> for CPUBackend {
+    fn fix_first(mle: Mle<Self, BaseField>, assignment: SecureField) -> Mle<Self, SecureField> {
+        let midpoint = mle.len() / 2;
+        let (lhs_evals, rhs_evals) = mle.split_at(midpoint);
+
+        let res = zip(lhs_evals, rhs_evals)
+            // Equivalent to `eq(0, assignment) * lhs_eval + eq(1, assignment) * rhs_eval`.
+            .map(|(&lhs_eval, &rhs_eval)| assignment * (rhs_eval - lhs_eval) + lhs_eval)
+            .collect();
+
+        Mle::new(res)
+    }
+}
+
+impl MleOps<SecureField> for CPUBackend {
+    fn fix_first(mle: Mle<Self, SecureField>, assignment: SecureField) -> Mle<Self, SecureField> {
+        let midpoint = mle.len() / 2;
+        let mut evals = mle.into_evals();
+
+        for i in 0..midpoint {
+            let lhs_eval = evals[i];
+            let rhs_eval = evals[i + midpoint];
+            // Equivalent to `eq(0, assignment) * lhs_eval + eq(1, assignment) * rhs_eval`.
+            evals[i] = lhs_eval + assignment * (rhs_eval - lhs_eval);
+        }
+
+        evals.truncate(midpoint);
+
+        Mle::new(evals)
+    }
+}
--- src/core/backend/cpu/mod.rs
+++ src/core/backend/cpu/mod.rs
@@ -2,12 +2,14 @@ mod accumulation;
 mod blake2s;
 mod circle;
 mod fri;
+mod lookups;
 pub mod quotients;
 
 use std::fmt::Debug;
 
 use super::{Backend, Column, ColumnOps, FieldOps};
 use crate::core::fields::Field;
+use crate::core::lookups::mle::Mle;
 use crate::core::poly::circle::{CircleEvaluation, CirclePoly};
 use crate::core::utils::bit_reverse;
 
@@ -49,6 +51,7 @@ impl<T: Debug + Clone + Default> Column<T> for Vec<T> {
 
 pub type CPUCirclePoly = CirclePoly<CPUBackend>;
 pub type CPUCircleEvaluation<F, EvalOrder> = CircleEvaluation<CPUBackend, F, EvalOrder>;
+pub type CPUMle<F> = Mle<CPUBackend, F>;
 
 #[cfg(test)]
 mod tests {
