439eaf3b6fd41395dbbc0cf5166296b299a0a944
Andrew Milson <andrew.j.milson@gmail.com>
2024-04-04 10:38:43 +0000

    Add sumcheck prover and verifier

--- src/core/lookups/mle.rs
+++ src/core/lookups/mle.rs
@@ -9,7 +9,7 @@ use crate::core::fields::Field;
 /// TODO
 pub trait MleOps<F: Field>: ColumnOps<F> + Sized {
     /// Returns a transformed [`Mle`] where the first variable is fixed to `assignment`.
-    fn fix_first(mle: Mle<Self, F>, assignment: SecureField) -> Mle<Self, SecureField>
+    fn fix_first_variable(mle: Mle<Self, F>, assignment: SecureField) -> Mle<Self, SecureField>
     where
         Self: MleOps<SecureField>;
 }
@@ -38,11 +38,11 @@ impl<B: MleOps<F>, F: Field> Mle<B, F> {
     }
 
     /// Returns a transformed polynomial where the first variable is fixed to `assignment`.
-    pub fn fix_first(self, assignment: SecureField) -> Mle<B, SecureField>
+    pub fn fix_first_variable(self, assignment: SecureField) -> Mle<B, SecureField>
     where
         B: MleOps<SecureField>,
     {
-        B::fix_first(self, assignment)
+        B::fix_first_variable(self, assignment)
     }
 
     /// Returns the number of variables in the polynomial.
@@ -56,5 +56,46 @@ impl<B: ColumnOps<F>, F: Field> Deref for Mle<B, F> {
 
     fn deref(&self) -> &Col<B, F> {
         &self.evals
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::{Mle, MleOps};
+    use crate::core::backend::Column;
+    use crate::core::fields::qm31::SecureField;
+    use crate::core::fields::{ExtensionOf, Field};
+
+    impl<B, F> Mle<B, F>
+    where
+        F: Field,
+        SecureField: ExtensionOf<F>,
+        B: MleOps<F>,
+    {
+        /// Evaluates the multilinear polynomial at `point`.
+        pub(crate) fn eval_at_point(self, point: &[SecureField]) -> SecureField {
+            pub fn eval(mle_evals: &[SecureField], p: &[SecureField]) -> SecureField {
+                match p {
+                    [] => mle_evals[0],
+                    &[p_i, ref p @ ..] => {
+                        let (lhs, rhs) = mle_evals.split_at(mle_evals.len() / 2);
+                        let lhs_eval = eval(lhs, p);
+                        let rhs_eval = eval(rhs, p);
+                        // Equivalent to `eq(0, p_i) * lhs_eval + eq(1, p_i) * rhs_eval`.
+                        p_i * (rhs_eval - lhs_eval) + lhs_eval
+                    }
+                }
+            }
+
+            let mle_evals = self
+                .clone()
+                .into_evals()
+                .to_cpu()
+                .into_iter()
+                .map(|v| v.into())
+                .collect::<Vec<_>>();
+
+            eval(&mle_evals, point)
+        }
     }
 }
