1803274f01d3b771cd33f362078328c097a86d17
Andrew Milson <andrew.j.milson@gmail.com>
2024-04-04 10:38:43 +0000

    Add sumcheck prover and verifier

--- src/core/backend/cpu/lookups/mle.rs
+++ src/core/backend/cpu/lookups/mle.rs
@@ -1,12 +1,19 @@
 use std::iter::zip;
 
+use num_traits::{One, Zero};
+
 use crate::core::backend::CPUBackend;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
 use crate::core::lookups::mle::{Mle, MleOps};
+use crate::core::lookups::sumcheck::MultivariatePolyOracle;
+use crate::core::lookups::utils::UnivariatePoly;
 
 impl MleOps<BaseField> for CPUBackend {
-    fn fix_first(mle: Mle<Self, BaseField>, assignment: SecureField) -> Mle<Self, SecureField> {
+    fn fix_first_variable(
+        mle: Mle<Self, BaseField>,
+        assignment: SecureField,
+    ) -> Mle<Self, SecureField> {
         let midpoint = mle.len() / 2;
         let (lhs_evals, rhs_evals) = mle.split_at(midpoint);
 
@@ -20,7 +27,10 @@ impl MleOps<BaseField> for CPUBackend {
 }
 
 impl MleOps<SecureField> for CPUBackend {
-    fn fix_first(mle: Mle<Self, SecureField>, assignment: SecureField) -> Mle<Self, SecureField> {
+    fn fix_first_variable(
+        mle: Mle<Self, SecureField>,
+        assignment: SecureField,
+    ) -> Mle<Self, SecureField> {
         let midpoint = mle.len() / 2;
         let mut evals = mle.into_evals();
 
@@ -34,5 +44,25 @@ impl MleOps<SecureField> for CPUBackend {
         evals.truncate(midpoint);
 
         Mle::new(evals)
+    }
+}
+
+impl MultivariatePolyOracle for Mle<CPUBackend, SecureField> {
+    fn n_variables(&self) -> usize {
+        self.n_variables()
+    }
+
+    fn sum_as_poly_in_first_variable(&self, claim: SecureField) -> UnivariatePoly<SecureField> {
+        let x0 = SecureField::zero();
+        let x1 = SecureField::one();
+
+        let y0 = self[0..self.len() / 2].iter().sum();
+        let y1 = claim - y0;
+
+        UnivariatePoly::interpolate_lagrange(&[x0, x1], &[y0, y1])
+    }
+
+    fn fix_first_variable(self, challenge: SecureField) -> Self {
+        self.fix_first_variable(challenge)
     }
 }
