288122c33375cb52068be6276f348be2948b159f
Alon Haramati <alonh@starkware.co>
2024-04-09 12:43:25 +0300

    Get lookup columns.

--- crates/prover/src/examples/wide_fibonacci/trace_gen.rs
+++ crates/prover/src/examples/wide_fibonacci/trace_gen.rs
@@ -1,3 +1,5 @@
+use num_traits::One;
+
 use super::component::{Input, N_COLUMNS};
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::FieldExpOps;
@@ -17,4 +19,20 @@ pub fn write_trace_row(
     }
 
     (dst[N_COLUMNS - 2][row_index], dst[N_COLUMNS - 1][row_index])
+}
+
+pub fn write_lookup_column(
+    dst: &mut [BaseField],
+    input_trace: &[Vec<BaseField>],
+    column_offset: usize,
+    alpha: BaseField,
+    z: BaseField,
+) {
+    let mut prev_value = BaseField::one();
+    for (i, cell) in dst.iter_mut().enumerate() {
+        let row_i_0 = input_trace[column_offset][i];
+        let row_i_1 = input_trace[column_offset + 1][i];
+        *cell = (row_i_0 + alpha * row_i_1 - z) * prev_value;
+        prev_value = *cell;
+    }
 }
