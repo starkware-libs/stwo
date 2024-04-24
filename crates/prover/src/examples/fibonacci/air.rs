355f2e5df78c7585abd24e082f27e3f864923bf0
Shahar Papini <43779613+spapinistarkware@users.noreply.github.com>
2024-04-24 10:30:39 +0300

    Seperate Component trait
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/stwo/580)
    <!-- Reviewable:end -->

--- crates/prover/src/examples/fibonacci/air.rs
+++ crates/prover/src/examples/fibonacci/air.rs
@@ -1,7 +1,7 @@
 use itertools::{zip_eq, Itertools};
 
 use super::component::FibonacciComponent;
-use crate::core::air::{Air, Component};
+use crate::core::air::{Air, AirProver, Component, ComponentProver};
 use crate::core::backend::CPUBackend;
 use crate::core::fields::m31::BaseField;
 
@@ -14,9 +14,13 @@ impl FibonacciAir {
         Self { component }
     }
 }
-
-impl Air<CPUBackend> for FibonacciAir {
-    fn components(&self) -> Vec<&dyn Component<CPUBackend>> {
+impl Air for FibonacciAir {
+    fn components(&self) -> Vec<&dyn Component> {
+        vec![&self.component]
+    }
+}
+impl AirProver<CPUBackend> for FibonacciAir {
+    fn prover_components(&self) -> Vec<&dyn ComponentProver<CPUBackend>> {
         vec![&self.component]
     }
 }
@@ -24,7 +28,6 @@ impl Air<CPUBackend> for FibonacciAir {
 pub struct MultiFibonacciAir {
     pub components: Vec<FibonacciComponent>,
 }
-
 impl MultiFibonacciAir {
     pub fn new(log_sizes: &[u32], claim: &[BaseField]) -> Self {
         let mut components = Vec::new();
@@ -34,12 +37,19 @@ impl MultiFibonacciAir {
         Self { components }
     }
 }
-
-impl Air<CPUBackend> for MultiFibonacciAir {
-    fn components(&self) -> Vec<&dyn Component<CPUBackend>> {
+impl Air for MultiFibonacciAir {
+    fn components(&self) -> Vec<&dyn Component> {
         self.components
             .iter()
-            .map(|c| c as &dyn Component<CPUBackend>)
+            .map(|c| c as &dyn Component)
+            .collect_vec()
+    }
+}
+impl AirProver<CPUBackend> for MultiFibonacciAir {
+    fn prover_components(&self) -> Vec<&dyn ComponentProver<CPUBackend>> {
+        self.components
+            .iter()
+            .map(|c| c as &dyn ComponentProver<CPUBackend>)
             .collect_vec()
     }
 }
