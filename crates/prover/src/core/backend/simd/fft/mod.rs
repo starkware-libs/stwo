3b382132ba1513985681c11ce0cdf6543969f208
Andrew Milson <andrew.j.milson@gmail.com>
2024-05-01 00:03:21 -0400

    Add FFT implementation for SIMD backend

--- crates/prover/src/core/backend/simd/fft/mod.rs
+++ crates/prover/src/core/backend/simd/fft/mod.rs
@@ -1,4 +1,3 @@
-use std::mem::transmute;
 use std::simd::{simd_swizzle, u32x16, u32x8};
 
 use super::m31::PackedBaseField;
@@ -7,76 +6,54 @@ use crate::core::fields::m31::P;
 pub mod ifft;
 pub mod rfft;
 
-/// An input to _mm512_permutex2var_epi32, and is used to interleave the even words of a
-/// with the even words of b.
-const _EVENS_INTERLEAVE_EVENS: u32x16 = unsafe {
-    core::mem::transmute([
-        0b00000, 0b10000, 0b00010, 0b10010, 0b00100, 0b10100, 0b00110, 0b10110, 0b01000, 0b11000,
-        0b01010, 0b11010, 0b01100, 0b11100, 0b01110, 0b11110,
-    ])
-};
-/// An input to _mm512_permutex2var_epi32, and is used to interleave the odd words of a
-/// with the odd words of b.
-const _ODDS_INTERLEAVE_ODDS: u32x16 = unsafe {
-    core::mem::transmute([
-        0b00001, 0b10001, 0b00011, 0b10011, 0b00101, 0b10101, 0b00111, 0b10111, 0b01001, 0b11001,
-        0b01011, 0b11011, 0b01101, 0b11101, 0b01111, 0b11111,
-    ])
-};
-
 pub const CACHED_FFT_LOG_SIZE: usize = 16;
+
 pub const MIN_FFT_LOG_SIZE: usize = 5;
 
 // TODO(spapini): FFTs return a redundant representation, that can get the value P. need to reduce
 // it somewhere.
 
-/// Transposes the AVX vectors in the given array.
+/// Transposes the SIMD vectors in the given array.
+///
 /// Swaps the bit index abc <-> cba, where |a|=|c| and |b| = 0 or 1, according to the parity of
 /// `log_n_vecs`.
 /// When log_n_vecs is odd, transforms the index abc <-> cba, w
 ///
-/// # Safety
-/// This function is unsafe because it takes a raw pointer to i32 values.
-/// `values` must be aligned to 64 bytes.
-///
 /// # Arguments
-/// * `values`: A mutable pointer to the values that are to be transposed.
-/// * `log_n_vecs`: The log of the number of AVX vectors in the `values` array.
-pub unsafe fn transpose_vecs(values: *mut i32, log_n_vecs: usize) {
+///
+/// - `values`: A mutable pointer to the values that are to be transposed.
+/// - `log_n_vecs`: The log of the number of SIMD vectors in the `values` array.
+///
+/// # Safety
+///
+/// Behavior is undefined if `values` does not have the same alignment as [`u32x16`].
+pub unsafe fn transpose_vecs(values: *mut u32, log_n_vecs: usize) {
     let half = log_n_vecs / 2;
-    for b in 0..(1 << (log_n_vecs & 1)) {
-        for a in 0..(1 << half) {
-            for c in 0..(1 << half) {
+    for b in 0..1 << (log_n_vecs & 1) {
+        for a in 0..1 << half {
+            for c in 0..1 << half {
                 let i = (a << (log_n_vecs - half)) | (b << half) | c;
                 let j = (c << (log_n_vecs - half)) | (b << half) | a;
                 if i >= j {
                     continue;
                 }
-                let val0 = _mm512_load_epi32(values.add(i << 4).cast_const());
-                let val1 = _mm512_load_epi32(values.add(j << 4).cast_const());
-                _mm512_store_epi32(values.add(i << 4), val1);
-                _mm512_store_epi32(values.add(j << 4), val0);
+                let val0 = load(values.add(i << 4).cast_const());
+                let val1 = load(values.add(j << 4).cast_const());
+                store(values.add(i << 4), val1);
+                store(values.add(j << 4), val0);
             }
         }
     }
 }
 
-unsafe fn _mm512_load_epi32(mem_addr: *const i32) -> u32x16 {
-    std::ptr::read(mem_addr as *const u32x16)
-}
-
-unsafe fn _mm512_store_epi32(mem_addr: *mut i32, a: u32x16) {
-    std::ptr::write(mem_addr as *mut u32x16, a);
-}
-
-/// Computes the twiddles for the first fft layer from the second, and loads both to AVX registers.
+/// Computes the twiddles for the first fft layer from the second, and loads both to SIMD registers.
+///
 /// Returns the twiddles for the first layer and the twiddles for the second layer.
-/// # Safety
-pub unsafe fn compute_first_twiddles(twiddle1_dbl: [i32; 8]) -> (u32x16, u32x16) {
+fn compute_first_twiddles(twiddle1_dbl: u32x8) -> (u32x16, u32x16) {
     // Start by loading the twiddles for the second layer (layer 1):
     let t1 = simd_swizzle!(
-        unsafe { transmute::<_, u32x8>(twiddle1_dbl) },
-        unsafe { transmute::<_, u32x8>(twiddle1_dbl) },
+        twiddle1_dbl,
+        twiddle1_dbl,
         [0, 1, 2, 3, 4, 5, 6, 7, 0, 1, 2, 3, 4, 5, 6, 7]
     );
 
@@ -108,6 +85,16 @@ pub unsafe fn compute_first_twiddles(twiddle1_dbl: [i32; 8]) -> (u32x16, u32x16)
         ]
     ) ^ NEGATION_MASK;
     (t0, t1)
+}
+
+#[inline]
+unsafe fn load(mem_addr: *const u32) -> u32x16 {
+    std::ptr::read(mem_addr as *const u32x16)
+}
+
+#[inline]
+unsafe fn store(mem_addr: *mut u32, a: u32x16) {
+    std::ptr::write(mem_addr as *mut u32x16, a);
 }
 
 /// Computes `v * twiddle`
