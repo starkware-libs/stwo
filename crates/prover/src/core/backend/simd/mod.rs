b28ed46f2b673183448279ebc9f6b011b05d922d
Andrew Milson <andrew.j.milson@gmail.com>
2024-05-02 16:23:43 -0400

    Copy QuotientOps from AVX backend

--- crates/prover/src/core/backend/simd/mod.rs
+++ crates/prover/src/core/backend/simd/mod.rs
@@ -1,3 +1,5 @@
+use super::Backend;
+
 pub mod accumulation;
 pub mod bit_reverse;
 pub mod blake2s;
@@ -8,7 +10,10 @@ pub mod fft;
 pub mod fri;
 pub mod m31;
 pub mod qm31;
+pub mod quotients;
 mod utils;
 
 #[derive(Copy, Clone, Debug)]
 pub struct SimdBackend;
+
+impl Backend for SimdBackend {}
