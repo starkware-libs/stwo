50a6982564e632dfa7923a4f81b563453806b1c8
Andrew Milson <andrew.j.milson@gmail.com>
2024-05-04 18:32:38 -0400

    Remove AVX backend

--- crates/prover/benches/fft.rs
+++ crates/prover/benches/fft.rs
@@ -3,15 +3,19 @@
 use std::hint::black_box;
 use std::mem::{size_of_val, transmute};
 
-use criterion::{BatchSize, BenchmarkId, Criterion, Throughput};
+use criterion::{criterion_group, criterion_main, BatchSize, BenchmarkId, Criterion, Throughput};
 use itertools::Itertools;
+use stwo_prover::core::backend::simd::column::BaseFieldVec;
+use stwo_prover::core::backend::simd::fft::ifft::{
+    get_itwiddle_dbls, ifft, ifft3_loop, ifft_vecwise_loop,
+};
+use stwo_prover::core::backend::simd::fft::rfft::{fft, get_twiddle_dbls};
+use stwo_prover::core::backend::simd::fft::transpose_vecs;
+use stwo_prover::core::backend::simd::m31::PackedBaseField;
 use stwo_prover::core::fields::m31::BaseField;
 use stwo_prover::core::poly::circle::CanonicCoset;
 
 pub fn simd_ifft(c: &mut Criterion) {
-    use stwo_prover::core::backend::simd::column::BaseFieldVec;
-    use stwo_prover::core::backend::simd::fft::ifft::{get_itwiddle_dbls, ifft};
-
     let mut group = c.benchmark_group("iffts");
 
     for log_size in 16..=28 {
@@ -37,12 +41,6 @@ pub fn simd_ifft(c: &mut Criterion) {
 }
 
 pub fn simd_ifft_parts(c: &mut Criterion) {
-    use stwo_prover::core::backend::simd::column::BaseFieldVec;
-    use stwo_prover::core::backend::simd::fft::ifft::{
-        get_itwiddle_dbls, ifft3_loop, ifft_vecwise_loop,
-    };
-    use stwo_prover::core::backend::simd::fft::transpose_vecs;
-
     const LOG_SIZE: u32 = 14;
 
     let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
@@ -104,10 +102,6 @@ pub fn simd_ifft_parts(c: &mut Criterion) {
 }
 
 pub fn simd_rfft(c: &mut Criterion) {
-    use stwo_prover::core::backend::simd::column::BaseFieldVec;
-    use stwo_prover::core::backend::simd::fft::rfft::{fft, get_twiddle_dbls};
-    use stwo_prover::core::backend::simd::m31::PackedBaseField;
-
     const LOG_SIZE: u32 = 20;
 
     let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
@@ -131,161 +125,8 @@ pub fn simd_rfft(c: &mut Criterion) {
     });
 }
 
-#[cfg(target_arch = "x86_64")]
-pub fn avx512_ifft(c: &mut criterion::Criterion) {
-    use stwo_prover::core::backend::avx512::fft::ifft;
-    use stwo_prover::platform;
-    if !platform::avx512_detected() {
-        return;
-    }
-
-    let mut group = c.benchmark_group("iffts");
-    for log_size in 16..=28 {
-        let (values, twiddle_dbls) = prepare_values(log_size);
-
-        group.throughput(Throughput::Bytes(
-            (std::mem::size_of::<BaseField>() as u64) << log_size,
-        ));
-        group.bench_function(BenchmarkId::new("avx ifft", log_size), |b| {
-            b.iter_batched(
-                || values.clone().data,
-                |mut values| unsafe {
-                    ifft::ifft(
-                        std::mem::transmute(values.as_mut_ptr()),
-                        &twiddle_dbls
-                            .iter()
-                            .map(|x| x.as_slice())
-                            .collect::<Vec<_>>(),
-                        log_size as usize,
-                    )
-                },
-                BatchSize::LargeInput,
-            );
-        });
-    }
-}
-
-#[cfg(target_arch = "x86_64")]
-pub fn avx512_ifft_parts(c: &mut criterion::Criterion) {
-    use stwo_prover::core::backend::avx512::fft::{ifft, transpose_vecs};
-    use stwo_prover::platform;
-    if !platform::avx512_detected() {
-        return;
-    }
-
-    let (values, twiddle_dbls) = prepare_values(14);
-    let mut group = c.benchmark_group("ifft parts");
-
-    // Note: These benchmarks run only on 2^14 elements ebcause of their parameters.
-    // Increasing the figure above won't change the runtime of these benchmarks.
-    group.throughput(Throughput::Bytes(4 << 14));
-    group.bench_function("avx ifft_vecwise_loop 2^14", |b| {
-        b.iter_batched(
-            || values.clone().data,
-            |mut values| unsafe {
-                ifft::ifft_vecwise_loop(
-                    std::mem::transmute(values.as_mut_ptr()),
-                    &twiddle_dbls
-                        .iter()
-                        .map(|x| x.as_slice())
-                        .collect::<Vec<_>>(),
-                    9,
-                    0,
-                )
-            },
-            BatchSize::LargeInput,
-        );
-    });
-
-    group.bench_function("avx ifft3_loop 2^14", |b| {
-        b.iter_batched(
-            || values.clone().data,
-            |mut values| unsafe {
-                ifft::ifft3_loop(
-                    std::mem::transmute(values.as_mut_ptr()),
-                    &twiddle_dbls
-                        .iter()
-                        .skip(3)
-                        .map(|x| x.as_slice())
-                        .collect::<Vec<_>>(),
-                    7,
-                    4,
-                    0,
-                )
-            },
-            BatchSize::LargeInput,
-        );
-    });
-
-    let (values, _twiddle_dbls) = prepare_values(20);
-    group.throughput(Throughput::Bytes(4 << 20));
-    group.bench_function("avx transpose_vecs 2^20", |b| {
-        b.iter_batched(
-            || values.clone().data,
-            |mut values| unsafe {
-                transpose_vecs(std::mem::transmute(values.as_mut_ptr()), (20 - 4) as usize);
-            },
-            BatchSize::LargeInput,
-        );
-    });
-}
-
-#[cfg(target_arch = "x86_64")]
-pub fn avx512_rfft(c: &mut criterion::Criterion) {
-    use stwo_prover::core::backend::avx512::fft::rfft;
-    use stwo_prover::core::backend::avx512::PackedBaseField;
-    use stwo_prover::platform;
-    if !platform::avx512_detected() {
-        return;
-    }
-
-    const LOG_SIZE: u32 = 20;
-    let (values, twiddle_dbls) = prepare_values(LOG_SIZE);
-
-    c.bench_function("avx rfft 20bit", |b| {
-        b.iter_with_large_drop(|| unsafe {
-            let mut target = Vec::<PackedBaseField>::with_capacity(values.data.len());
-            #[allow(clippy::uninit_vec)]
-            target.set_len(values.data.len());
-
-            rfft::fft(
-                black_box(std::mem::transmute(values.data.as_ptr())),
-                std::mem::transmute(target.as_mut_ptr()),
-                &twiddle_dbls
-                    .iter()
-                    .map(|x| x.as_slice())
-                    .collect::<Vec<_>>(),
-                LOG_SIZE as usize,
-            );
-        })
-    });
-}
-
-#[cfg(target_arch = "x86_64")]
-fn prepare_values(
-    log_size: u32,
-) -> (
-    stwo_prover::core::backend::avx512::BaseFieldVec,
-    Vec<Vec<i32>>,
-) {
-    use stwo_prover::core::backend::avx512::fft::ifft::get_itwiddle_dbls;
-    let domain = CanonicCoset::new(log_size).circle_domain();
-    let values = (0..domain.size())
-        .map(|i| BaseField::from_u32_unchecked(i as u32))
-        .collect::<Vec<_>>();
-    let values = stwo_prover::core::backend::avx512::BaseFieldVec::from_iter(values);
-    let twiddle_dbls = get_itwiddle_dbls(domain.half_coset);
-    (values, twiddle_dbls)
-}
-
-#[cfg(target_arch = "x86_64")]
-criterion::criterion_group!(
-    name = benches;
-    config = Criterion::default().sample_size(10);
-    targets = avx512_ifft, avx512_ifft_parts, avx512_rfft, simd_ifft, simd_ifft_parts, simd_rfft);
-#[cfg(not(target_arch = "x86_64"))]
-criterion::criterion_group!(
+criterion_group!(
     name = benches;
     config = Criterion::default().sample_size(10);
     targets = simd_ifft, simd_ifft_parts, simd_rfft);
-criterion::criterion_main!(benches);
+criterion_main!(benches);
