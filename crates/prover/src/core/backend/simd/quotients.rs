2d18fc39ae64395c90603ebad4f802739b215eb0
Andrew Milson <andrew.j.milson@gmail.com>
2024-05-06 15:57:00 -0400

    Fix compilation issues

--- crates/prover/src/core/backend/simd/quotients.rs
+++ crates/prover/src/core/backend/simd/quotients.rs
@@ -1,8 +1,12 @@
-use itertools::{izip, zip_eq, Itertools};
+use std::iter::zip;
 
+use itertools::izip;
+use num_traits::{One, Zero};
+
+use super::column::SecureFieldVec;
+use super::m31::{PackedBaseField, LOG_N_LANES, N_LANES};
 use super::qm31::PackedSecureField;
-use super::{AVX512Backend, SecureFieldVec, K_BLOCK_SIZE, VECS_LOG_SIZE};
-use crate::core::backend::avx512::PackedBaseField;
+use super::SimdBackend;
 use crate::core::backend::cpu::quotients::{
     batch_random_coeffs, column_line_coeffs, QuotientConstants,
 };
@@ -11,29 +15,29 @@ use crate::core::circle::CirclePoint;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
 use crate::core::fields::secure_column::SecureColumn;
-use crate::core::fields::{ComplexConjugate, FieldOps};
+use crate::core::fields::FieldOps;
 use crate::core::pcs::quotients::{ColumnSampleBatch, QuotientOps};
 use crate::core::poly::circle::{CircleDomain, CircleEvaluation, SecureEvaluation};
 use crate::core::poly::BitReversedOrder;
 use crate::core::utils::bit_reverse_index;
 
-impl QuotientOps for AVX512Backend {
+impl QuotientOps for SimdBackend {
     fn accumulate_quotients(
         domain: CircleDomain,
         columns: &[&CircleEvaluation<Self, BaseField, BitReversedOrder>],
         random_coeff: SecureField,
         sample_batches: &[ColumnSampleBatch],
     ) -> SecureEvaluation<Self> {
-        assert!(domain.log_size() >= VECS_LOG_SIZE as u32);
-        let mut values = SecureColumn::<AVX512Backend>::zeros(domain.size());
+        assert!(domain.log_size() >= LOG_N_LANES);
+        let mut values = SecureColumn::<Self>::zeros(domain.size());
         let quotient_constants = quotient_constants(sample_batches, random_coeff, domain);
 
         // TODO(spapini): bit reverse iterator.
-        for vec_row in 0..(1 << (domain.log_size() - VECS_LOG_SIZE as u32)) {
+        for vec_row in 0..1 << (domain.log_size() - LOG_N_LANES) {
             // TODO(spapini): Optimize this, for the small number of columns case.
             let points = std::array::from_fn(|i| {
                 domain.at(bit_reverse_index(
-                    (vec_row << VECS_LOG_SIZE) + i,
+                    (vec_row << LOG_N_LANES) + i,
                     domain.log_size(),
                 ))
             });
@@ -52,35 +56,27 @@ impl QuotientOps for AVX512Backend {
     }
 }
 
+// TODO(Ohad): no longer using pair_vanishing, remove domain_point_vec and line_coeffs, or write a
+// function that deals with quotients over pair_vanishing polynomials.
 pub fn accumulate_row_quotients(
     sample_batches: &[ColumnSampleBatch],
-    columns: &[&CircleEvaluation<AVX512Backend, BaseField, BitReversedOrder>],
-    quotient_constants: &QuotientConstants<AVX512Backend>,
+    columns: &[&CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>],
+    quotient_constants: &QuotientConstants<SimdBackend>,
     vec_row: usize,
-    domain_point_vec: (PackedBaseField, PackedBaseField),
+    _domain_point_vec: (PackedBaseField, PackedBaseField),
 ) -> PackedSecureField {
     let mut row_accumulator = PackedSecureField::zero();
-    for (sample_batch, line_coeffs, batch_coeff, denominator_inverses) in izip!(
+    for (sample_batch, _, batch_coeff, denominator_inverses) in izip!(
         sample_batches,
         &quotient_constants.line_coeffs,
         &quotient_constants.batch_random_coeffs,
         &quotient_constants.denominator_inverses
     ) {
         let mut numerator = PackedSecureField::zero();
-        for ((column_index, _), (a, b, c)) in zip_eq(&sample_batch.columns_and_values, line_coeffs)
-        {
+        for (column_index, sampled_value) in sample_batch.columns_and_values.iter() {
             let column = &columns[*column_index];
-            let value = PackedSecureField::broadcast(*c) * column.data[vec_row];
-            // The numerator is a line equation passing through
-            //   (sample_point.y, sample_value), (conj(sample_point), conj(sample_value))
-            // evaluated at (domain_point.y, value).
-            // When substituting a polynomial in this line equation, we get a polynomial with a root
-            // at sample_point and conj(sample_point) if the original polynomial had the values
-            // sample_value and conj(sample_value) at these points.
-            // TODO(AlonH): Use single point vanishing to save a multiplication.
-            let linear_term = PackedSecureField::broadcast(*a) * domain_point_vec.1
-                + PackedSecureField::broadcast(*b);
-            numerator += value - linear_term;
+            let value = column.data[vec_row];
+            numerator += PackedSecureField::broadcast(-*sampled_value) + value;
         }
 
         row_accumulator = row_accumulator * PackedSecureField::broadcast(*batch_coeff)
@@ -89,56 +85,68 @@ pub fn accumulate_row_quotients(
     row_accumulator
 }
 
-/// Pair vanishing for the packed representation of the points. See
-/// [crate::core::constraints::pair_vanishing] for more details.
-fn packed_pair_vanishing(
-    excluded0: CirclePoint<SecureField>,
-    excluded1: CirclePoint<SecureField>,
-    packed_p: (PackedBaseField, PackedBaseField),
-) -> PackedSecureField {
-    PackedSecureField::broadcast(excluded0.y - excluded1.y) * packed_p.0
-        + PackedSecureField::broadcast(excluded1.x - excluded0.x) * packed_p.1
-        + PackedSecureField::broadcast(excluded0.x * excluded1.y - excluded0.y * excluded1.x)
+/// Point vanishing for the packed representation of the points. skips the division.
+/// See [crate::core::constraints::point_vanishing_fraction] for more details.
+fn packed_point_vanishing_fraction(
+    excluded: CirclePoint<SecureField>,
+    p: (PackedBaseField, PackedBaseField),
+) -> (PackedSecureField, PackedSecureField) {
+    let e_conjugate = excluded.conjugate();
+    let h_x = PackedSecureField::broadcast(e_conjugate.x) * p.0
+        - PackedSecureField::broadcast(e_conjugate.y) * p.1;
+    let h_y = PackedSecureField::broadcast(e_conjugate.y) * p.0
+        + PackedSecureField::broadcast(e_conjugate.x) * p.1;
+    (h_y, PackedSecureField::one() + h_x)
 }
 
 fn denominator_inverses(
     sample_batches: &[ColumnSampleBatch],
     domain: CircleDomain,
-) -> Vec<Col<AVX512Backend, SecureField>> {
-    let flat_denominators: SecureFieldVec = sample_batches
-        .iter()
-        .flat_map(|sample_batch| {
-            (0..(1 << (domain.log_size() - VECS_LOG_SIZE as u32)))
-                .map(|vec_row| {
-                    // TODO(spapini): Optimize this, for the small number of columns case.
-                    let points = std::array::from_fn(|i| {
-                        domain.at(bit_reverse_index(
-                            (vec_row << VECS_LOG_SIZE) + i,
-                            domain.log_size(),
-                        ))
-                    });
-                    let domain_points_x = PackedBaseField::from_array(points.map(|p| p.x));
-                    let domain_points_y = PackedBaseField::from_array(points.map(|p| p.y));
-                    let domain_point_vec = (domain_points_x, domain_points_y);
-                    packed_pair_vanishing(
-                        sample_batch.point,
-                        sample_batch.point.complex_conjugate(),
-                        domain_point_vec,
-                    )
-                })
-                .collect_vec()
-        })
-        .collect();
+) -> Vec<Col<SimdBackend, SecureField>> {
+    let mut numerators = Vec::new();
+    let mut denominators = Vec::new();
 
-    let mut flat_denominator_inverses = SecureFieldVec::zeros(flat_denominators.len());
-    <AVX512Backend as FieldOps<SecureField>>::batch_inverse(
-        &flat_denominators,
+    for sample_batch in sample_batches {
+        for vec_row in 0..1 << (domain.log_size() - LOG_N_LANES) {
+            // TODO(spapini): Optimize this, for the small number of columns case.
+            let points = std::array::from_fn(|i| {
+                domain.at(bit_reverse_index(
+                    (vec_row << LOG_N_LANES) + i,
+                    domain.log_size(),
+                ))
+            });
+            let domain_points_x = PackedBaseField::from_array(points.map(|p| p.x));
+            let domain_points_y = PackedBaseField::from_array(points.map(|p| p.y));
+            let domain_point_vec = (domain_points_x, domain_points_y);
+            let (denominator, numerator) =
+                packed_point_vanishing_fraction(sample_batch.point, domain_point_vec);
+            denominators.push(denominator);
+            numerators.push(numerator);
+        }
+    }
+
+    let denominators = SecureFieldVec {
+        length: denominators.len() * N_LANES,
+        data: denominators,
+    };
+
+    let numerators = SecureFieldVec {
+        length: numerators.len() * N_LANES,
+        data: numerators,
+    };
+
+    let mut flat_denominator_inverses = SecureFieldVec::zeros(denominators.len());
+    <SimdBackend as FieldOps<SecureField>>::batch_inverse(
+        &denominators,
         &mut flat_denominator_inverses,
     );
 
+    zip(&mut flat_denominator_inverses.data, &numerators.data)
+        .for_each(|(inv, denom_denom)| *inv *= *denom_denom);
+
     flat_denominator_inverses
         .data
-        .chunks(domain.size() / K_BLOCK_SIZE)
+        .chunks(domain.size() / N_LANES)
         .map(|denominator_inverses| denominator_inverses.iter().copied().collect())
         .collect()
 }
@@ -147,7 +155,7 @@ fn quotient_constants(
     sample_batches: &[ColumnSampleBatch],
     random_coeff: SecureField,
     domain: CircleDomain,
-) -> QuotientConstants<AVX512Backend> {
+) -> QuotientConstants<SimdBackend> {
     let line_coeffs = column_line_coeffs(sample_batches, random_coeff);
     let batch_random_coeffs = batch_random_coeffs(sample_batches, random_coeff);
     let denominator_inverses = denominator_inverses(sample_batches, domain);
@@ -158,12 +166,12 @@ fn quotient_constants(
     }
 }
 
-#[cfg(all(target_arch = "x86_64", target_feature = "avx512f"))]
 #[cfg(test)]
 mod tests {
     use itertools::Itertools;
 
-    use crate::core::backend::avx512::{AVX512Backend, BaseFieldVec};
+    use crate::core::backend::simd::column::BaseFieldVec;
+    use crate::core::backend::simd::SimdBackend;
     use crate::core::backend::{CPUBackend, Column};
     use crate::core::circle::SECURE_FIELD_CIRCLE_GEN;
     use crate::core::fields::m31::BaseField;
@@ -173,14 +181,14 @@ mod tests {
     use crate::qm31;
 
     #[test]
-    fn test_avx_accumulate_quotients() {
+    fn test_accumulate_quotients() {
         const LOG_SIZE: u32 = 8;
         let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
         let e0: BaseFieldVec = (0..domain.size()).map(BaseField::from).collect();
         let e1: BaseFieldVec = (0..domain.size()).map(|i| BaseField::from(2 * i)).collect();
         let columns = vec![
-            CircleEvaluation::<AVX512Backend, _, BitReversedOrder>::new(domain, e0),
-            CircleEvaluation::<AVX512Backend, _, BitReversedOrder>::new(domain, e1),
+            CircleEvaluation::<SimdBackend, BaseField, BitReversedOrder>::new(domain, e0),
+            CircleEvaluation::<SimdBackend, BaseField, BitReversedOrder>::new(domain, e1),
         ];
         let random_coeff = qm31!(1, 2, 3, 4);
         let a = qm31!(3, 6, 9, 12);
@@ -189,15 +197,6 @@ mod tests {
             point: SECURE_FIELD_CIRCLE_GEN,
             columns_and_values: vec![(0, a), (1, b)],
         }];
-        let avx_result = AVX512Backend::accumulate_quotients(
-            domain,
-            &columns.iter().collect_vec(),
-            random_coeff,
-            &samples,
-        )
-        .values
-        .to_vec();
-
         let cpu_columns = columns
             .iter()
             .map(|c| {
@@ -207,7 +206,6 @@ mod tests {
                 )
             })
             .collect::<Vec<_>>();
-
         let cpu_result = CPUBackend::accumulate_quotients(
             domain,
             &cpu_columns.iter().collect_vec(),
@@ -217,6 +215,15 @@ mod tests {
         .values
         .to_vec();
 
-        assert_eq!(avx_result, cpu_result);
+        let res = SimdBackend::accumulate_quotients(
+            domain,
+            &columns.iter().collect_vec(),
+            random_coeff,
+            &samples,
+        )
+        .values
+        .to_vec();
+
+        assert_eq!(res, cpu_result);
     }
 }
