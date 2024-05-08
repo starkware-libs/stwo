f53801504c5a920f24a42dcd87e3d95fa82b2842
Andrew Milson <andrew.j.milson@gmail.com>
2024-05-08 18:20:12 -0400

    Add GKR implementation of Logup lookups

---
+++ crates/prover/src/core/backend/cpu/lookups/logup.rs
@@ -0,0 +1,319 @@
+use num_traits::{One, Zero};
+
+use crate::core::backend::CpuBackend;
+use crate::core::fields::m31::BaseField;
+use crate::core::fields::qm31::SecureField;
+use crate::core::fields::Field;
+use crate::core::lookups::gkr::{correct_sum_as_poly_in_first_variable, GkrBinaryLayer};
+use crate::core::lookups::logup::{Fraction, LogupOps, LogupOracle, LogupTrace};
+use crate::core::lookups::mle::Mle;
+use crate::core::lookups::sumcheck::MultivariatePolyOracle;
+use crate::core::lookups::utils::UnivariatePoly;
+
+impl LogupOps for CpuBackend {
+    fn next_layer(layer: &LogupTrace<Self>) -> LogupTrace<Self> {
+        let half_n = 1 << (layer.n_variables() - 1);
+        let mut next_numerators = Vec::with_capacity(half_n);
+        let mut next_denominators = Vec::with_capacity(half_n);
+        let one = BaseField::one();
+
+        match layer {
+            LogupTrace::Singles { denominators } => {
+                for i in 0..half_n {
+                    let d0 = denominators[i * 2];
+                    let d1 = denominators[i * 2 + 1];
+
+                    let res = Fraction::new(one, d0) + Fraction::new(one, d1);
+
+                    next_numerators.push(res.numerator);
+                    next_denominators.push(res.denominator);
+                }
+            }
+            LogupTrace::Multiplicities {
+                numerators,
+                denominators,
+            } => {
+                for i in 0..half_n {
+                    let n0 = numerators[i * 2];
+                    let d0 = denominators[i * 2];
+
+                    let n1 = numerators[i * 2 + 1];
+                    let d1 = denominators[i * 2 + 1];
+
+                    let res = Fraction::new(n0, d0) + Fraction::new(n1, d1);
+
+                    next_numerators.push(res.numerator);
+                    next_denominators.push(res.denominator);
+                }
+            }
+            LogupTrace::Generic {
+                numerators,
+                denominators,
+            } => {
+                for i in 0..half_n {
+                    let n0 = numerators[i * 2];
+                    let d0 = denominators[i * 2];
+
+                    let n1 = numerators[i * 2 + 1];
+                    let d1 = denominators[i * 2 + 1];
+
+                    let res = Fraction::new(n0, d0) + Fraction::new(n1, d1);
+
+                    next_numerators.push(res.numerator);
+                    next_denominators.push(res.denominator);
+                }
+            }
+        }
+
+        LogupTrace::Generic {
+            numerators: Mle::new(next_numerators),
+            denominators: Mle::new(next_denominators),
+        }
+    }
+
+    fn sum_as_poly_in_first_variable(
+        h: &LogupOracle<'_, Self>,
+        claim: SecureField,
+    ) -> UnivariatePoly<SecureField> {
+        let k = h.n_variables();
+        let n_terms = 1 << (k - 1);
+        let lambda = h.lambda();
+        let eq_evals = h.eq_evals();
+        let y = eq_evals.y();
+        let trace = h.trace();
+
+        let mut eval_at_0 = SecureField::zero();
+        let mut eval_at_2 = SecureField::zero();
+
+        for i in 0..n_terms {
+            let (fraction0, fraction2) = match trace {
+                LogupTrace::Singles { denominators } => {
+                    let d0_lhs = denominators[i * 2];
+                    let d1_lhs = denominators[i * 2 + 1];
+
+                    let fraction0 = Fraction::new(d0_lhs + d1_lhs, d0_lhs * d1_lhs);
+
+                    let d0_rhs = denominators[(n_terms + i) * 2];
+                    let d1_rhs = denominators[(n_terms + i) * 2 + 1];
+
+                    let fraction2 = {
+                        let d0 = d0_rhs.double() - d0_lhs;
+                        let d1 = d1_rhs.double() - d1_lhs;
+                        Fraction::new(d0 + d1, d0 * d1)
+                    };
+
+                    (fraction0, fraction2)
+                }
+                LogupTrace::Multiplicities {
+                    numerators,
+                    denominators,
+                } => {
+                    let n0_lhs = numerators[i * 2];
+                    let n1_lhs = numerators[i * 2 + 1];
+
+                    let d0_lhs = denominators[i * 2];
+                    let d1_lhs = denominators[i * 2 + 1];
+
+                    let fraction0 = {
+                        let a = Fraction::new(n0_lhs, d0_lhs);
+                        let b = Fraction::new(n1_lhs, d1_lhs);
+                        a + b
+                    };
+
+                    let n0_rhs = numerators[(n_terms + i) * 2];
+                    let n1_rhs = numerators[(n_terms + i) * 2 + 1];
+
+                    let d0_rhs = denominators[(n_terms + i) * 2];
+                    let d1_rhs = denominators[(n_terms + i) * 2 + 1];
+
+                    let fraction2 = {
+                        let n0 = n0_rhs.double() - n0_lhs;
+                        let n1 = n1_rhs.double() - n1_lhs;
+
+                        let d0 = d0_rhs.double() - d0_lhs;
+                        let d1 = d1_rhs.double() - d1_lhs;
+
+                        let a = Fraction::new(n0, d0);
+                        let b = Fraction::new(n1, d1);
+                        a + b
+                    };
+
+                    (fraction0, fraction2)
+                }
+                LogupTrace::Generic {
+                    numerators,
+                    denominators,
+                } => {
+                    let n0_lhs = numerators[i * 2];
+                    let n1_lhs = numerators[i * 2 + 1];
+
+                    let d0_lhs = denominators[i * 2];
+                    let d1_lhs = denominators[i * 2 + 1];
+
+                    let fraction0 = {
+                        let a = Fraction::new(n0_lhs, d0_lhs);
+                        let b = Fraction::new(n1_lhs, d1_lhs);
+                        a + b
+                    };
+
+                    let n0_rhs = numerators[(n_terms + i) * 2];
+                    let n1_rhs = numerators[(n_terms + i) * 2 + 1];
+
+                    let d0_rhs = denominators[(n_terms + i) * 2];
+                    let d1_rhs = denominators[(n_terms + i) * 2 + 1];
+
+                    let fraction2 = {
+                        let n0 = n0_rhs.double() - n0_lhs;
+                        let n1 = n1_rhs.double() - n1_lhs;
+
+                        let d0 = d0_rhs.double() - d0_lhs;
+                        let d1 = d1_rhs.double() - d1_lhs;
+
+                        let a = Fraction::new(n0, d0);
+                        let b = Fraction::new(n1, d1);
+                        a + b
+                    };
+
+                    (fraction0, fraction2)
+                }
+            };
+
+            let eq_eval = eq_evals[i];
+            eval_at_0 += eq_eval * (fraction0.numerator + lambda * fraction0.denominator);
+            eval_at_2 += eq_eval * (fraction2.numerator + lambda * fraction2.denominator);
+        }
+
+        eval_at_0 *= h.eq_fixed_var_correction();
+        eval_at_2 *= h.eq_fixed_var_correction();
+
+        correct_sum_as_poly_in_first_variable(eval_at_0, eval_at_2, claim, y, k)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::iter::zip;
+
+    use num_traits::One;
+    use rand::rngs::SmallRng;
+    use rand::{Rng, SeedableRng};
+
+    use crate::core::backend::CpuBackend;
+    use crate::core::fields::m31::BaseField;
+    use crate::core::fields::qm31::SecureField;
+    use crate::core::lookups::gkr::{
+        partially_verify_batch, prove_batch, GkrBatchVerificationArtifact, GkrError,
+    };
+    use crate::core::lookups::logup::{Fraction, LogupGate, LogupTrace};
+    use crate::core::lookups::mle::Mle;
+    use crate::core::test_utils::test_channel;
+
+    #[test]
+    fn logup_with_generic_trace_works() -> Result<(), GkrError> {
+        const N: usize = 1 << 5;
+        let mut rng = SmallRng::seed_from_u64(0);
+        let numerator_values = (0..N).map(|_| rng.gen()).collect::<Vec<SecureField>>();
+        let denominator_values = (0..N).map(|_| rng.gen()).collect::<Vec<SecureField>>();
+        let sum = zip(&numerator_values, &denominator_values)
+            .map(|(&n, &d)| Fraction::new(n, d))
+            .sum::<Fraction<SecureField>>();
+        let numerators = Mle::<CpuBackend, SecureField>::new(numerator_values);
+        let denominators = Mle::<CpuBackend, SecureField>::new(denominator_values);
+        let top_layer = LogupTrace::Generic {
+            numerators: numerators.clone(),
+            denominators: denominators.clone(),
+        };
+        let proof = prove_batch(&mut test_channel(), vec![top_layer.clone()]);
+
+        let GkrBatchVerificationArtifact {
+            ood_point,
+            claims_to_verify_by_component,
+        } = partially_verify_batch(vec![&LogupGate], &proof, &mut test_channel())?;
+
+        assert_eq!(claims_to_verify_by_component.len(), 1);
+        assert_eq!(proof.output_claims_by_component.len(), 1);
+        assert_eq!(
+            claims_to_verify_by_component[0],
+            [
+                numerators.eval_at_point(&ood_point),
+                denominators.eval_at_point(&ood_point)
+            ]
+        );
+        assert_eq!(
+            proof.output_claims_by_component[0],
+            [sum.numerator, sum.denominator]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn logup_with_singles_trace_works() -> Result<(), GkrError> {
+        const N: usize = 1 << 5;
+        let mut rng = SmallRng::seed_from_u64(0);
+        let denominator_values = (0..N).map(|_| rng.gen()).collect::<Vec<SecureField>>();
+        let sum = denominator_values
+            .iter()
+            .map(|&d| Fraction::new(SecureField::one(), d))
+            .sum::<Fraction<SecureField>>();
+        let denominators = Mle::<CpuBackend, SecureField>::new(denominator_values);
+        let top_layer = LogupTrace::Singles {
+            denominators: denominators.clone(),
+        };
+        let proof = prove_batch(&mut test_channel(), vec![top_layer.clone()]);
+
+        let GkrBatchVerificationArtifact {
+            ood_point,
+            claims_to_verify_by_component,
+        } = partially_verify_batch(vec![&LogupGate], &proof, &mut test_channel())?;
+
+        assert_eq!(claims_to_verify_by_component.len(), 1);
+        assert_eq!(proof.output_claims_by_component.len(), 1);
+        assert_eq!(
+            claims_to_verify_by_component[0],
+            [SecureField::one(), denominators.eval_at_point(&ood_point)]
+        );
+        assert_eq!(
+            proof.output_claims_by_component[0],
+            [sum.numerator, sum.denominator]
+        );
+        Ok(())
+    }
+
+    #[test]
+    fn logup_with_multiplicities_trace_works() -> Result<(), GkrError> {
+        const N: usize = 1 << 5;
+        let mut rng = SmallRng::seed_from_u64(0);
+        let numerator_values = (0..N).map(|_| rng.gen()).collect::<Vec<BaseField>>();
+        let denominator_values = (0..N).map(|_| rng.gen()).collect::<Vec<SecureField>>();
+        let sum = zip(&numerator_values, &denominator_values)
+            .map(|(&n, &d)| Fraction::new(n.into(), d))
+            .sum::<Fraction<SecureField>>();
+        let numerators = Mle::<CpuBackend, BaseField>::new(numerator_values);
+        let denominators = Mle::<CpuBackend, SecureField>::new(denominator_values);
+        let top_layer = LogupTrace::Multiplicities {
+            numerators: numerators.clone(),
+            denominators: denominators.clone(),
+        };
+        let proof = prove_batch(&mut test_channel(), vec![top_layer.clone()]);
+
+        let GkrBatchVerificationArtifact {
+            ood_point,
+            claims_to_verify_by_component,
+        } = partially_verify_batch(vec![&LogupGate], &proof, &mut test_channel())?;
+
+        assert_eq!(claims_to_verify_by_component.len(), 1);
+        assert_eq!(proof.output_claims_by_component.len(), 1);
+        assert_eq!(
+            claims_to_verify_by_component[0],
+            [
+                numerators.eval_at_point(&ood_point),
+                denominators.eval_at_point(&ood_point)
+            ]
+        );
+        assert_eq!(
+            proof.output_claims_by_component[0],
+            [sum.numerator, sum.denominator]
+        );
+        Ok(())
+    }
+}
---
+++ crates/prover/src/core/lookups/logup.rs
@@ -0,0 +1,287 @@
+use std::iter::Sum;
+use std::ops::Add;
+
+use num_traits::{One, Zero};
+
+use super::gkr::{
+    EqEvals, GkrBinaryGate, GkrBinaryLayer, GkrMask, GkrMultivariatePolyOracle, GkrOps, Layer,
+};
+use super::mle::{Mle, MleOps};
+use super::sumcheck::MultivariatePolyOracle;
+use super::utils::{eq, UnivariatePoly};
+use crate::core::backend::Column;
+use crate::core::fields::m31::BaseField;
+use crate::core::fields::qm31::SecureField;
+use crate::core::fields::{ExtensionOf, Field};
+
+pub trait LogupOps: MleOps<BaseField> + MleOps<SecureField> + GkrOps + Sized + 'static {
+    /// Generates the next GKR layer from the current one.
+    fn next_layer(layer: &LogupTrace<Self>) -> LogupTrace<Self>;
+
+    /// Returns univariate polynomial `f(t) = sum_x h(t, x)` where the sum on `x` is taken over
+    /// the boolean hypercube.
+    ///
+    /// `claim` equals `f(0) + f(1)`.
+    ///
+    /// See docs of [`MultivariatePolyOracle::sum_as_poly_in_first_variable`]
+    fn sum_as_poly_in_first_variable(
+        h: &LogupOracle<'_, Self>,
+        claim: SecureField,
+    ) -> UnivariatePoly<SecureField>;
+}
+
+#[derive(Debug, Clone)]
+pub enum LogupTrace<B: LogupOps> {
+    /// All numerators implicitly equal "1".
+    Singles { denominators: Mle<B, SecureField> },
+    Multiplicities {
+        numerators: Mle<B, BaseField>,
+        denominators: Mle<B, SecureField>,
+    },
+    Generic {
+        numerators: Mle<B, SecureField>,
+        denominators: Mle<B, SecureField>,
+    },
+}
+
+impl<B: LogupOps> GkrBinaryLayer for LogupTrace<B> {
+    type Backend = B;
+
+    type MultivariatePolyOracle<'a> = LogupOracle<'a, B>;
+
+    fn n_variables(&self) -> usize {
+        match self {
+            Self::Singles { denominators }
+            | Self::Multiplicities { denominators, .. }
+            | Self::Generic { denominators, .. } => denominators.n_variables(),
+        }
+    }
+
+    fn next(&self) -> Layer<Self> {
+        assert_ne!(0, self.n_variables());
+        let next_layer = B::next_layer(self);
+
+        if next_layer.n_variables() == 0 {
+            Layer::Output(match next_layer {
+                Self::Singles { denominators } => {
+                    let numerator = SecureField::one();
+                    let denominator = denominators.at(0);
+                    vec![numerator, denominator]
+                }
+                Self::Multiplicities {
+                    numerators,
+                    denominators,
+                } => {
+                    let numerator = numerators.at(0).into();
+                    let denominator = denominators.at(0);
+                    vec![numerator, denominator]
+                }
+                Self::Generic {
+                    numerators,
+                    denominators,
+                } => {
+                    vec![numerators.at(0), denominators.at(0)]
+                }
+            })
+        } else {
+            Layer::Internal(next_layer)
+        }
+    }
+
+    fn into_multivariate_poly(
+        self,
+        lambda: SecureField,
+        eq_evals: &super::gkr::EqEvals<Self::Backend>,
+    ) -> Self::MultivariatePolyOracle<'_> {
+        LogupOracle::new(eq_evals, lambda, self)
+    }
+}
+
+/// Sumcheck oracle for a logup+GKR layer.
+pub struct LogupOracle<'a, B: LogupOps> {
+    /// See docs [`GkrBinaryLayer::into_multivariate_poly`].
+    eq_evals: &'a EqEvals<B>,
+    eq_fixed_var_correction: SecureField,
+    /// Used to perform a random linear combination of the multivariate polynomial for the
+    /// numerators and denominators.
+    lambda: SecureField,
+    trace: LogupTrace<B>,
+}
+
+// TODO: Remove all these and change LogupOps to return two evaluations instead of polynomial.
+impl<'a, B: LogupOps> LogupOracle<'a, B> {
+    pub fn new(eq_evals: &'a EqEvals<B>, lambda: SecureField, trace: LogupTrace<B>) -> Self {
+        Self {
+            eq_evals,
+            eq_fixed_var_correction: SecureField::one(),
+            lambda,
+            trace,
+        }
+    }
+
+    pub fn lambda(&self) -> SecureField {
+        self.lambda
+    }
+
+    pub fn eq_evals(&self) -> &EqEvals<B> {
+        self.eq_evals
+    }
+
+    pub fn eq_fixed_var_correction(&self) -> SecureField {
+        self.eq_fixed_var_correction
+    }
+
+    pub fn trace(&self) -> &LogupTrace<B> {
+        &self.trace
+    }
+}
+
+impl<'a, B: LogupOps> MultivariatePolyOracle for LogupOracle<'a, B> {
+    fn n_variables(&self) -> usize {
+        self.trace.n_variables() - 1
+    }
+
+    fn sum_as_poly_in_first_variable(&self, claim: SecureField) -> UnivariatePoly<SecureField> {
+        B::sum_as_poly_in_first_variable(self, claim)
+    }
+
+    fn fix_first_variable(self, challenge: SecureField) -> Self {
+        if self.n_variables() == 0 {
+            return self;
+        }
+
+        let z0 = self.eq_evals.y()[self.eq_evals.y().len() - self.n_variables()];
+        let eq_fixed_var_correction = self.eq_fixed_var_correction * eq(&[challenge], &[z0]);
+
+        let trace = match self.trace {
+            LogupTrace::Generic {
+                numerators,
+                denominators,
+            } => LogupTrace::Generic {
+                numerators: numerators.fix_first_variable(challenge),
+                denominators: denominators.fix_first_variable(challenge),
+            },
+            LogupTrace::Multiplicities {
+                numerators,
+                denominators,
+            } => LogupTrace::Generic {
+                numerators: numerators.fix_first_variable(challenge),
+                denominators: denominators.fix_first_variable(challenge),
+            },
+            LogupTrace::Singles { denominators } => LogupTrace::Singles {
+                denominators: denominators.fix_first_variable(challenge),
+            },
+        };
+
+        Self {
+            eq_evals: self.eq_evals,
+            eq_fixed_var_correction,
+            lambda: self.lambda,
+            trace,
+        }
+    }
+}
+
+impl<'a, B: LogupOps> GkrMultivariatePolyOracle for LogupOracle<'a, B> {
+    type Backend = B;
+
+    fn try_into_mask(self) -> Option<GkrMask> {
+        if self.n_variables() != 0 {
+            return None;
+        }
+
+        let columns = match self.trace {
+            LogupTrace::Generic {
+                numerators,
+                denominators,
+            } => {
+                let numerators = numerators.to_cpu().try_into().unwrap();
+                let denominators = denominators.to_cpu().try_into().unwrap();
+                vec![numerators, denominators]
+            }
+            LogupTrace::Singles { denominators } => {
+                let numerators = [SecureField::one(); 2];
+                let denominators = denominators.to_cpu().try_into().unwrap();
+                vec![numerators, denominators]
+            }
+            // Should only ever get called on `Singles` or `Generic`.
+            LogupTrace::Multiplicities { .. } => unimplemented!(),
+        };
+
+        Some(GkrMask::new(columns))
+    }
+}
+
+/// Logup gate from <https://eprint.iacr.org/2023/1284.pdf> (section 3.1)
+pub struct LogupGate;
+
+impl GkrBinaryGate for LogupGate {
+    fn eval(&self, even_row: &[SecureField], odd_row: &[SecureField]) -> Vec<SecureField> {
+        assert_eq!(even_row.len(), 2);
+        assert_eq!(odd_row.len(), 2);
+
+        let a = Fraction::new(even_row[0], even_row[1]);
+        let b = Fraction::new(odd_row[0], odd_row[1]);
+        let c = a + b;
+
+        vec![c.numerator, c.denominator]
+    }
+}
+
+/// Projective fraction.
+#[derive(Debug, Clone, Copy)]
+pub struct Fraction<F> {
+    pub numerator: F,
+    pub denominator: SecureField,
+}
+
+impl<F> Fraction<F> {
+    pub fn new(numerator: F, denominator: SecureField) -> Self {
+        Self {
+            numerator,
+            denominator,
+        }
+    }
+}
+
+impl<F> Add for Fraction<F>
+where
+    F: Field,
+    SecureField: ExtensionOf<F> + Field,
+{
+    type Output = Fraction<SecureField>;
+
+    fn add(self, rhs: Self) -> Fraction<SecureField> {
+        if self.numerator.is_one() && rhs.numerator.is_one() {
+            Fraction {
+                numerator: self.denominator + rhs.denominator,
+                denominator: self.denominator * rhs.denominator,
+            }
+        } else {
+            Fraction {
+                numerator: rhs.denominator * self.numerator + self.denominator * rhs.numerator,
+                denominator: self.denominator * rhs.denominator,
+            }
+        }
+    }
+}
+
+impl Zero for Fraction<SecureField> {
+    fn zero() -> Self {
+        Self {
+            numerator: SecureField::zero(),
+            denominator: SecureField::one(),
+        }
+    }
+
+    fn is_zero(&self) -> bool {
+        self.numerator.is_zero() && !self.denominator.is_zero()
+    }
+}
+
+impl Sum for Fraction<SecureField> {
+    fn sum<I: Iterator<Item = Self>>(mut iter: I) -> Self {
+        let first = iter.next().unwrap_or_else(Self::zero);
+        iter.fold(first, |a, b| a + b)
+    }
+}
