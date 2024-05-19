462a48c7ddc9db339953960b8a68f3a6b9a590e8
Alon Haramati <alonh@starkware.co>
2024-05-19 16:22:57 +0300

    Add interaction to proof.

--- crates/prover/src/examples/fibonacci/air.rs
+++ crates/prover/src/examples/fibonacci/air.rs
@@ -1,9 +1,15 @@
 use itertools::{zip_eq, Itertools};
 
 use super::component::FibonacciComponent;
-use crate::core::air::{Air, AirProver, Component, ComponentProver};
+use crate::core::air::{
+    Air, AirProver, AirTraceVerifier, AirTraceWriter, Component, ComponentProver,
+};
 use crate::core::backend::CpuBackend;
+use crate::core::channel::Blake2sChannel;
 use crate::core::fields::m31::BaseField;
+use crate::core::poly::circle::CircleEvaluation;
+use crate::core::poly::BitReversedOrder;
+use crate::core::{ColumnVec, ComponentVec, InteractionElements};
 
 pub struct FibonacciAir {
     pub component: FibonacciComponent,
@@ -14,11 +20,33 @@ impl FibonacciAir {
         Self { component }
     }
 }
+
 impl Air for FibonacciAir {
     fn components(&self) -> Vec<&dyn Component> {
         vec![&self.component]
     }
 }
+
+impl AirTraceVerifier for FibonacciAir {
+    fn interaction_elements(&self, _channel: &mut Blake2sChannel) -> InteractionElements {
+        InteractionElements::default()
+    }
+}
+
+impl AirTraceWriter<CpuBackend> for FibonacciAir {
+    fn interact(
+        &self,
+        _trace: &ColumnVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>>,
+        _elements: &InteractionElements,
+    ) -> ComponentVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>> {
+        ComponentVec(vec![vec![]])
+    }
+
+    fn to_air_prover(&self) -> &impl AirProver<CpuBackend> {
+        self
+    }
+}
+
 impl AirProver<CpuBackend> for FibonacciAir {
     fn prover_components(&self) -> Vec<&dyn ComponentProver<CpuBackend>> {
         vec![&self.component]
@@ -28,6 +56,7 @@ impl AirProver<CpuBackend> for FibonacciAir {
 pub struct MultiFibonacciAir {
     pub components: Vec<FibonacciComponent>,
 }
+
 impl MultiFibonacciAir {
     pub fn new(log_sizes: &[u32], claim: &[BaseField]) -> Self {
         let mut components = Vec::new();
@@ -37,6 +66,7 @@ impl MultiFibonacciAir {
         Self { components }
     }
 }
+
 impl Air for MultiFibonacciAir {
     fn components(&self) -> Vec<&dyn Component> {
         self.components
@@ -45,6 +75,27 @@ impl Air for MultiFibonacciAir {
             .collect_vec()
     }
 }
+
+impl AirTraceVerifier for MultiFibonacciAir {
+    fn interaction_elements(&self, _channel: &mut Blake2sChannel) -> InteractionElements {
+        InteractionElements::default()
+    }
+}
+
+impl AirTraceWriter<CpuBackend> for MultiFibonacciAir {
+    fn interact(
+        &self,
+        _trace: &ColumnVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>>,
+        _elements: &InteractionElements,
+    ) -> ComponentVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>> {
+        ComponentVec(vec![vec![]])
+    }
+
+    fn to_air_prover(&self) -> &impl AirProver<CpuBackend> {
+        self
+    }
+}
+
 impl AirProver<CpuBackend> for MultiFibonacciAir {
     fn prover_components(&self) -> Vec<&dyn ComponentProver<CpuBackend>> {
         self.components
--- crates/prover/src/examples/wide_fibonacci/constraint_eval.rs
+++ crates/prover/src/examples/wide_fibonacci/constraint_eval.rs
@@ -1,21 +1,52 @@
-use itertools::Itertools;
+use std::collections::BTreeMap;
+
+use itertools::{zip_eq, Itertools};
 use num_traits::Zero;
 
 use super::component::{Input, WideFibAir, WideFibComponent};
 use super::trace_gen::write_trace_row;
 use crate::core::air::accumulation::DomainEvaluationAccumulator;
-use crate::core::air::{AirProver, Component, ComponentProver, ComponentTrace};
-use crate::core::backend::{Column, CpuBackend};
+use crate::core::air::{
+    AirProver, AirTraceVerifier, AirTraceWriter, Component, ComponentProver, ComponentTrace,
+    ComponentTraceWriter,
+};
+use crate::core::backend::CpuBackend;
+use crate::core::channel::{Blake2sChannel, Channel};
 use crate::core::constraints::coset_vanishing;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
 use crate::core::fields::FieldExpOps;
-use crate::core::poly::circle::CanonicCoset;
+use crate::core::poly::circle::{CanonicCoset, CircleEvaluation};
+use crate::core::poly::BitReversedOrder;
 use crate::core::utils::bit_reverse;
-use crate::core::ColumnVec;
+use crate::core::{ColumnVec, ComponentVec, InteractionElements};
 use crate::examples::wide_fibonacci::component::LOG_N_COLUMNS;
 
 // TODO(AlonH): Rename file to `cpu.rs`.
+
+impl AirTraceVerifier for WideFibAir {
+    fn interaction_elements(&self, channel: &mut Blake2sChannel) -> InteractionElements {
+        let ids = self.component.interaction_element_ids();
+        let elements = channel.draw_felts(ids.len()).into_iter().map(|e| e.0 .0);
+        InteractionElements::new(BTreeMap::from_iter(zip_eq(ids, elements)))
+    }
+}
+
+impl AirTraceWriter<CpuBackend> for WideFibAir {
+    fn interact(
+        &self,
+        trace: &ColumnVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>>,
+        elements: &InteractionElements,
+    ) -> ComponentVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>> {
+        ComponentVec(vec![self
+            .component
+            .write_interaction_trace(&trace.iter().collect(), elements)])
+    }
+
+    fn to_air_prover(&self) -> &impl AirProver<CpuBackend> {
+        self
+    }
+}
 
 impl AirProver<CpuBackend> for WideFibAir {
     fn prover_components(&self) -> Vec<&dyn ComponentProver<CpuBackend>> {
@@ -28,6 +59,7 @@ impl ComponentProver<CpuBackend> for WideFibComponent {
         &self,
         trace: &ComponentTrace<'_, CpuBackend>,
         evaluation_accumulator: &mut DomainEvaluationAccumulator<CpuBackend>,
+        _interaction_elements: &InteractionElements,
     ) {
         let max_constraint_degree = self.max_constraint_log_degree_bound();
         let trace_eval_domain = CanonicCoset::new(max_constraint_degree).circle_domain();
@@ -49,9 +81,8 @@ impl ComponentProver<CpuBackend> for WideFibComponent {
             // Step constraints.
             for j in 0..self.n_columns() - 2 {
                 numerators[i] += accum.random_coeff_powers[self.n_columns() - 3 - j]
-                    * (trace_evals[j].values.at(i).square()
-                        + trace_evals[j + 1].values.at(i).square()
-                        - trace_evals[j + 2].values.at(i));
+                    * (trace_evals[0][j][i].square() + trace_evals[0][j + 1][i].square()
+                        - trace_evals[0][j + 2][i]);
             }
         }
         for (i, (num, denom)) in numerators.iter().zip(denom_inverses.iter()).enumerate() {
