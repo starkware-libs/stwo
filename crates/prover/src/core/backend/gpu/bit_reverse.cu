21240c432f623873b66bafe50da6d805060308b1
Sergio Chouhy <sergio.chouhy@gmail.com>
2024-05-28 12:13:05 +0000

    remove __host__

--- crates/prover/src/core/backend/gpu/bit_reverse.cu
+++ crates/prover/src/core/backend/gpu/bit_reverse.cu
@@ -1,4 +1,4 @@
-__device__ __host__ unsigned int bit_reverse(unsigned int n, int bits) {
+__device__ unsigned int bit_reverse(unsigned int n, int bits) {
     unsigned int reversed_n = 0;
     for (int i = 0; i < bits; ++i) {
         reversed_n |= ((n >> i) & 1) << (bits - 1 - i);
