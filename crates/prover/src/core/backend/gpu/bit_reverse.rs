f2d907bbe51af2fb1ee89f3c8ca330243651d2d7
Sergio Chouhy <sergio.chouhy@gmail.com>
2024-05-28 11:54:24 +0000

    use cpubacknd as ground truth in bit_reverse GpuBackend test

--- crates/prover/src/core/backend/gpu/bit_reverse.rs
+++ crates/prover/src/core/backend/gpu/bit_reverse.rs
@@ -17,7 +17,7 @@ impl ColumnOps<BaseField> for GpuBackend {
         let size = column.len();
         assert!(size.is_power_of_two());
         let bits = usize::BITS - size.leading_zeros() - 1;
-        let config = LaunchConfig::for_num_elems(16);
+        let config = LaunchConfig::for_num_elems(size as u32);
         let kernel = DEVICE.get_func("bit_reverse", "kernel").unwrap();
         unsafe { kernel.launch(config, (column.as_mut_slice(), size, bits)) }.unwrap();
     }
@@ -42,24 +42,24 @@ mod tests {
     use super::DEVICE;
     use crate::core::backend::gpu::column::BaseFieldCudaColumn;
     use crate::core::backend::gpu::GpuBackend;
-    use crate::core::backend::ColumnOps;
+    use crate::core::backend::{ColumnOps, CpuBackend};
     use crate::core::fields::m31::BaseField;
 
     #[test]
     fn test_bit_reverse() {
-        let size: usize = 16;
+        let size: usize = 128;
         let mut h_column: Vec<u32> = (0..size as u32).collect();
         let mut column: BaseFieldCudaColumn =
             BaseFieldCudaColumn::new(DEVICE.htod_sync_copy(&h_column).unwrap());
 
         <GpuBackend as ColumnOps<BaseField>>::bit_reverse_column(&mut column);
-
         DEVICE
             .dtoh_sync_copy_into(column.as_slice(), &mut h_column)
             .unwrap();
-        assert_eq!(
-            h_column,
-            vec![0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-        );
+
+        let mut expected_result: Vec<u32> = (0..size as u32).collect();
+        CpuBackend::bit_reverse_column(&mut expected_result);
+
+        assert_eq!(h_column, expected_result);
     }
 }
