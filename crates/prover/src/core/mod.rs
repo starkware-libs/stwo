3578d6698a5737e072032337d673dd973a4856e8
Alon Haramati <alonh@starkware.co>
2024-06-16 11:13:22 +0300

    Add lookup final boundary constraints.

--- crates/prover/src/core/mod.rs
+++ crates/prover/src/core/mod.rs
@@ -1,6 +1,8 @@
 use std::collections::BTreeMap;
 use std::ops::{Deref, DerefMut, Index};
 
+use fields::m31::BaseField;
+
 use self::fields::qm31::SecureField;
 
 pub mod air;
@@ -61,7 +63,7 @@ impl<T> DerefMut for ComponentVec<T> {
     }
 }
 
-#[derive(Default)]
+#[derive(Default, Debug)]
 pub struct InteractionElements(BTreeMap<String, SecureField>);
 
 impl InteractionElements {
@@ -76,6 +78,28 @@ impl InteractionElements {
 
 impl Index<&str> for InteractionElements {
     type Output = SecureField;
+
+    fn index(&self, index: &str) -> &Self::Output {
+        // TODO(AlonH): Return an error if the key is not found.
+        &self.0[index]
+    }
+}
+
+#[derive(Default, Debug)]
+pub struct LookupValues(BTreeMap<String, BaseField>);
+
+impl LookupValues {
+    pub fn new(values: BTreeMap<String, BaseField>) -> Self {
+        Self(values)
+    }
+
+    pub fn extend(&mut self, other: Self) {
+        self.0.extend(other.0);
+    }
+}
+
+impl Index<&str> for LookupValues {
+    type Output = BaseField;
 
     fn index(&self, index: &str) -> &Self::Output {
         // TODO(AlonH): Return an error if the key is not found.
