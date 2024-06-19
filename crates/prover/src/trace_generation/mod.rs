de097de891b2a675b02b66528eaf87d897e5730f
Ohad Agadi <ohad.a@starkware.co>
2024-06-19 14:15:31 +0300

    seperated trace gen trait

--- crates/prover/src/trace_generation/mod.rs
+++ crates/prover/src/trace_generation/mod.rs
@@ -1,8 +1,9 @@
 pub mod registry;
 
 use downcast_rs::{impl_downcast, Downcast};
-use registry::ComponentRegistry;
+use registry::ComponentGenerationRegistry;
 
+use crate::core::air::Component;
 use crate::core::backend::Backend;
 use crate::core::fields::m31::BaseField;
 use crate::core::poly::circle::CircleEvaluation;
@@ -15,19 +16,22 @@ impl_downcast!(ComponentGen);
 // A trait to generate a a trace.
 // Generates the trace given a list of inputs collects inputs for subcomponents.
 pub trait TraceGenerator<B: Backend> {
-    type ComponentInputs;
+    type Component: Component;
+    type Inputs;
 
     /// Add inputs for the trace generation of the component.
     /// This function should be called from the caller components before calling `write_trace` of
     /// this component.
-    fn add_inputs(&mut self, inputs: &Self::ComponentInputs);
+    fn add_inputs(&mut self, inputs: &Self::Inputs);
 
     /// Allocates and returns the trace of the component and updates the
     /// subcomponents with the corresponding inputs.
     /// Should be called only after all the inputs are available.
     // TODO(ShaharS): change `component_id` to a struct that contains the id and the component name.
     fn write_trace(
-        component_id: &str,
-        registry: &mut ComponentRegistry,
+        &self,
+        registry: &mut ComponentGenerationRegistry,
     ) -> ColumnVec<CircleEvaluation<B, BaseField, BitReversedOrder>>;
+
+    fn component(&self) -> Self::Component;
 }
