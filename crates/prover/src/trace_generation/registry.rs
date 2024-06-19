7b82dddf6cd29b7494d324dbc1b5029912e39daf
Ohad Agadi <ohad.a@starkware.co>
2024-06-19 14:15:31 +0300

    input registry

--- crates/prover/src/trace_generation/registry.rs
+++ crates/prover/src/trace_generation/registry.rs
@@ -1,15 +1,29 @@
 use std::collections::HashMap;
 
-use super::ComponentGen;
+use super::{ComponentGen, InputsType, TraceGenerator};
+use crate::core::backend::Backend;
 
 #[derive(Default)]
 pub struct ComponentRegistry {
     components: HashMap<String, Box<dyn ComponentGen>>,
+    inputs: HashMap<String, Box<dyn InputsType>>,
 }
 
 impl ComponentRegistry {
-    pub fn register_component(&mut self, component_id: String, component: impl ComponentGen) {
-        self.components.insert(component_id, Box::new(component));
+    pub fn register_component(&mut self, component_id: &str, component: impl ComponentGen) {
+        self.components
+            .insert(component_id.to_string(), Box::new(component));
+    }
+
+    pub fn register_component_and_init_input<B: Backend>(
+        &mut self,
+        component_id: &str,
+        component: impl ComponentGen + TraceGenerator<B>,
+    ) {
+        self.inputs
+            .insert(component_id.to_string(), component.initialize_inputs());
+        self.components
+            .insert(component_id.to_string(), Box::new(component));
     }
 
     pub fn get_component<T: ComponentGen>(&self, component_id: &str) -> &T {
@@ -24,6 +38,22 @@ impl ComponentRegistry {
         self.components
             .get_mut(component_id)
             .unwrap_or_else(|| panic!("Component name {} not found.", component_id))
+            .downcast_mut()
+            .unwrap()
+    }
+
+    pub fn get_inputs<T: InputsType>(&self, component_id: &str) -> &T {
+        self.inputs
+            .get(component_id)
+            .unwrap_or_else(|| panic!("No inputs found for Component name {}", component_id))
+            .downcast_ref()
+            .unwrap()
+    }
+
+    pub fn get_inputs_mut<T: InputsType>(&mut self, component_id: &str) -> &mut T {
+        self.inputs
+            .get_mut(component_id)
+            .unwrap_or_else(|| panic!("No inputs found for Component name {}", component_id))
             .downcast_mut()
             .unwrap()
     }
@@ -49,7 +79,11 @@ mod tests {
     impl TraceGenerator<CpuBackend> for ComponentA {
         type ComponentInputs = u32;
 
-        fn add_inputs(&mut self, _inputs: &Self::ComponentInputs) {
+        fn add_inputs(
+            _component_id: &str,
+            _registry: &mut ComponentRegistry,
+            _inputs: &Self::ComponentInputs,
+        ) {
             unimplemented!("TestTraceGenerator::add_inputs")
         }
 
@@ -59,6 +93,10 @@ mod tests {
         ) -> ColumnVec<CircleEvaluation<CpuBackend, BaseField, BitReversedOrder>> {
             unimplemented!("TestTraceGenerator::write_trace")
         }
+
+        fn initialize_inputs(&self) -> Box<dyn InputsType> {
+            unimplemented!("TestTraceGenerator::initialize_inputs")
+        }
     }
 
     #[test]
@@ -66,7 +104,7 @@ mod tests {
         let mut registry = ComponentRegistry::default();
         let component = ComponentA { inputs: vec![1] };
 
-        registry.register_component("test".to_string(), component);
+        registry.register_component("test", component);
 
         assert_eq!(registry.get_component::<ComponentA>("test").inputs, vec![1]);
     }
