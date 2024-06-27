cc087f26eb3331a1ded905190bdc88c5ea3dd664
Shahar Papini <spapini@starkware.co>
2024-06-27 11:03:38 +0300

    Poseidon2

---
+++ crates/prover/benches/poseidon.rs
@@ -0,0 +1,32 @@
+use criterion::{criterion_group, criterion_main, Criterion, Throughput};
+use stwo_prover::core::backend::simd::SimdBackend;
+use stwo_prover::core::channel::{Blake2sChannel, Channel};
+use stwo_prover::core::fields::m31::BaseField;
+use stwo_prover::core::fields::IntoSlice;
+use stwo_prover::core::prover::prove;
+use stwo_prover::core::vcs::blake2_hash::Blake2sHasher;
+use stwo_prover::core::vcs::hasher::Hasher;
+use stwo_prover::examples::poseidon::{gen_trace, PoseidonAir, PoseidonComponent};
+
+pub fn simd_poseidon(c: &mut Criterion) {
+    const LOG_N_ROWS: u32 = 15;
+    let mut group = c.benchmark_group("poseidon2");
+    group.throughput(Throughput::Elements(1u64 << (LOG_N_ROWS + 3)));
+    group.bench_function(format!("poseidon2 2^{} instances", LOG_N_ROWS + 3), |b| {
+        b.iter(|| {
+            let component = PoseidonComponent {
+                log_n_instances: LOG_N_ROWS,
+            };
+            let trace = gen_trace(component.log_column_size());
+            let channel = &mut Blake2sChannel::new(Blake2sHasher::hash(BaseField::into_slice(&[])));
+            let air = PoseidonAir { component };
+            prove::<SimdBackend>(&air, channel, trace).unwrap()
+        });
+    });
+}
+
+criterion_group!(
+    name = bit_rev;
+    config = Criterion::default().sample_size(10);
+    targets = simd_poseidon);
+criterion_main!(bit_rev);
--- crates/prover/src/examples/wide_fibonacci/simd.rs
+++ crates/prover/src/examples/wide_fibonacci/simd.rs
@@ -248,7 +248,7 @@ mod tests {
     fn test_simd_wide_fib_prove() {
         // Note: To see time measurement, run test with
         //   RUST_LOG_SPAN_EVENTS=enter,close RUST_LOG=info RUST_BACKTRACE=1 RUSTFLAGS="
-        //   -C target-cpu=native -C target-feature=+avx512f -C opt-level=2" cargo test
+        //   -C target-cpu=native -C target-feature=+avx512f -C opt-level=3" cargo test
         //   test_simd_wide_fib_prove -- --nocapture
 
         // Note: 17 means 128MB of trace.
