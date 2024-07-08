e62fdb8fd583935c087aaccb2d09cf8d1b085913
Shahar Papini <spapini@starkware.co>
2024-07-08 13:50:22 +0300

    Logup trace generation

--- crates/prover/benches/poseidon.rs
+++ crates/prover/benches/poseidon.rs
@@ -1,27 +1,12 @@
 use criterion::{criterion_group, criterion_main, Criterion, Throughput};
-use stwo_prover::core::backend::simd::SimdBackend;
-use stwo_prover::core::channel::{Blake2sChannel, Channel};
-use stwo_prover::core::fields::m31::BaseField;
-use stwo_prover::core::fields::IntoSlice;
-use stwo_prover::core::vcs::blake2_hash::Blake2sHasher;
-use stwo_prover::core::vcs::hasher::Hasher;
-use stwo_prover::examples::poseidon::{gen_trace, PoseidonAir, PoseidonComponent};
-use stwo_prover::trace_generation::commit_and_prove;
+use stwo_prover::examples::poseidon::prove_poseidon;
 
 pub fn simd_poseidon(c: &mut Criterion) {
-    const LOG_N_ROWS: u32 = 15;
+    const LOG_N_INSTANCES: u32 = 18;
     let mut group = c.benchmark_group("poseidon2");
-    group.throughput(Throughput::Elements(1u64 << (LOG_N_ROWS + 3)));
-    group.bench_function(format!("poseidon2 2^{} instances", LOG_N_ROWS + 3), |b| {
-        b.iter(|| {
-            let component = PoseidonComponent {
-                log_n_rows: LOG_N_ROWS,
-            };
-            let trace = gen_trace(component.log_column_size());
-            let channel = &mut Blake2sChannel::new(Blake2sHasher::hash(BaseField::into_slice(&[])));
-            let air = PoseidonAir { component };
-            commit_and_prove::<SimdBackend>(&air, channel, trace).unwrap()
-        });
+    group.throughput(Throughput::Elements(1u64 << LOG_N_INSTANCES));
+    group.bench_function(format!("poseidon2 2^{} instances", LOG_N_INSTANCES), |b| {
+        b.iter(|| prove_poseidon(LOG_N_INSTANCES));
     });
 }
 
