7bc9d41854cf0aa9e9a9949d87cb8d27a8fde244
Shahar Papini <spapini@starkware.co>
2024-07-11 18:27:02 +0300

    Treespan

--- crates/prover/src/core/pcs/mod.rs
+++ crates/prover/src/core/pcs/mod.rs
@@ -11,6 +11,38 @@ pub mod quotients;
 mod utils;
 mod verifier;
 
+use std::ops::{Deref, DerefMut};
+
 pub use self::prover::{CommitmentSchemeProof, CommitmentSchemeProver, CommitmentTreeProver};
 pub use self::utils::TreeVec;
 pub use self::verifier::CommitmentSchemeVerifier;
+
+#[derive(Copy, Debug, Clone)]
+pub struct TreeColumnSpan {
+    pub tree_index: usize,
+    pub col_start: usize,
+    pub col_end: usize,
+}
+
+#[derive(Debug, Clone)]
+pub struct TreePortion(pub TreeVec<TreeColumnSpan>);
+impl Deref for TreePortion {
+    type Target = TreeVec<TreeColumnSpan>;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+impl DerefMut for TreePortion {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+impl TreePortion {
+    pub fn take<'a, T>(&self, source: TreeVec<&'a [T]>) -> TreeVec<&'a [T]> {
+        self.0
+            .as_ref()
+            .map(|span| &source[span.tree_index][span.col_start..span.col_end])
+    }
+}
