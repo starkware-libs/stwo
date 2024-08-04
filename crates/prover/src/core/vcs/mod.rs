5b9966d7d00dcf918c611a67648112d39e177e18
carlogf <cgferrari@gmail.com>
2024-08-04 04:16:52 -0300

    Refactor generalize channel and merkleHasher (#759)
    Co-authored-by: Sergio Chouhy <sergio.chouhy@gmail.com>

--- crates/prover/src/core/vcs/mod.rs
+++ crates/prover/src/core/vcs/mod.rs
@@ -4,7 +4,7 @@ pub mod blake2_hash;
 pub mod blake2_merkle;
 pub mod blake2s_ref;
 pub mod blake3_hash;
-pub mod hasher;
+pub mod hash;
 pub mod ops;
 #[cfg(not(target_arch = "wasm32"))]
 pub mod poseidon252_merkle;
