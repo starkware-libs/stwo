48a733f29717e19333828ccce7a2c7ad072cc195
Andrew Milson <andrew.j.milson@gmail.com>
2024-09-21 14:09:26 -1000

    Update toolchain

--- crates/prover/src/core/pcs/prover.rs
+++ crates/prover/src/core/pcs/prover.rs
@@ -162,7 +162,7 @@ pub struct TreeBuilder<'a, 'b, B: BackendForChannel<MC>, MC: MerkleChannel> {
     commitment_scheme: &'a mut CommitmentSchemeProver<'b, B, MC>,
     polys: ColumnVec<CirclePoly<B>>,
 }
-impl<'a, 'b, B: BackendForChannel<MC>, MC: MerkleChannel> TreeBuilder<'a, 'b, B, MC> {
+impl<B: BackendForChannel<MC>, MC: MerkleChannel> TreeBuilder<'_, '_, B, MC> {
     pub fn extend_evals(
         &mut self,
         columns: impl IntoIterator<Item = CircleEvaluation<B, BaseField, BitReversedOrder>>,
