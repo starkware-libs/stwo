971c8cf03297e4a29dd3f56f968a51b5e93f4678
Ilya Lesokhin <ilya@starkware.co>
2024-11-05 16:22:27 +0200

    Support shared preproccessed columns.

--- crates/prover/src/constraint_framework/info.rs
+++ crates/prover/src/constraint_framework/info.rs
@@ -2,7 +2,9 @@ use std::ops::Mul;
 
 use num_traits::One;
 
+use super::preprocessed_columns::PreprocessedColumn;
 use super::EvalAtRow;
+use crate::constraint_framework::PREPROCESSED_TRACE_IDX;
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
 use crate::core::pcs::TreeVec;
@@ -13,6 +15,7 @@ use crate::core::pcs::TreeVec;
 pub struct InfoEvaluator {
     pub mask_offsets: TreeVec<Vec<Vec<isize>>>,
     pub n_constraints: usize,
+    pub preprocessed_columns: Vec<PreprocessedColumn>,
 }
 impl InfoEvaluator {
     pub fn new() -> Self {
@@ -22,11 +25,17 @@ impl InfoEvaluator {
 impl EvalAtRow for InfoEvaluator {
     type F = BaseField;
     type EF = SecureField;
+
     fn next_interaction_mask<const N: usize>(
         &mut self,
         interaction: usize,
         offsets: [isize; N],
     ) -> [Self::F; N] {
+        assert!(
+            interaction != PREPROCESSED_TRACE_IDX,
+            "Preprocessed should be accesses with `get_preprocessed_column`",
+        );
+
         // Check if requested a mask from a new interaction
         if self.mask_offsets.len() <= interaction {
             // Extend `mask_offsets` so that `interaction` is the last index.
@@ -35,6 +44,12 @@ impl EvalAtRow for InfoEvaluator {
         self.mask_offsets[interaction].push(offsets.into_iter().collect());
         [BaseField::one(); N]
     }
+
+    fn get_preprocessed_column(&mut self, column: PreprocessedColumn) -> Self::F {
+        self.preprocessed_columns.push(column);
+        BaseField::one()
+    }
+
     fn add_constraint<G>(&mut self, _constraint: G)
     where
         Self::EF: Mul<G, Output = Self::EF>,
