eb994164742c7beaf5c4a3d3cea50fca7209e68d
Alon Titelman <alon.titelman@starkware.co>
2024-11-25 13:05:36 +0200

    Added `From<G>` in `add_constraint`.

--- crates/prover/src/constraint_framework/cpu_domain.rs
+++ crates/prover/src/constraint_framework/cpu_domain.rs
@@ -85,7 +85,7 @@ impl<'a> EvalAtRow for CpuDomainEvaluator<'a> {
 
     fn add_constraint<G>(&mut self, constraint: G)
     where
-        Self::EF: Mul<G, Output = Self::EF>,
+        Self::EF: Mul<G, Output = Self::EF> + From<G>,
     {
         self.row_res += self.random_coeff_powers[self.constraint_index] * constraint;
         self.constraint_index += 1;
