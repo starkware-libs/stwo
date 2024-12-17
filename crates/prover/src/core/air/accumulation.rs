d670a19f42970daaaea2145cca10fbb765b1c17d
Gali Michlevich <gali@starkware.co>
2024-12-17 10:21:17 +0200

    Unite impls of DomainEvaluationAccumulator

--- crates/prover/src/core/air/accumulation.rs
+++ crates/prover/src/core/air/accumulation.rs
@@ -99,16 +99,7 @@ impl<B: Backend> DomainEvaluationAccumulator<B> {
     pub fn log_size(&self) -> u32 {
         (self.sub_accumulations.len() - 1) as u32
     }
-}
 
-pub trait AccumulationOps: FieldOps<BaseField> + Sized {
-    /// Accumulates other into column:
-    ///   column = column + other.
-    fn accumulate(column: &mut SecureColumnByCoords<Self>, other: &SecureColumnByCoords<Self>);
-    fn generate_secure_powers(felt: SecureField, n_powers: usize) -> Vec<SecureField>;
-}
-
-impl<B: Backend> DomainEvaluationAccumulator<B> {
     /// Computes f(P) as coefficients.
     pub fn finalize(self) -> SecureCirclePoly<B> {
         assert_eq!(
@@ -155,6 +146,13 @@ impl<B: Backend> DomainEvaluationAccumulator<B> {
             }))
         })
     }
+}
+
+pub trait AccumulationOps: FieldOps<BaseField> + Sized {
+    /// Accumulates other into column:
+    ///   column = column + other.
+    fn accumulate(column: &mut SecureColumnByCoords<Self>, other: &SecureColumnByCoords<Self>);
+    fn generate_secure_powers(felt: SecureField, n_powers: usize) -> Vec<SecureField>;
 }
 
 /// A domain accumulator for polynomials of a single size.
