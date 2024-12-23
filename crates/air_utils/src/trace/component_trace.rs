a972671e57f843ebfe9ee2e7e84278180663c4c1
Ohad Agadi <ohad.a@starkware.co>
2024-12-23 11:58:26 +0200

    trace to evals

--- crates/air_utils/src/trace/component_trace.rs
+++ crates/air_utils/src/trace/component_trace.rs
@@ -1,8 +1,9 @@
 use bytemuck::Zeroable;
+use stwo_prover::core::backend::simd::column::BaseColumn;
 use stwo_prover::core::backend::simd::m31::{PackedM31, LOG_N_LANES, N_LANES};
 use stwo_prover::core::backend::simd::SimdBackend;
 use stwo_prover::core::fields::m31::M31;
-use stwo_prover::core::poly::circle::CircleEvaluation;
+use stwo_prover::core::poly::circle::{CanonicCoset, CircleEvaluation};
 use stwo_prover::core::poly::BitReversedOrder;
 
 use super::row_iterator::{ParRowIterMut, RowIterMut};
@@ -79,7 +80,14 @@ impl<const N: usize> ComponentTrace<N> {
     }
 
     pub fn to_evals(self) -> [CircleEvaluation<SimdBackend, M31, BitReversedOrder>; N] {
-        todo!()
+        let domain = CanonicCoset::new(self.log_size).circle_domain();
+        self.data.map(|column| {
+            let eval = BaseColumn {
+                data: column,
+                length: 1 << self.log_size,
+            };
+            CircleEvaluation::<SimdBackend, M31, BitReversedOrder>::new(domain, eval)
+        })
     }
 
     pub fn row_at(&self, row: usize) -> [M31; N] {
