eaf42366d70d90787489a0b8dea5820d43dd2664
Ohad Agadi <ohad.a@starkware.co>
2024-12-23 11:58:26 +0200

    iter mut

--- crates/air_utils/src/lookup_data/mod.rs
+++ crates/air_utils/src/lookup_data/mod.rs
@@ -2,12 +2,12 @@
 // TODO(Ohad): write a derive macro for this.
 use rayon::iter::plumbing::{bridge, Consumer, Producer, ProducerCallback, UnindexedConsumer};
 use rayon::prelude::*;
-use stwo_air_utils_derive::Uninitialized;
+use stwo_air_utils_derive::{MutIter, Uninitialized};
 use stwo_prover::core::backend::simd::m31::{PackedM31, N_LANES};
 
 /// Lookup data for the example ComponentTrace.
 /// Vectors are assumed to be of the same length.
-#[derive(Uninitialized)]
+#[derive(Uninitialized, MutIter)]
 struct LookupData {
     field0: Vec<PackedM31>,
     field1: Vec<[PackedM31; 2]>,
@@ -41,5 +41,104 @@ mod tests {
         ]
         .concat();
         assert!(all(lengths, |len| len == 1 << LOG_SIZE));
+    }
+
+    #[test]
+    fn test_derived_lookup_data_iter_mut() {
+        const N_COLUMNS: usize = 5;
+        const LOG_SIZE: u32 = 8;
+        let mut trace = ComponentTrace::<N_COLUMNS>::zeroed(LOG_SIZE);
+        let arr = (0..1 << LOG_SIZE).map(M31::from).collect_vec();
+        let mut lookup_data = unsafe { LookupData::uninitialized(LOG_SIZE - LOG_N_LANES) };
+        let expected: (Vec<_>, Vec<_>, Vec<_>) = arr
+            .array_chunks::<N_LANES>()
+            .map(|x| {
+                let x = PackedM31::from_array(*x);
+                let x1 = x + PackedM31::broadcast(M31(1));
+                let x2 = x + x1;
+                let x3 = x + x1 + x2;
+                let x4 = x + x1 + x2 + x3;
+                (
+                    x4,
+                    [x1, x1.double()],
+                    ([x2, x2.double()], [x3, x3.double()]),
+                )
+            })
+            .multiunzip();
+
+        trace
+            .iter_mut()
+            .zip(arr.chunks(N_LANES))
+            .zip(lookup_data.iter_mut())
+            .for_each(|((row, input), lookup_data)| {
+                *row[0] = PackedM31::from_array(input.try_into().unwrap());
+                *row[1] = *row[0] + PackedM31::broadcast(M31(1));
+                *row[2] = *row[0] + *row[1];
+                *row[3] = *row[0] + *row[1] + *row[2];
+                *row[4] = *row[0] + *row[1] + *row[2] + *row[3];
+                *lookup_data.field0 = *row[4];
+                *lookup_data.field1 = [*row[1], row[1].double()];
+                *lookup_data.field2[0] = [*row[2], row[2].double()];
+                *lookup_data.field2[1] = [*row[3], row[3].double()];
+            });
+
+        let actual = (
+            lookup_data.field0,
+            lookup_data.field1,
+            (lookup_data.field2[0].clone(), lookup_data.field2[1].clone()),
+        );
+
+        assert!(
+            all(
+                expected
+                    .0
+                    .iter()
+                    .zip(actual.0.iter())
+                    .map(|(expected, actual)| (expected.to_array(), actual.to_array())),
+                |(expected, actual)| expected == actual
+            ),
+            "Failed on Vec<PackedM31>"
+        );
+        assert!(
+            all(
+                expected
+                    .1
+                    .iter()
+                    .zip(actual.1.iter())
+                    .map(|(expected, actual)| (
+                        expected.iter().flat_map(|v| v.to_array()).collect_vec(),
+                        actual.iter().flat_map(|v| v.to_array()).collect_vec()
+                    )),
+                |(expected, actual)| expected == actual
+            ),
+            "Failed on Vec<[PackedM31; 2]>"
+        );
+        assert!(all(
+            expected
+                .2
+                .iter()
+                .map(|expected| expected.0)
+                .zip(actual.2 .0.into_iter())
+                .map(|(expected, actual)| (
+                    expected.iter().flat_map(|v| v.to_array()).collect_vec(),
+                    actual.iter().flat_map(|v| v.to_array()).collect_vec()
+                )),
+            |(expected, actual)| expected == actual
+        ));
+        assert!(
+            all(
+                expected
+                    .2
+                    .iter()
+                    .map(|e| e.1)
+                    .zip(actual.2 .1.into_iter())
+                    .map(|(expected, actual)| (
+                        expected.iter().flat_map(|v| v.to_array()).collect_vec(),
+                        actual.iter().flat_map(|v| v.to_array()).collect_vec()
+                    )),
+                |(expected, actual)| expected == actual
+            ),
+            "Failed on [Vec<[PackedM31; 2]>; 2]"
+        );
     }
 }
--- crates/air_utils_derive/src/lib.rs
+++ crates/air_utils_derive/src/lib.rs
@@ -1,4 +1,5 @@
 mod allocation;
+mod iter_mut;
 mod iterable_field;
 use iterable_field::IterableField;
 use syn::{parse_macro_input, Data, DeriveInput, Fields};
@@ -14,6 +15,19 @@ pub fn derive_uninitialized(input: proc_macro::TokenStream) -> proc_macro::Token
     };
 
     allocation::expand_uninitialized_impl(&struct_name, &iterable_fields).into()
+}
+
+#[proc_macro_derive(MutIter)]
+pub fn derive_mut_iter(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
+    let input = parse_macro_input!(input as DeriveInput);
+    let struct_name = input.ident.clone();
+
+    let iterable_fields = match parse_derive_input_to_iterable_fields(input) {
+        Ok(iterable_fields) => iterable_fields,
+        Err(err) => return err.into_compile_error().into(),
+    };
+
+    iter_mut::expand_iter_mut_structs(&struct_name, &iterable_fields).into()
 }
 
 fn parse_derive_input_to_iterable_fields(
