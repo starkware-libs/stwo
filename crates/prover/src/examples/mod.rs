6ba7258947ef672602a62f73ac22300ae7d49ac0
Gali Michlevich <gali@starkware.co>
2025-01-09 16:05:38 +0200

    Add Structs using PreprocessedColumn Trait

--- crates/prover/src/examples/mod.rs
+++ crates/prover/src/examples/mod.rs
@@ -1,6 +1,7 @@
 pub mod blake;
 pub mod plonk;
 pub mod poseidon;
+pub mod preprocessed_columns;
 pub mod state_machine;
 pub mod wide_fibonacci;
 pub mod xor;
---
+++ crates/prover/src/examples/plonk/preprocessed_columns.rs
@@ -0,0 +1,25 @@
+use crate::constraint_framework::preprocessed_columns::PreprocessedColumnTrait;
+
+// TODO(Gali): Add documentation.
+#[derive(Debug)]
+pub struct Plonk {
+    pub wire: usize,
+}
+impl Plonk {
+    pub const fn new(wire: usize) -> Self {
+        Self { wire }
+    }
+}
+impl PreprocessedColumnTrait for Plonk {
+    fn name(&self) -> &'static str {
+        "preprocessed_plonk"
+    }
+
+    fn id(&self) -> String {
+        format!("Plonk(wire: {})", self.wire)
+    }
+    
+    fn log_size(&self) -> u32 {
+        todo!("Plonk::log_size")
+    }
+}
