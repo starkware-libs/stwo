bf8e47212bd34e4f292a9288c708d3c20f949a2d
alon.dotan <alon.dotan@starkware.co>
2025-01-15 09:50:46 +0200

    chore: fix bench chore: use cargo-criterion

--- scripts/bench.sh
+++ scripts/bench.sh
@@ -2,4 +2,4 @@
 # Can be used as a drop in replacement for `cargo bench`.
 # For example, `./scripts/bench.sh` will run all benchmarks.
 # or `./scripts/bench.sh M31` will run only the M31 benchmarks.
-RUSTFLAGS="-Awarnings -C target-cpu=native -C target-feature=+avx512f -C opt-level=3" cargo bench $@
+RUSTFLAGS="-Awarnings -C target-cpu=native -C opt-level=3" cargo criterion --output-format bencher $@
