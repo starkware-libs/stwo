bfd32bbc2376df573a42554c0912d24cd1752cb7
ohad-starkware <ohad.a@starkware.co>
2025-07-21 08:57:53 +0300

    rfft throughput benchmark

--- crates/stwo/benches/fft.rs
+++ crates/stwo/benches/fft.rs
@@ -101,14 +101,16 @@ pub fn simd_ifft_parts(c: &mut Criterion) {
 }
 
 pub fn simd_rfft(c: &mut Criterion) {
-    const LOG_SIZE: u32 = 20;
+    const LOG_SIZE: u32 = 24;
 
     let domain = CanonicCoset::new(LOG_SIZE).circle_domain();
     let twiddle_dbls = get_twiddle_dbls(domain.half_coset);
     let twiddle_dbls_refs = twiddle_dbls.iter().map(|x| x.as_slice()).collect_vec();
     let values: BaseColumn = (0..domain.size()).map(BaseField::from).collect();
 
-    c.bench_function("simd rfft 20bit", |b| {
+    let mut group = c.benchmark_group("rfft");
+    group.throughput(Throughput::Bytes(4 << LOG_SIZE));
+    group.bench_function("simd rfft 20bit", |b| {
         b.iter_with_large_drop(|| unsafe {
             let mut target = Vec::<PackedBaseField>::with_capacity(values.data.len());
             #[allow(clippy::uninit_vec)]
@@ -129,5 +131,5 @@ pub fn simd_rfft(c: &mut Criterion) {
 criterion_group!(
     name = benches;
     config = Criterion::default().sample_size(10);
-    targets = simd_ifft, simd_ifft_parts, simd_rfft);
+    targets = simd_rfft, simd_ifft, simd_ifft_parts);
 criterion_main!(benches);
