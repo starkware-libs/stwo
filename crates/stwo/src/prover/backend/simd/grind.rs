d007f9fbf8509b41be42409add2cb0fb1135d99b
ohad-starkware <ohad.a@starkware.co>
2025-07-23 15:22:48 +0300

    fixed pow performance bug

--- crates/stwo/src/prover/backend/simd/grind.rs
+++ crates/stwo/src/prover/backend/simd/grind.rs
@@ -32,10 +32,7 @@ impl GrindOps<Blake2sChannel> for SimdBackend {
             .expect("Grind failed to find a solution.");
 
         #[cfg(feature = "parallel")]
-        let res = (0..=(1 << GRIND_HI_BITS))
-            .into_par_iter()
-            .find_map_first(|hi| grind_blake(digest, hi, pow_bits))
-            .expect("Grind failed to find a solution.");
+        let res = parallel_grind(digest, pow_bits, grind_blake, 1 << GRIND_HI_BITS);
 
         res
     }
@@ -70,6 +67,57 @@ fn grind_blake(digest: &[u32], hi: u64, pow_bits: u32) -> Option<u64> {
     None
 }
 
+// Deterministically finds the smallest nonce that satisfies:
+// `hash(digest, nonce).trailing_zeros() >= pow_bits`.
+#[cfg(feature = "parallel")]
+fn parallel_grind<GRIND, DIGEST>(
+    digest: DIGEST,
+    pow_bits: u32,
+    grind: GRIND,
+    hi_bits_upper_bound: u64,
+) -> u64
+where
+    GRIND: Fn(DIGEST, u64, u32) -> Option<u64> + Send + Sync,
+    DIGEST: Send + Sync + Copy,
+{
+    use core::sync::atomic::{AtomicU64, Ordering};
+
+    let base_hi = AtomicU64::new(0);
+    let smallest_hi_found = AtomicU64::new(u64::MAX);
+    let found = (0..rayon::current_num_threads())
+        .into_par_iter()
+        .filter_map(|_| {
+            let mut found = None;
+            while found.is_none() {
+                let hi = base_hi.fetch_add(1, Ordering::Relaxed);
+                // Bailout if we've reached the upper bound.
+                if hi >= hi_bits_upper_bound {
+                    break;
+                }
+                // Bailout if we've already found a smaller hi.
+                if smallest_hi_found.load(Ordering::Relaxed) <= hi {
+                    break;
+                }
+                found = grind(digest, hi, pow_bits).map(|nonce| (nonce, hi));
+            }
+            found.map(|(nonce, hi)| {
+                let current_smallest_hi = smallest_hi_found.load(Ordering::Relaxed);
+                if hi < current_smallest_hi {
+                    let _ = smallest_hi_found.compare_exchange(
+                        current_smallest_hi,
+                        hi,
+                        Ordering::Relaxed,
+                        Ordering::Relaxed,
+                    );
+                }
+                nonce
+            })
+        })
+        .min();
+
+    found.expect("Grind failed to find a solution.")
+}
+
 #[cfg(not(target_arch = "wasm32"))]
 pub mod poseidon252 {
     use starknet_ff::FieldElement as FieldElement252;
@@ -77,24 +125,21 @@ pub mod poseidon252 {
     use super::*;
     use crate::core::channel::Poseidon252Channel;
 
-    const GRIND_LOW_BITS: u32 = 12;
+    const GRIND_LOW_BITS: u32 = 14;
     const GRIND_HI_BITS: u32 = 64 - GRIND_LOW_BITS;
 
     impl GrindOps<Poseidon252Channel> for SimdBackend {
         fn grind(channel: &Poseidon252Channel, pow_bits: u32) -> u64 {
             let digest = channel.digest();
+            let upper_bound = 1 << GRIND_HI_BITS;
 
             #[cfg(not(feature = "parallel"))]
-            let res = (0..=(1 << GRIND_HI_BITS))
+            let res = (0..=upper_bound)
                 .find_map(|hi| grind_poseidon(digest, hi, pow_bits))
                 .expect("Grind failed to find a solution.");
 
             #[cfg(feature = "parallel")]
-            let res = (0..=(1 << GRIND_HI_BITS))
-                .into_par_iter()
-                .find_map_first(|hi| grind_poseidon(digest, hi, pow_bits))
-                .expect("Grind failed to find a solution.");
-
+            let res = parallel_grind(digest, pow_bits, grind_poseidon, upper_bound);
             res
         }
     }
