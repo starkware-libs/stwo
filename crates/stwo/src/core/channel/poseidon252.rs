6b1bc111f3480399c0e87954766c6beb2c1ee78d
Alon F <alonf@starkware.co>
2025-09-04 10:42:16 +0300

    domain separation in poseidon252 channel

--- crates/stwo/src/core/channel/poseidon252.rs
+++ crates/stwo/src/core/channel/poseidon252.rs
@@ -1,7 +1,7 @@
 use core::{array, iter};
 
 use itertools::Itertools;
-use starknet_crypto::{poseidon_hash, poseidon_hash_many};
+use starknet_crypto::{poseidon_hash, poseidon_hash_many, poseidon_permute_comp};
 use starknet_ff::FieldElement as FieldElement252;
 use std_shims::{vec, Vec};
 
@@ -30,7 +30,13 @@ impl Poseidon252Channel {
         self.channel_time.inc_challenges();
     }
     fn draw_secure_felt252(&mut self) -> FieldElement252 {
-        let res = poseidon_hash(self.digest, self.channel_time.n_sent.into());
+        let mut state = [
+            self.digest,
+            self.channel_time.n_sent.into(),
+            FieldElement252::THREE,
+        ];
+        poseidon_permute_comp(&mut state);
+        let res = state[0];
         self.channel_time.inc_sent();
         res
     }
