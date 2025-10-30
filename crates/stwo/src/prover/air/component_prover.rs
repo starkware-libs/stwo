c7cbe7e05b9d5c2f36101fec14dfac24131a73b0
Gali Michlevich <gali@starkware.co>
2025-10-30 16:21:06 +0200

    Poly Struct for Storing Trace Polynomials

--- crates/stwo/src/prover/air/component_prover.rs
+++ crates/stwo/src/prover/air/component_prover.rs
@@ -4,11 +4,14 @@ use crate::core::air::{Component, Components};
 use crate::core::fields::m31::BaseField;
 use crate::core::fields::qm31::SecureField;
 use crate::core::pcs::TreeVec;
+use crate::core::poly::circle::CircleDomain;
 use crate::core::ColumnVec;
 use crate::prover::air::accumulation::DomainEvaluationAccumulator;
 use crate::prover::backend::Backend;
 use crate::prover::poly::circle::{CircleEvaluation, CirclePoly, SecureCirclePoly};
+use crate::prover::poly::twiddles::TwiddleTree;
 use crate::prover::poly::BitReversedOrder;
+use crate::prover::CirclePoint;
 
 pub trait ComponentProver<B: Backend>: Component {
     /// Evaluates the constraint quotients of the component on the evaluation domain.
@@ -28,6 +31,51 @@ pub struct Trace<'a, B: Backend> {
     pub polys: TreeVec<ColumnVec<&'a CirclePoly<B>>>,
     /// Evaluations for each column (evaluated on their commitment domains).
     pub evals: TreeVec<ColumnVec<&'a CircleEvaluation<B, BaseField, BitReversedOrder>>>,
+}
+
+/// A struct for representing a polynomial corresponding to a trace column.
+/// A polynomial is defined by it's evaluations on a circle domain of size at least it's degree,
+/// and optionally its coefficients in the FFT basis.
+#[allow(dead_code)]
+pub struct Poly<B: Backend> {
+    pub coeffs: Option<CirclePoly<B>>,
+    pub evals: CircleEvaluation<B, BaseField, BitReversedOrder>,
+}
+
+impl<B: Backend> Poly<B> {
+    #[allow(dead_code)]
+    pub const fn new(
+        coeffs: Option<CirclePoly<B>>,
+        evals: CircleEvaluation<B, BaseField, BitReversedOrder>,
+    ) -> Self {
+        Self { coeffs, evals }
+    }
+
+    #[allow(dead_code)]
+    pub fn eval_at_point(
+        &self,
+        point: CirclePoint<SecureField>,
+        twiddles: &TwiddleTree<B>,
+    ) -> SecureField {
+        if let Some(coeffs) = &self.coeffs {
+            coeffs.eval_at_point(point)
+        } else {
+            self.evals.eval_at_point_by_folding(point, twiddles)
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn get_evaluation_on_domain(
+        &self,
+        domain: CircleDomain,
+        twiddles: &TwiddleTree<B>,
+    ) -> CircleEvaluation<B, BaseField, BitReversedOrder> {
+        if let Some(coeffs) = &self.coeffs {
+            coeffs.evaluate_with_twiddles(domain, twiddles)
+        } else {
+            panic!("The polynomial's coefficients are not stored");
+        }
+    }
 }
 
 pub struct ComponentProvers<'a, B: Backend> {
