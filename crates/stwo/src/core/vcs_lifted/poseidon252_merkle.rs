b36a45e75637aedb8f853c6a67a3b8aa80552e6a
leo-starkware <leo@starkware.co>
2025-11-30 15:56:31 +0200

    Add poseidon hasher.

--- crates/stwo/src/core/vcs_lifted/poseidon252_merkle.rs
+++ crates/stwo/src/core/vcs_lifted/poseidon252_merkle.rs
@@ -1,43 +1,109 @@
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
+use starknet_crypto::{poseidon_hash, poseidon_permute_comp};
 use starknet_ff::FieldElement as FieldElement252;
+use std_shims::Vec;
 
 use crate::core::channel::{MerkleChannel, Poseidon252Channel};
 use crate::core::fields::m31::BaseField;
+use crate::core::vcs::poseidon252_merkle::{construct_felt252_from_m31s, ELEMENTS_IN_BLOCK};
 use crate::core::vcs_lifted::merkle_hasher::MerkleHasherLifted;
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Default, Deserialize, Serialize)]
-pub struct Poseidon252MerkleHasher;
+pub const ELEMENTS_IN_BUFFER: usize = 2 * ELEMENTS_IN_BLOCK;
 
-#[allow(unused)]
+#[derive(Clone, Debug, PartialEq, Eq, Default, Deserialize, Serialize)]
+pub struct Poseidon252MerkleHasher {
+    state: [FieldElement252; 3],
+    buffer: Vec<BaseField>,
+}
+
 impl MerkleHasherLifted for Poseidon252MerkleHasher {
     type Hash = FieldElement252;
 
     fn default_with_initial_state() -> Self {
-        unimplemented!()
+        Self::default()
     }
 
-    fn hash_children(children_hashes: (Self::Hash, Self::Hash)) -> Self::Hash {
-        unimplemented!()
+    fn hash_children((left, right): (Self::Hash, Self::Hash)) -> Self::Hash {
+        poseidon_hash(left, right)
     }
 
     fn update_leaf(&mut self, column_values: &[BaseField]) {
-        unimplemented!()
+        let chunks = self
+            .buffer
+            .iter()
+            .chain(column_values)
+            .chunks(ELEMENTS_IN_BUFFER);
+
+        // The elements to be hashed are collected in `values`.
+        let mut values = Vec::new();
+        // The rest is collected in `remainder` and stored in the hasher's buffer.
+        let mut remainder = Vec::new();
+        for chunk in &chunks {
+            let mut chunk: Vec<_> = chunk.into_iter().copied().collect();
+            // If we take this branch we are in the last iteration.
+            if chunk.len() < ELEMENTS_IN_BUFFER {
+                remainder.extend(chunk);
+                break;
+            }
+            let second = chunk.split_off(ELEMENTS_IN_BLOCK);
+            values.extend([
+                construct_felt252_from_m31s(&chunk),
+                construct_felt252_from_m31s(&second),
+            ])
+        }
+        self.buffer = remainder;
+        poseidon_update(&values, &mut self.state);
     }
 
     fn finalize(self) -> Self::Hash {
-        unimplemented!()
+        let remainder: Vec<FieldElement252> = self
+            .buffer
+            .chunks(ELEMENTS_IN_BLOCK)
+            .map(construct_felt252_from_m31s)
+            .collect();
+        let state = poseidon_finalize(&remainder, self.state);
+        state[0]
     }
+}
+
+pub fn poseidon_update(values: &[FieldElement252], state: &mut [FieldElement252; 3]) {
+    debug_assert!(values.len().is_multiple_of(2));
+    let mut iter = values.chunks_exact(2);
+    for msg in iter.by_ref() {
+        state[0] += msg[0];
+        state[1] += msg[1];
+        poseidon_permute_comp(state);
+    }
+}
+
+pub fn poseidon_finalize(
+    values: &[FieldElement252],
+    mut state: [FieldElement252; 3],
+) -> [FieldElement252; 3] {
+    let mut iter = values.chunks_exact(2);
+    for msg in iter.by_ref() {
+        state[0] += msg[0];
+        state[1] += msg[1];
+        poseidon_permute_comp(&mut state);
+    }
+    let r = iter.remainder();
+    if r.len() == 1 {
+        state[0] += r[0];
+    }
+    state[r.len()] += FieldElement252::ONE;
+    poseidon_permute_comp(&mut state);
+    state
 }
 
 #[derive(Default)]
 pub struct Poseidon252MerkleChannel;
 
-#[allow(unused)]
 impl MerkleChannel for Poseidon252MerkleChannel {
     type C = Poseidon252Channel;
     type H = Poseidon252MerkleHasher;
 
     fn mix_root(channel: &mut Self::C, root: <Self::H as MerkleHasherLifted>::Hash) {
-        unimplemented!()
+        channel.update_digest(poseidon_hash(channel.digest(), root));
     }
 }
