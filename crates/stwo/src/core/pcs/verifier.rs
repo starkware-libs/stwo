1820d125385c7fc7e57b0ecfca07d7f700559d4b
leo-starkware <146624637+leo-starkware@users.noreply.github.com>
2025-12-07 10:58:47 +0200

    Add pcs verifier lifted. (#1255)

--- crates/stwo/src/core/pcs/verifier.rs
+++ crates/stwo/src/core/pcs/verifier.rs
@@ -1,7 +1,7 @@
 use core::iter::zip;
 
 use itertools::Itertools;
-use std_shims::Vec;
+use std_shims::{vec, Vec};
 
 use super::super::circle::CirclePoint;
 use super::super::fields::qm31::SecureField;
@@ -62,25 +62,11 @@ impl<MC: MerkleChannel> CommitmentSchemeVerifier<MC> {
     ) -> Result<(), VerificationError> {
         channel.mix_felts(&proof.sampled_values.clone().flatten_cols());
         let random_coeff = channel.draw_secure_felt();
+        let max_log_size = *self.column_log_sizes().iter().flatten().max().unwrap();
 
-        let bounds = self
-            .column_log_sizes()
-            .zip_cols(&sampled_points)
-            .flatten()
-            .into_iter()
-            .filter_map(|(log_size, sampled_points)| {
-                if sampled_points.is_empty() {
-                    return None;
-                }
-                Some(log_size)
-            })
-            .sorted()
-            .rev()
-            .dedup()
-            .map(|log_size| {
-                CirclePolyDegreeBound::new(log_size - self.config.fri_config.log_blowup_factor)
-            })
-            .collect_vec();
+        let bounds = vec![CirclePolyDegreeBound::new(
+            max_log_size - self.config.fri_config.log_blowup_factor,
+        )];
 
         // FRI commitment phase on OODS quotients.
         let mut fri_verifier =
@@ -121,11 +107,14 @@ impl<MC: MerkleChannel> CommitmentSchemeVerifier<MC> {
 
         let n_columns_per_log_size = self.trees.as_ref().map(|tree| &tree.n_columns_per_log_size);
 
+        // TODO(Leo): remove after we remove query_positions_per_log_size
+        let query_positions = query_positions_per_log_size.values().next().unwrap();
+
         let fri_answers = fri_answers(
             self.column_log_sizes(),
             samples,
             random_coeff,
-            &query_positions_per_log_size,
+            query_positions,
             proof.queried_values,
             n_columns_per_log_size,
         )?;
--- crates/stwo/src/prover/pcs/mod.rs
+++ crates/stwo/src/prover/pcs/mod.rs
@@ -154,14 +154,12 @@ impl<'a, B: BackendForChannel<MC>, MC: MerkleChannel> CommitmentSchemeProver<'a,
             .polynomials()
             .zip_cols(&sampled_points)
             .map_cols(|(poly, points)| {
-                let lde_domain_log_size =
-                    poly.evals.domain.log_size() + self.config.fri_config.log_blowup_factor;
                 points
                     .iter()
                     .map(|&point| PointSample {
                         point,
                         value: poly.eval_at_point(
-                            point.repeated_double(max_log_size - lde_domain_log_size),
+                            point.repeated_double(max_log_size - poly.evals.domain.log_size()),
                             &*weights_hash_map
                                 .get(&(poly.evals.domain.log_size(), point))
                                 .expect("weights should exist for all sampled points"),
