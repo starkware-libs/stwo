50646803c17b6b45b1600a78c28df43c991af591
Yuval Goldberg <yuval@starkware.co>
2025-12-24 14:01:06 +0200

    Made clippy script more extensive + fixed issue
    Turns out a per-crate clippy catches issues that are not caught with a workspace level clippy (due to features sets differences)

--- scripts/clippy.sh
+++ scripts/clippy.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-cargo +nightly-2025-07-14 clippy "$@" --all-targets --all-features -- -D warnings -D future-incompatible \
-    -D nonstandard-style -D rust-2018-idioms -D unused
+cargo +nightly-2025-07-14 clippy --workspace "$@" --all-targets --all-features -- -D warnings \
+    -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused
+
+# Extract all crate names from the workspace metadata
+crates=$(cargo metadata --no-deps --format-version 1 | jq -r '.packages[].name' | sort -u)
+
+# Run clippy on each crate individually for catching issues that might not be detected when running
+# clippy on the entire workspace
+for crate in $crates; do
+  echo "Clippy on crate: $crate"
+  cargo +nightly-2025-07-14 clippy -p "$crate" --all-targets --all-features -- -D warnings \
+    -D future-incompatible -D nonstandard-style -D rust-2018-idioms -D unused
+done
